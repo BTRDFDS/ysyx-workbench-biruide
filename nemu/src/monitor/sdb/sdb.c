4276d5dc9609a1a2ec78d650bb6011891b56826e
tracer-ysyx <tracer@ysyx.org>
2026-01-22 16:12:31 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:12:31 up  8:22,  1 user,  load average: 0.09, 0.17, 0.12

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -56,6 +56,7 @@ static int cmd_si(char *args) {
 }
 
 static int cmd_info(char *args) {
+  if(args == NULL) {printf("r - print register\nw - print watching points\n");}
   switch (*args)
   {
   case 'r':
