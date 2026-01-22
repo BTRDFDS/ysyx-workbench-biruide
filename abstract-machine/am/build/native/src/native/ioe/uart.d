fd963d4d3f09e0c5b820076dc9dd9f1821b7ddb3
tracer-ysyx <tracer@ysyx.org>
2026-01-22 09:59:31 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:59:31 up  1:35,  1 user,  load average: 0.02, 0.06, 0.01

--- abstract-machine/am/build/native/src/native/ioe/uart.d
+++
@@ -1,5 +0,0 @@
-/home/biruide/ysyx-workbench/abstract-machine/am/build/native/src/native/ioe/uart.o: \
- /home/biruide/ysyx-workbench/abstract-machine/am/src/native/ioe/uart.c \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/arch/native.h \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/amdev.h
--- npc/vsrc/our.v
+++
@@ -1,7 +0,0 @@
-module our(input a,b,output f);
-	assign f = a^b;
-	initial begin
-		$display("Hello World");
-		// $finish;
-		end
-endmodule
