69083ea8facd2d30aab487958401d2c6b5ef9d44
Bi RuiDe <1776427100@qq.com>
2026-01-23 22:11:49 +0800

    完成了测试用例的生成工具

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -30,20 +30,75 @@ static char *code_format =
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
-
+int len = 65535;
+int iTemp;
+// char *cTemp = NULL;
+uint32_t choose(uint32_t n){
+  return rand()%n;
+}
+void gen_num() {
+  if(len>31){
+    iTemp=2^31;
+  }else{
+    iTemp=2^len;
+  }
+  // sprintf(cTemp,"%u",iTemp);
+  // if(len-strlen(cTemp)>0)
+  sprintf(buf+ strlen(buf), "%u",abs(rand()%iTemp));
+  len=65536-strlen(buf);
+}
+void gen(char c) {
+  sprintf(buf + strlen(buf), "%c", c);
+}
+void gen_rand_op(){
+  switch(choose(4)){
+    case 0: gen('+'); break;
+    case 1: gen('-'); break;
+    case 2: gen('*'); break;
+    case 3: gen('/'); break;
+    default: gen('+'); break;
+  }
+}
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  iTemp = choose(3);
+  if(len<=1){iTemp=0;}
+  switch (iTemp) {
+    case 0:
+      gen_num();
+      break;
+    case 1:
+      len-=2;
+      gen('(');
+      gen_rand_expr();
+      gen(')');
+      break;
+    case 2:
+      len-=2;
+      gen_rand_expr();
+      gen_rand_op();
+      len++;
+      gen_rand_expr();
+      break;
+    default:
+      gen_num();
+      break;
+  }
 }
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
-  srand(seed);
+  // srand(seed);
+  // srand(1769172359);//修复清零的种子
+  srand(1769173342);//修复除以零的种子
+  printf("seed = %d\n", seed);
   int loop = 1;
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    len = 65535;
+    buf[0] = '\0';
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
@@ -53,7 +108,8 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr -Werror");
+    // int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
@@ -62,7 +118,8 @@ int main(int argc, char *argv[]) {
     int result;
     ret = fscanf(fp, "%d", &result);
     pclose(fp);
-
+    // if(ret != 1) continue;
+    // if(res != 0) continue;
     printf("%u %s\n", result, buf);
   }
   return 0;
