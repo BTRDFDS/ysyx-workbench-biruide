fba8bccd146c9cf854ac11d42c372ecbd45e922c
tracer-ysyx <tracer@ysyx.org>
2026-01-25 19:39:25 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:39:25 up 10:37,  1 user,  load average: 0.90, 0.68, 0.47

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -24,8 +24,8 @@ int is_exit_status_bad();
 #define genExprMax 10000
 void gen_expr(){
   // init_regex();
-  FILE *fp = fopen("/home/biruide/ysyx-workbench/nemu/tools/gen-expr/genExpr", "r");
-  // FILE *fp = fopen("/home/biruide/ysyx-workbench/nemu/tools/gen-expr/use", "r");
+  // FILE *fp = fopen("/home/biruide/ysyx-workbench/nemu/tools/gen-expr/genExpr", "r");
+  FILE *fp = fopen("/home/biruide/ysyx-workbench/nemu/tools/gen-expr/use", "r");
   assert(fp!=NULL);
   char buf[65570];
   char *res;
