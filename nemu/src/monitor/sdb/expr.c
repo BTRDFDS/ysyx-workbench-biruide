a93029a91c3343d9902ca0d072c109ddad5dfadb
tracer-ysyx <tracer@ysyx.org>
2026-01-25 09:18:32 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:18:32 up 22 min,  1 user,  load average: 0.05, 0.05, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -250,7 +250,7 @@ word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
-  }
+  }else{*success=true;}
 
   /* TODO: Insert codes to evaluate the expression. */
   // TODO();
