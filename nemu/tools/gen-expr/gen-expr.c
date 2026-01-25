59575dafa507a89ceca4cbcd0397e63b31cbc438
tracer-ysyx <tracer@ysyx.org>
2026-01-26 07:39:58 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:39:58 up 10 min,  1 user,  load average: 0.25, 0.43, 0.24

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -150,7 +150,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
     system("sed 's/\\*/\\*(uint32_t)/g' /tmp/.code.c > /tmp/.code2.c");
-    int ret = system("gcc /tmp/.code2.c -o /tmp/.expr 2>/dev/null");
+    int ret = system("gcc /tmp/.code2.c -Werror -o /tmp/.expr 2>/dev/null");
     // int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
     // printf("%d\n",ret);
     if (ret != 0){i--;continue;}
