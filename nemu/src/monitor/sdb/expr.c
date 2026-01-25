b539a467fa1625b2a2825748058c1a73f083db82
tracer-ysyx <tracer@ysyx.org>
2026-01-25 16:28:27 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:28:27 up  7:14,  1 user,  load average: 0.21, 0.11, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -192,7 +192,7 @@ uint32_t eval(int p, int q) {
           j=2;
           break;
         case '-':
-          if(i>p&&tokens[i-1].type==TK_NUM){
+          if(i>p&&(tokens[i-1].type==TK_NUM||tokens[i-1].type==')')){
             j=2;
           }else{
             j=0;
