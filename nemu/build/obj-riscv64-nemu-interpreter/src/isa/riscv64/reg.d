37dffda9ab2479c3eb9a549c2977597e5e81da20
Bi RuiDe <1776427100@qq.com>
2026-01-27 22:03:33 +0800

    合并完成，等待答辩

--- nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.d
+++ nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.d
@@ -1,24 +1,9 @@
-cmd_/home/biruide/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.o := unused
-
-source_/home/biruide/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.o := src/isa/riscv64/reg.c
-
-deps_/home/biruide/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.o := \
-  /home/biruide/ysyx-workbench/nemu/include/isa.h \
-  /home/biruide/ysyx-workbench/nemu/src/isa/riscv64/include/isa-def.h \
-    $(wildcard include/config/rve.h) \
-    $(wildcard include/config/rv64.h) \
-  /home/biruide/ysyx-workbench/nemu/include/common.h \
-    $(wildcard include/config/target/am.h) \
-    $(wildcard include/config/mbase.h) \
-    $(wildcard include/config/msize.h) \
-    $(wildcard include/config/isa64.h) \
-  /home/biruide/ysyx-workbench/nemu/include/macro.h \
-  /home/biruide/ysyx-workbench/nemu/include/debug.h \
-  /home/biruide/ysyx-workbench/nemu/include/utils.h \
-    $(wildcard include/config/target/native/elf.h) \
-  src/isa/riscv64/local-include/reg.h \
-    $(wildcard include/config/rt/check.h) \
-
-/home/biruide/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.o: $(deps_/home/biruide/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.o)
-
-$(deps_/home/biruide/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.o):
+/home/biruide/ysyx-workbench/nemu/build/obj-riscv64-nemu-interpreter/src/isa/riscv64/reg.o: \
+ src/isa/riscv64/reg.c /home/biruide/ysyx-workbench/nemu/include/isa.h \
+ /home/biruide/ysyx-workbench/nemu/src/isa/riscv64/include/isa-def.h \
+ /home/biruide/ysyx-workbench/nemu/include/common.h \
+ /home/biruide/ysyx-workbench/nemu/include/generated/autoconf.h \
+ /home/biruide/ysyx-workbench/nemu/include/macro.h \
+ /home/biruide/ysyx-workbench/nemu/include/debug.h \
+ /home/biruide/ysyx-workbench/nemu/include/utils.h \
+ src/isa/riscv64/local-include/reg.h
