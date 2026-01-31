1a5b960513c9c5550d8a50b42d0d05af962b2273
tracer-ysyx <tracer@ysyx.org>
2026-01-31 09:40:12 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:40:12 up  2:00,  1 user,  load average: 0.22, 0.18, 0.17

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -135,8 +135,7 @@ static bool make_token(char *e) {
             break;
         }
         // printf("%d:%s(%s)\n",nr_token,tokens[nr_token].str,substr_start);
-        Log("match rules[%d] = \"%s\" at %d with len %d: %.*s when %d:%s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start,nr_token,tokens[nr_token].str);
+        // Log("match rules[%d] = \"%s\" at %d with len %d: %.*s when %d:%s",i, rules[i].regex, position, substr_len, substr_len, substr_start,nr_token,tokens[nr_token].str);
         position += substr_len;
         nr_token++;
         break;
