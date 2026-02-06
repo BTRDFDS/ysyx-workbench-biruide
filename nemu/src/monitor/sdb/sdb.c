46f2913484ea37d895043b4dad9cad69f7519bf2
tracer-ysyx <tracer@ysyx.org>
2026-02-06 13:43:51 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:43:51 up  2:26,  1 user,  load average: 0.07, 0.12, 0.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -105,7 +105,7 @@ static int cmd_x(char *args) {
 static int cmd_p(char *args) {
   bool success;
   word_t val=expr(args,&success);
-  if(success==true) printf("%u\n",val);
+  if(success==true) printf("%u or 0x%x\n",val,val);
   return 0;
 }
 
