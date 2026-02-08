e189dc2ad0ed30da506b51f3ed4181806da0b567
tracer-ysyx <tracer@ysyx.org>
2026-02-08 21:10:09 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:10:09 up  9:17,  1 user,  load average: 0.37, 0.17, 0.14

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -20,7 +20,7 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-    printf("%d,nemu_state.halt_ret:%d,nemu_state.state:%d\n",!good,nemu_state.halt_ret,nemu_state.state);
+    // printf("%d,nemu_state.halt_ret:%d,nemu_state.state:%d\n",!good,nemu_state.halt_ret,nemu_state.state);
   return !good;
   // return (good==1)?0:-1;
 }
