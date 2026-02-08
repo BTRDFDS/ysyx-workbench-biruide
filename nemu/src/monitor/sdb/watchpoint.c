af8e2cd08ec4dddd9fbb5afefc43f062752bd08e
tracer-ysyx <tracer@ysyx.org>
2026-02-08 21:40:49 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:40:49 up  9:49,  1 user,  load average: 0.26, 0.18, 0.12

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -75,7 +75,7 @@ int checkWp(){
   while(wp!=NULL&&wp->use==true){
     val=expr(wp->require,&success);
     if(success&&val!=wp->value&&wp->use==true){
-      printf("watchpoint %d : %s form %x to %x\n",wp->NO,wp->require,wp->value,val);
+      printf("pc = %x watchpoint %d : %s form %x to %x\n",cpu.pc,wp->NO,wp->require,wp->value,val);
       wp->value=val;
       return 0;
     }
