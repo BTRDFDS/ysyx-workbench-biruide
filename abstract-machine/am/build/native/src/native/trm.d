aef6a60a75c4c395fe38863e43e587e8878a0c44
tracer-ysyx <tracer@ysyx.org>
2026-02-08 12:33:12 +0800

    > compile NEMU 100019871 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:33:12 up 25 min,  1 user,  load average: 0.25, 0.11, 0.06

--- abstract-machine/am/build/native/src/native/trm.d
+++
@@ -1,6 +0,0 @@
-/home/biruide/ysyx-workbench/abstract-machine/am/build/native/src/native/trm.o: \
- /home/biruide/ysyx-workbench/abstract-machine/am/src/native/trm.c \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/arch/native.h \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/biruide/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
--- abstract-machine/am/build/native/src/native/vme.d
+++
@@ -1,8 +0,0 @@
-/home/biruide/ysyx-workbench/abstract-machine/am/build/native/src/native/vme.o: \
- /home/biruide/ysyx-workbench/abstract-machine/am/src/native/vme.c \
- /home/biruide/ysyx-workbench/abstract-machine/am/src/native/platform.h \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/arch/native.h \
- /home/biruide/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/biruide/ysyx-workbench/abstract-machine/klib/include/klib.h \
- /home/biruide/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
