f611ee0e3e45f4fa05e749f695eb3ceaf94d92a6
Bi RuiDe <1776427100@qq.com>
2026-02-10 17:35:49 +0800

    通过全部测试

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,62 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...) {//DONE:hello-str
+  // panic("Not implemented");
+  //只能用str系列的函数，不能用其他的所有
+  if(out==NULL){panic("out is NULL");}
+  if(fmt==NULL){panic("fmt is NULL");}
+  va_list argp;
+  va_start(argp, fmt);
+  char *start = out;
+  while(*fmt!='\0'){
+    // *out=*fmt;
+    if(*fmt=='%'){
+      fmt++;
+      switch(*fmt){
+        case 'c':
+          *out=va_arg(argp, int);
+          out++;
+          fmt++;
+          break;
+        case 's':
+          char *str=va_arg(argp, char*);
+          for(int i=0;str[i]!='\0'&&i<100;i++){
+            *out=str[i];
+            out++;
+          }
+          fmt++;
+          break;
+        case 'd':
+          fmt++;
+          #define MAX 11
+          int num=va_arg(argp, int);
+          char number[MAX]={0};
+          int point=MAX-1;
+          if(num==0){*out='0';out++;break;}
+          else if(num<0){*out='-';out++;num=-num;}
+          while(num>0&&point>= 0){
+              number[point]=(num % 10)+'0';
+              num/= 10;
+              point--;
+          }
+          for(int i=point+1;i<MAX;i++){
+              if(number[i]!='\0'){
+                *out=number[i];
+                out++;
+              }
+          }
+          break;
+        default :panic("sprintf error");break;
+      }
+    }else{
+      *out=*fmt;
+      out++;
+      fmt++;
+    }
+  }
+  *out='\0';
+  return out-start;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -8,28 +8,65 @@ size_t strlen(const char *s) {
   panic("Not implemented");
 }
 
-char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcpy(char *dst, const char *src) {//TODO:string
+  // panic("Not implemented");
+  char *out;
+  if(dst==NULL){panic("error:dst is NULL");}
+  if(src==NULL){panic("error:src is NULL");}
+  for(out=dst;*src!='\0';src++){
+    *dst=*src;
+    dst++;
+  }
+  *dst='\0';
+  return out;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
   panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+char *strcat(char *dst, const char *src) {//TODO:string
+  // panic("Not implemented");
+  if(dst==NULL){panic("error:dst is NULL");}
+  if(src==NULL){panic("error:src is NULL");}
+  char *out;
+  for(out=dst;*dst!='\0';dst++);
+  for(;*src!='\0';src++){
+    *dst=*src;
+    dst++;
+  }
+  *dst='\0';
+  return out;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+int strcmp(const char *s1, const char *s2) {//TODO:string
+  // panic("Not implemented");
+  if(s1==NULL){panic("error:s1 is NULL");}
+  if(s2==NULL){panic("error:s2 is NULL");}
+  // while(*s1!='\0'||*s2!='\0'){
+  //   if(*s1=='\0'&&*s2!='\0'){return -1;}
+  //   else if(*s1!='\0'&&*s2=='\0'){return 1;}
+  //   else if(*s1<*s2){return -1;}
+  //   else if(*s1>*s2){return 1;}
+  //   s1++;
+  //   s2++;
+  // }
+  // return 0;
+  for(;*s1!='\0'&&*s2!='\0'&&*s1==*s2;s1++,s2++);
+  return *s1-*s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
   panic("Not implemented");
 }
 
-void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+void *memset(void *s, int c, size_t n) {//TODO:string
+  // panic("Not implemented");
+  if(s==NULL){panic("error:s is NULL");}
+  for(size_t i=0;i<n;i++){
+    ((char*)s)[i]=(char)c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -40,8 +77,16 @@ void *memcpy(void *out, const void *in, size_t n) {
   panic("Not implemented");
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+int memcmp(const void *s1, const void *s2, size_t n) {//TODO:string
+  // panic("Not implemented");
+  if(s1==NULL){panic("error:s1 is NULL");}
+  if(s2==NULL){panic("error:s2 is NULL");}
+  for (size_t i = 0; i < n; i++) {
+    if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i]) {
+      return ((unsigned char *)s1)[i] - ((unsigned char *)s2)[i];
+    }
+  }
+  return 0;
 }
 
 #endif
