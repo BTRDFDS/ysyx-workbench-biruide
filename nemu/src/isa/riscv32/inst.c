b942b40d8e071202539519f7d807d8eacfddfab4
tracer-ysyx <tracer@ysyx.org>
2026-02-13 14:24:14 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:24:14 up  6:41,  1 user,  load average: 0.07, 0.08, 0.11

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -40,7 +40,8 @@ enum {
 int32_t riscv32mDiv(int32_t rs1,int32_t rs2){
   if(rs2==0){
     printf("div 0\n");
-    exit(-1);
+    // exit(-1);
+    return 0;
   }
   else if(rs1==INT32_MIN&&rs2==-1){return INT32_MIN;}
   else return rs1/rs2;
