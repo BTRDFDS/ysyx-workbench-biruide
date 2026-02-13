00473962b7da6f40fdb82d8d7e7c4a843715df47
tracer-ysyx <tracer@ysyx.org>
2026-02-13 21:02:12 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:02:12 up  2:37,  1 user,  load average: 0.06, 0.06, 0.07

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -29,9 +29,9 @@ image: image-dep
 run: insert-arg
 ifeq ($(TEST_AM),1)
 	echo "TEST_AM is defined, running with test_am"
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CFLAGS_BUILD+=-DTEST_AM CFLAGS_BUILD+=-DITRACE_COND=1
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CFLAGS_BUILD+=-DTEST_AM CFLAGS_BUILD+=-DITRACE_COND
 else
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CFLAGS_BUILD+=-DITRACE_COND=1
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CFLAGS_BUILD+=-DITRACE_COND
 endif
 
 gdb: insert-arg
