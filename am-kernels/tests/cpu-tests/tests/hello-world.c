3eedf5a543bac2809ceab40afc9cbd2e374c1a15
tracer-ysyx <tracer@ysyx.org>
2026-02-14 20:42:19 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:42:19 up 12:53,  1 user,  load average: 0.22, 0.23, 0.15

--- am-kernels/tests/cpu-tests/tests/hello-world.c
+++ am-kernels/tests/cpu-tests/tests/hello-world.c
@@ -1,7 +1,5 @@
 #include "trap.h"
 int main() {
-    char buf[128];
-	sprintf(buf, "%s", "Hello world!\n");
-	check(strcmp(buf, "Hello world!\n") == 0);
+    printf("Hello, World!\n");
 	return 0;
 }
