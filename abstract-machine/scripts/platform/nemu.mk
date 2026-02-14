7936a6dbee86000e6cfd833e74b2a9fa1e0cb1df
tracer-ysyx <tracer@ysyx.org>
2026-02-14 19:00:05 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:00:05 up 11:04,  1 user,  load average: 0.16, 0.15, 0.11

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -36,5 +36,12 @@ endif
 
 gdb: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+val: insert-arg
+ifeq ($(TEST_AM),1)
+	echo "TEST_AM is defined, running with test_am"
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) val ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CFLAGS_BUILD+="-DAUTO_RUN"
+else
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) val ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+endif
 
 .PHONY: insert-arg
