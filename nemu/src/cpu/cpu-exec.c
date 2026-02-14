9df2b9b2c5273f084ec68ddab924cb152d7740ef
tracer-ysyx <tracer@ysyx.org>
2026-02-14 13:46:27 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:46:27 up  5:31,  1 user,  load average: 0.21, 0.12, 0.10

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -55,7 +55,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
       // printf("%s\n",_this->logbuf);
       //   fprintf(log_iringbuf_fp,"%s\n",_this->logbuf);
       //   fflush(log_iringbuf_fp);
-    for(int i=0;minCount?i<iringbufCount:i<iringmax;i++){
+    for(int i=0;minCount?i<iringmax:i<iringbufCount;i++){
       if(i==iringbufCount-1){
         // printf("--> %s\n",iringbufChar[i]);
         fprintf(log_iringbuf_fp,"-->%s",iringbufChar[i]);
