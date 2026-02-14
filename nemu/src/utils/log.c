813c0cd9b79c56c44cebb2ddcd490f8a1ff647d1
tracer-ysyx <tracer@ysyx.org>
2026-02-14 16:16:05 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:16:05 up  8:11,  1 user,  load average: 0.27, 0.22, 0.19

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -73,21 +73,24 @@ bool log_enable() {
 
 void closeLog(){
   if(log_fp!=NULL&&log_fp!=stdout){
-    if(log_file_printf!=NULL){Log("Log is written to %s",log_file_printf);free(log_file_printf);}
+    if(log_file_printf!=NULL){Log("Log is written to %s",log_file_printf);}
     fclose(log_fp);
   }
+  if(log_file_printf!=NULL){free(log_file_printf);}
   if(log_iringbuf_fp!=NULL&&log_iringbuf_fp!=stdout){
     // Log("Log of iringbuf is written to %s",log_iringbuf_file);
-    if(log_iringbuf_file!=NULL){Log("Log of iringbuf is written to %s",log_iringbuf_file);free(log_iringbuf_file);}
+    if(log_iringbuf_file!=NULL){Log("Log of iringbuf is written to %s",log_iringbuf_file);}
     fclose(log_iringbuf_fp);
   }
+  if(log_iringbuf_file!=NULL){free(log_iringbuf_file);}
 
 #ifdef CONFIG_MTRACE
   if(log_mtraece_fp!=NULL&&log_mtraece_fp!=stdout){
-    if(log_mtrace_file!=NULL){Log("Log of mtrace is written to %s",log_mtrace_file);free(log_mtrace_file);}
+    if(log_mtrace_file!=NULL){Log("Log of mtrace is written to %s",log_mtrace_file);}
     // Log("Log of mtrace is written to %s",log_mtrace_file);
     fclose(log_mtraece_fp);
   }
+  if(log_mtrace_file!=NULL){free(log_mtrace_file);}
 #endif
 }
 #endif
