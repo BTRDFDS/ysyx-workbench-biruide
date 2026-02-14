8ee056f03e6c12dc6155a0e36f046dd5a7a807bb
tracer-ysyx <tracer@ysyx.org>
2026-02-14 13:01:48 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:01:48 up  4:44,  1 user,  load average: 0.24, 0.24, 0.16

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,14 +41,16 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 #ifndef CONFIG_TARGET_AM
-extern FILE *log_iringbuf_fp;
+  extern FILE *log_iringbuf_fp;
   #define iringmax 16
   static int iringbufCount=0;
   static char iringbufChar[iringmax][128];
+  static bool minCount=0;
   if(MUXDEF(CONFIG_TRACE,(g_nr_guest_inst>=CONFIG_TRACE_START)&&(g_nr_guest_inst<=CONFIG_TRACE_END),true)&&log_iringbuf_fp!=NULL){
     //先清空
     memset(iringbufChar[iringbufCount],0,sizeof(iringbufChar[iringbufCount]));
     strncpy(iringbufChar[iringbufCount],_this->logbuf,sizeof(iringbufChar[iringbufCount]));
+    if(iringbufCount==iringmax-1&&minCount==0){minCount=1;}
     iringbufCount=(iringbufCount+1==iringbufCount)?0:iringbufCount+1;
       // printf("%s\n",_this->logbuf);
       //   fprintf(log_iringbuf_fp,"%s\n",_this->logbuf);
