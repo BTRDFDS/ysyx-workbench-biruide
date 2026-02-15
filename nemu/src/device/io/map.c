521a70a3d0d9fd906ffdcb712d4f93adb08a65f6
tracer-ysyx <tracer@ysyx.org>
2026-02-15 17:50:12 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:50:12 up 10:03,  1 user,  load average: 0.60, 0.49, 0.37

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -57,20 +57,24 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
 #ifdef CONFIG_DTRACE
-    fprintf(log_dtrace_fp,"%s x%x(%x\n",map->name,addr,len);
+    fprintf(log_dtrace_fp,"%s x%x(%x)",map->name,addr,len);
 #endif
   assert(len >= 1 && len <= 8);
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+
+#ifdef CONFIG_DTRACE
+    fprintf(log_dtrace_fp,"=%x\n",ret);
+#endif
   return ret;
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
 
 #ifdef CONFIG_DTRACE
-    fprintf(log_dtrace_fp,"%s x%x(%x)<=%x\n",map->name,addr,len,data);
+    fprintf(log_dtrace_fp,"%s x%x(%x)<=%x ",map->name,addr,len,data);
 #endif
 
   assert(len >= 1 && len <= 8);
@@ -78,4 +82,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
+
+#ifdef CONFIG_DTRACE
+    fprintf(log_dtrace_fp,"s \n");
+#endif
 }
