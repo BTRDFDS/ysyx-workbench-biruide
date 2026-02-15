92e01842310099b030869c54cb8ca5f5590c3979
tracer-ysyx <tracer@ysyx.org>
2026-02-15 15:29:18 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:29:18 up  7:38,  1 user,  load average: 0.82, 2.58, 2.66

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -42,7 +42,8 @@ void *malloc(size_t size) {
     addr=(void *)ROUNDUP(heap.start, 8);
     addrInit = true;
   }
-  size=(size_t)ROUNDUP(size==0?1:size,8); 
+  // size=(size_t)ROUNDUP(size==0?1:size,8); 
+  size=(size_t)ROUNDUP(size,8); 
   char *now = addr;
   addr += size;
   assert((uintptr_t)heap.start <= (uintptr_t)addr && (uintptr_t)addr < (uintptr_t)heap.end);
