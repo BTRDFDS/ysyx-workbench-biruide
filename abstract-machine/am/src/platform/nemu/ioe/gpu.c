06183f0fced282991a66a5e56048cd995d9bb413
tracer-ysyx <tracer@ysyx.org>
2026-02-18 22:16:15 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:16:15 up  3:32,  1 user,  load average: 0.21, 0.23, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -32,10 +32,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+    printf("x:%d,y:%d,w:%d,h:%d %d\n",ctl->x,ctl->y,ctl->w,ctl->h,ctl->sync);
   }else{
     outl(SYNC_ADDR, 0);
   }
-  printf("x:%d,y:%d,w:%d,h:%d %d\n",ctl->x,ctl->y,ctl->w,ctl->h,ctl->sync);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *pixels=ctl->pixels;
   for(uint32_t i=0;i<ctl->h;i++){
