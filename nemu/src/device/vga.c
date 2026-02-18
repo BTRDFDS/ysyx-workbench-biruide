64526c7a03d79a66ddd837af7e4350deb9b8826e
tracer-ysyx <tracer@ysyx.org>
2026-02-18 22:46:37 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:46:37 up  4:04,  1 user,  load average: 0.41, 0.34, 0.31

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -55,6 +55,7 @@ static void init_screen() {
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
   SDL_RenderPresent(renderer);
+    printf("finish init screen\n");
 }
 
 static inline void update_screen() {
