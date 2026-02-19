1fe9e5c34f31c03df75f74c9bd37d4afbfabcda8
tracer-ysyx <tracer@ysyx.org>
2026-02-19 20:06:56 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:06:56 up 13:08,  1 user,  load average: 0.06, 0.12, 0.17

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -158,6 +158,10 @@ void vga_update_screen() {
 //   printf("use CONFIG_TARGET_AM\n");
 // #endif
   }else{
+    const char* sdl_error = SDL_GetError();
+    if (sdl_error && sdl_error[0] != '\0') {
+      printf("[VGA] SDL error: %s\n", sdl_error);
+    }
     update_screen();
   }
 }
