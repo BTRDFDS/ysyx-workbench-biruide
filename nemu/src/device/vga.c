523ff1635d359bd6984ee5d90970d8a1d13978cd
tracer-ysyx <tracer@ysyx.org>
2026-02-19 18:09:56 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:09:56 up 11:02,  1 user,  load average: 0.67, 0.42, 0.49

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -47,7 +47,7 @@ static void init_screen() {
   char title[128];
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
   // SDL_Init(SDL_INIT_VIDEO);
-  if(SDL_Init(SDL_INIT_VIDEO) != 0) {printf("ERROR: SDL_Init failed\n");exit(-1);}
+  if(SDL_Init(SDL_INIT_VIDEO) != 0) {printf("ERROR: SDL_Init failed\n");exit(-1);}else{printf("SDL_Init success\n");}
   SDL_CreateWindowAndRenderer(
       screen_width() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
       screen_height() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
