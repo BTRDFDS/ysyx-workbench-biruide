0f7ebd1058a550179b2f34b82068a988dd03cb48
tracer-ysyx <tracer@ysyx.org>
2026-02-19 10:32:46 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:32:46 up  2:56,  1 user,  load average: 0.52, 0.46, 0.47

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -78,7 +78,20 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
     fprintf(log_dtrace_fp,"map %s x%x(%x)<=%x",map->name,addr,len,data);
     void *callstack[64];
     int frames = backtrace(callstack, 64);
-    backtrace_symbols_fd(callstack, frames, fileno(log_dtrace_fp));
+    char **symbols = backtrace_symbols(callstack, frames);
+    for (int i = 0; i < frames; i++) {
+        char *func_name = symbols[i];
+        // 查找函数名（最后一个括号内的内容）
+        char *start = strchr(func_name, '(');
+        if (start) {
+            char *end = strchr(start, '+');
+            if (end) {
+                *end = '\0'; // 截断字符串
+                fprintf(log_dtrace_fp, "#%d %s\n", i, start + 1);
+            }
+        }
+    }
+    free(symbols);
 #endif
 
   assert(len >= 1 && len <= 8);
