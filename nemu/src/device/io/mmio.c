4d95de6547a9b9554dcbe8751b517538351efbc6
tracer-ysyx <tracer@ysyx.org>
2026-02-19 10:11:56 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:11:56 up  2:34,  1 user,  load average: 1.50, 0.88, 0.46

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -59,12 +59,12 @@ void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_
 /* bus interface */
 word_t mmio_read(paddr_t addr, int len) {
 #ifdef CONFIG_DTRACE
-    fprintf(log_dtrace_fp,"mmio x%x(%x)",addr,len);
+    fprintf(log_dtrace_fp,"mmio x%x(%x) ",addr,len);
 #endif
   word_t data = map_read(addr, len, fetch_mmio_map(addr));
-#ifdef CONFIG_DTRACE
-    fprintf(log_dtrace_fp,"==%x\n",data);
-#endif
+// #ifdef CONFIG_DTRACE
+//     fprintf(log_dtrace_fp,"==%x\n",data);
+// #endif
   return data;
 }
 
