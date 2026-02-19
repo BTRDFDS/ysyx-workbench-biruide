1f032a6c9885bb4c1e9f9f001a869a8469233cfd
tracer-ysyx <tracer@ysyx.org>
2026-02-19 08:03:04 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:03:04 up 18 min,  1 user,  load average: 0.82, 0.53, 0.43

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -48,19 +48,19 @@ static void init_screen() {
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
   SDL_Init(SDL_INIT_VIDEO);
   SDL_CreateWindowAndRenderer(
-      SCREEN_W * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
-      SCREEN_H * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
+      screen_width() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
+      screen_height() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
       0, &window, &renderer);
   SDL_SetWindowTitle(window, title);
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
-      SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
+      SDL_TEXTUREACCESS_STATIC, screen_width(), screen_height());
   SDL_RenderPresent(renderer);
     printf("finish init screen\n");
 }
 
 static inline void update_screen() {
   printf("update screen begin\n");
-  SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
+  SDL_UpdateTexture(texture, NULL, vmem, screen_width() * sizeof(uint32_t));
   SDL_RenderClear(renderer);
   SDL_RenderCopy(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
