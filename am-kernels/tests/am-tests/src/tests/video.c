264a9a75d9672b2e2b46d085f70fdaa8c70a7daf
tracer-ysyx <tracer@ysyx.org>
2026-02-19 11:14:59 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:14:59 up  3:40,  1 user,  load average: 0.68, 0.48, 0.40

--- am-kernels/tests/am-tests/src/tests/video.c
+++ am-kernels/tests/am-tests/src/tests/video.c
@@ -59,11 +59,11 @@ void update() {
       if (x1 >= 0 && x1 < N && y1 >= 0 && y1 < N && !used[x1][y1]) {
         x = x1; y = y1;
         used[x][y] = 1;
-        canvas[x][y] = p(init + step / 2);return;
+        canvas[x][y] = p(init + step / 2);
         break;
       }
       d = (d + 1) % 4;
-    }
+    }return;
   }
 }
 
