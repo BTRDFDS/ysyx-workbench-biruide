f4d61795c26920ab9c24c520941477aa169e8de9
tracer-ysyx <tracer@ysyx.org>
2026-02-19 22:42:51 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:42:51 up  1:29,  1 user,  load average: 0.12, 0.09, 0.15

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -57,13 +57,22 @@ static void init_screen() {
       SDL_ClearError();
     }
   
-  if (SDL_CreateWindowAndRenderer(
-      screen_width() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
-      screen_height() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
-      SDL_WINDOW_SHOWN, &window, &renderer) != 0) {
-    printf("SDL_CreateWindowAndRenderer failed: %s\n", SDL_GetError());
-    return;
-  }
+  // if (SDL_CreateWindowAndRenderer(
+  //     screen_width() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
+  //     screen_height() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
+  //     SDL_WINDOW_SHOWN, &window, &renderer) != 0) {
+  //   printf("SDL_CreateWindowAndRenderer failed: %s\n", SDL_GetError());
+  //   return;
+  // }
+
+    // 先手动创建窗口
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              screen_width() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
+                              screen_height() * (MUXDEF(CONFIG_VGA_SIZE_400x300, 2, 1)),
+                              SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    // 【核心修改】强制创建软件渲染器，彻底解决WSL2硬件渲染不兼容
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
     const char* sdl_error2 = SDL_GetError();
     if (sdl_error2 && sdl_error2[0] != '\0') {
       printf("2[VGA] SDL error: %s\n", sdl_error2);
