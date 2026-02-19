5fc64e2e3e0e9b3ebf859c2bc2809b7af91d779f
tracer-ysyx <tracer@ysyx.org>
2026-02-19 09:25:03 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:25:03 up  1:44,  1 user,  load average: 0.28, 0.18, 0.12

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -67,9 +67,15 @@ static inline void update_screen() {
   if (SDL_UpdateTexture(texture, NULL, vmem, screen_width() * sizeof(uint32_t)) != 0) {
     printf("ERROR: SDL_UpdateTexture failed: %s\n", SDL_GetError());
   }
+  if( SDL_RenderClear(renderer) != 0) {
+    printf("ERROR: SDL_RenderClear failed: %s\n", SDL_GetError());
+  }
+  if (SDL_RenderCopy(renderer, texture, NULL, NULL) != 0) {
+    printf("ERROR: SDL_RenderCopy failed: %s\n", SDL_GetError());
+  }
   // SDL_UpdateTexture(texture, NULL, vmem, screen_width() * sizeof(uint32_t));
-  SDL_RenderClear(renderer);
-  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  // SDL_RenderClear(renderer);
+  // SDL_RenderCopy(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
   printf("update_screen finish: pitch=%ld\n", screen_width() * sizeof(uint32_t));
 }
