6f2928ab238919f1fc9d443ff001aa9da4cfa832
tracer-ysyx <tracer@ysyx.org>
2026-02-19 18:53:35 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:53:35 up 11:49,  1 user,  load average: 1.35, 1.01, 0.74

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -110,8 +110,16 @@ static inline void update_screen() {
   // SDL_UpdateTexture(texture, NULL, vmem, screen_width() * sizeof(uint32_t));
   // SDL_RenderClear(renderer);
   // SDL_RenderCopy(renderer, texture, NULL, NULL);
+    const char* sdl_error1 = SDL_GetError();
+    if (sdl_error1 && sdl_error1[0] != '\0') {
+      printf("1[VGA] SDL error: %s\n", sdl_error1);
+    }
   SDL_RenderPresent(renderer);
   printf("update_screen finish: pitch=%ld\n", screen_width() * sizeof(uint32_t));
+    const char* sdl_error2 = SDL_GetError();
+    if (sdl_error2 && sdl_error2[0] != '\0') {
+      printf("2[VGA] SDL error: %s\n", sdl_error2);
+    }
 }
 #else
 static void init_screen() {}
