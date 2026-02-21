3ef64544a2e9b99e912d00684ac39c034f8534ff
tracer-ysyx <tracer@ysyx.org>
2026-02-21 20:33:26 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:33:26 up 13:03,  1 user,  load average: 0.76, 0.69, 0.38

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -46,7 +46,7 @@ void init_log(const char *log_file) {
     Assert(fp, "Can not open '%s'", log_file);
     log_fp = fp;
 
-#ifdef IRINGTRACE
+#ifdef CONFIG_IRINGTRACE
     log_iringbuf_file = malloc(strlen(log_file) + strlen("_iringbuf.txt") + 1);
     strcpy(log_iringbuf_file, log_file);
     strcat(log_iringbuf_file, "_iringbuf.txt");
