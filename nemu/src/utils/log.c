8db8b00f262c8f7ad153bad92329f106adaf343a
tracer-ysyx <tracer@ysyx.org>
2026-02-21 21:38:20 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:38:20 up 14:13,  1 user,  load average: 0.18, 0.27, 0.18

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -95,8 +95,7 @@ void init_log(const char *log_file) {
 
   }
   Log("Log is written to %s", log_file_printf ? log_file_printf : "stdout");
-  free(log_file_copy);
-  // if( log_file_copy!=NULL){free(log_file_copy);}
+  if( log_file_copy!=NULL){free(log_file_copy);}
 }
 
 bool log_enable() {
