a2cdfd426911c72f31d6b198747649c61a68ed2e
tracer-ysyx <tracer@ysyx.org>
2026-03-03 20:20:43 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:20:43 up 10:17,  1 user,  load average: 0.21, 0.31, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -15,10 +15,10 @@ Context* __am_irq_handle(Context *c) {
 
     c = user_handler(ev, c);
     assert(c != NULL);
-    printf("mcause 0x%x\n",c->mcause);
-    printf("mstatus 0x%x\n",c->mstatus);
-    printf("mepc 0x%x\n",c->mepc);
-    printf("s\n");
+    // printf("mcause 0x%x\n",c->mcause);
+    // printf("mstatus 0x%x\n",c->mstatus);
+    // printf("mepc 0x%x\n",c->mepc);
+    // printf("s\n");
   }
   return c;
 }
