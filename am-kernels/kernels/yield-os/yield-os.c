e5a30207be44620a4ae29fc9d6964d728918413c
tracer-ysyx <tracer@ysyx.org>
2026-03-03 21:54:41 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:54:41 up 11:56,  1 user,  load average: 0.10, 0.20, 0.08

--- am-kernels/kernels/yield-os/yield-os.c
+++ am-kernels/kernels/yield-os/yield-os.c
@@ -14,9 +14,9 @@ static void f(void *arg) {
     // putch('1');
     putch("?AB"[(uintptr_t)arg > 2 ? 0 : (uintptr_t)arg]);
     for (int volatile i = 0; i < 100000; i++) ;
-    putch('2');
     yield();
   }
+  putch('2');
 }
 
 static Context *schedule(Event ev, Context *prev) {
