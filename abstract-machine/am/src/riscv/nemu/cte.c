c66338800da985315a85b37e57e55d3d201f09e3
tracer-ysyx <tracer@ysyx.org>
2026-03-03 13:01:29 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:01:29 up  2:35,  1 user,  load average: 0.03, 0.11, 0.04

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -30,10 +30,21 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *c = (Context *)((uintptr_t)kstack.end - sizeof(Context));
+
+uintptr_t stack_top = (uintptr_t)kstack.end;
+stack_top &= ~0xF;
+  Context *c = (Context *)(stack_top - sizeof(Context));
+if ((void *)c < kstack.start) {
+    return NULL; // 栈空间不足，返回空指针
+}
   memset(c, 0, sizeof(Context));
+  c->gpr[1] = 0;
+  c->gpr[2] = stack_top;
   c->gpr[10] = (uintptr_t)arg;
   c->mepc = (uintptr_t)entry;
+  c->mcause  = 0;
+  c->mstatus = 0x1800;
+  c->pdir = NULL;
   printf("21\n");
   return c;
   // return NULL;
