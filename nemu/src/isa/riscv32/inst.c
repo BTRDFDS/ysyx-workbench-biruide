fb8be91fde754f31812d89ce597734a4943e1f93
tracer-ysyx <tracer@ysyx.org>
2026-03-03 16:43:01 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:43:01 up  6:28,  1 user,  load average: 0.14, 0.25, 0.24

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -154,7 +154,7 @@ word_t riscv32mret(){
 word_t riscv32ecall(word_t pc){
   mepc=pc;
   mcause=11;
-  // printf("ecall@0x%x to 0x%x\n",pc,mtvec);
+  printf("ecall@0x%x to 0x%x\n",pc,mtvec);
   return mtvec;
 }
 
