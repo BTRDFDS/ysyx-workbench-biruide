d8d81d71b23c0786570f22c4f02c7fa5a8556ca3
tracer-ysyx <tracer@ysyx.org>
2026-03-03 13:25:16 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:25:16 up  3:00,  1 user,  load average: 0.25, 0.16, 0.11

--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -73,6 +73,7 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   // assert(count == 1);
   if(count!=1){
     printf("count=%ld\n",count);
+    printf("Disasm failed at pc = 0x%lx, nbyte = %d, code = %02x %02x %02x %02x\n", pc, nbyte, code[0], code[1], code[2], code[3]);
     assert(count == 1);
   }
   int ret = snprintf(str, size, "%s", insn->mnemonic);
