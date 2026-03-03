c1c8c004c7c117b6cd19a1feee1ebbd848841cde
tracer-ysyx <tracer@ysyx.org>
2026-03-03 22:55:57 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:55:57 up 13:00,  1 user,  load average: 0.24, 0.26, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -51,8 +51,8 @@ if ((void *)c < kstack.start) {
   c->mepc = (uintptr_t)entry;
   c->mcause  = 0;
   c->mstatus = 0x1800;
-  // c->pdir = NULL;
-  printf("%d\n",sizeof(Context));
+  c->pdir = NULL;
+  // printf("%d\n",sizeof(Context));
     // printf("mcause 0x%x\n",c->mcause);
     // printf("mstatus 0x%x\n",c->mstatus);
     // printf("mepc 0x%x\n",c->mepc);
