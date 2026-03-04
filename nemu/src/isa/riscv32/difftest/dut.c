c0f9ecf74ee96652b7b1cea83239a0d027e01ab9
tracer-ysyx <tracer@ysyx.org>
2026-03-04 20:01:41 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:01:41 up  7:12,  1 user,  load average: 0.30, 0.19, 0.07

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,7 +21,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {//TODO
   extern CPU_state cpu;
   for(int i = 0; i < 32; i++) {
     if (ref_r->gpr[i] != cpu.gpr[i]) {
-      printf("reg %d: %d != %x at pc=0x%x\n", i, ref_r->gpr[i], cpu.gpr[i], pc);
+      printf("reg %d: (ref)%x != (dut)%x at pc=0x%x\n", i, ref_r->gpr[i], cpu.gpr[i], pc);
       return false;
     }
   }
