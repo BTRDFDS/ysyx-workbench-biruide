3477626a55de14c9e4a68b061ab025df7ddcbf79
tracer-ysyx <tracer@ysyx.org>
2026-03-07 16:30:02 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:30:02 up  8:47,  1 user,  load average: 1.55, 1.12, 0.60

--- am-kernels/tests/am-tests/src/tests/intr.c
+++ am-kernels/tests/am-tests/src/tests/intr.c
@@ -20,7 +20,7 @@ void hello_intr() {
   io_read(AM_INPUT_CONFIG);
   iset(1);
   while (1) {
-    for (volatile int i = 0; i < 10000000; i++) ;//10000000
+    for (volatile int i = 0; i < 10000; i++) ;//10000000
     // printf("into yield\n");
     putch('\n');
     yield();
