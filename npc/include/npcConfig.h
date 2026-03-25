7bae8487b3d729d7d95e3c44c77642f55963db27
Bi RuiDe <1776427100@qq.com>
2026-03-25 22:55:49 +0800

    校验完成

--- npc/include/npcConfig.h
+++ npc/include/npcConfig.h
@@ -1,8 +1,8 @@
 #ifndef _NPC_CONFIG_
 #define _NPC_CONFIG_
 
-#define NPC_SDB
-#define NPC_DIFFTEST
+// #define NPC_SDB
+// #define NPC_DIFFTEST
 // #define NPC_I_TRACE
 // #define NPC_M_TRACE
 // #define NPC_F_TRACE
