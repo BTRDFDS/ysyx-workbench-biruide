0b6faa49dc78c6193573575e7e69edd65b7f4bfa
Bi RuiDe <1776427100@qq.com>
2026-05-03 22:58:29 +0800

    小修改来分析错误

--- am-kernels/tests/klib-tests/src/chat.c
+++ am-kernels/tests/klib-tests/src/chat.c
@@ -12,6 +12,6 @@ void _start() {
   // *(volatile char *)(UART_BASE + UART_TX) = 'B';
   // *(volatile char *)(UART_BASE + UART_TX) = 'C';
 //   *(volatile char *)(UART_BASE + UART_TX) = '\n';
-  // asm("ebreak");
-  while (1);
+  asm("ebreak");
+  // while (1);
 }
