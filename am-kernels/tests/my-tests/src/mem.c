30e02aa9d9dd613a3cd9905306fe5c5e4acb05e0
Bi RuiDe <1776427100@qq.com>
2026-05-13 22:38:35 +0800

    三个一起就会有问题

--- am-kernels/tests/my-tests/src/mem.c
+++ am-kernels/tests/my-tests/src/mem.c
@@ -3,10 +3,13 @@
 
 #include <limits.h>
 int main() {
-	uint8_t *start =(uint8_t *)0x0f000000;
-	// uint8_t *end   =(uint8_t *)0x0f000004;
-	uint8_t *end   =(uint8_t *)0x0f001fff;
-	for(uint8_t *p=start;p<=end;p++)*p=(uint8_t)((uintptr_t)p & 0xFF);
-	for(uint8_t *p=start;p<=end;p++)if(*p != (uint8_t)((uintptr_t)p & 0xFF))panic("mem error\n");
+	uintptr_t start =0x0f000000;
+	uintptr_t end   =0x0f001fff;
+	for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)*p=(uint8_t)((uintptr_t)p&0xFF);
+	for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)if(*p != (uint8_t)((uintptr_t)p&0xFF))panic("8\n");
+	for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)*p=(uint16_t)((uintptr_t)p&0xFFFF);
+	for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)if(*p != (uint16_t)((uintptr_t)p&0xFFFF))panic("16\n");
+	for(uint32_t *p=(uint32_t*)start;p<=(uint32_t*)end;p++)*p=(uint32_t)((uintptr_t)p);
+	for(uint32_t *p=(uint32_t*)start;p<=(uint32_t*)end;p++)if(*p != (uint32_t)((uintptr_t)p))panic("32\n");
 	return 0;
 }
