2bbacf1503377281e5700415670bd61f593418e1
Bi RuiDe <1776427100@qq.com>
2026-05-13 22:46:31 +0800

    好了。原本用了panic导致貌似超过ROM空间了

--- am-kernels/tests/my-tests/src/mem.c
+++ am-kernels/tests/my-tests/src/mem.c
@@ -6,10 +6,10 @@ int main() {
 	uintptr_t start =0x0f000000;
 	uintptr_t end   =0x0f001fff;
 	for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)*p=(uint8_t)((uintptr_t)p&0xFF);
-	for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)if(*p != (uint8_t)((uintptr_t)p&0xFF))panic("8\n");
+	for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)if(*p != (uint8_t)((uintptr_t)p&0xFF))return -1;
 	for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)*p=(uint16_t)((uintptr_t)p&0xFFFF);
-	for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)if(*p != (uint16_t)((uintptr_t)p&0xFFFF))panic("16\n");
+	for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)if(*p != (uint16_t)((uintptr_t)p&0xFFFF))return -2;
 	for(uint32_t *p=(uint32_t*)start;p<=(uint32_t*)end;p++)*p=(uint32_t)((uintptr_t)p);
-	for(uint32_t *p=(uint32_t*)start;p<=(uint32_t*)end;p++)if(*p != (uint32_t)((uintptr_t)p))panic("32\n");
+	for(uint32_t *p=(uint32_t*)start;p<=(uint32_t*)end;p++)if(*p != (uint32_t)((uintptr_t)p))return -3;
 	return 0;
 }
