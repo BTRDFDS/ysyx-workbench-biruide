bfbb1633219b06b5a459dde05e1da540e44154a1
Bi RuiDe <1776427100@qq.com>
2026-05-13 22:27:39 +0800

    8位测试完成。检查出来的小bug是nemu卡了一位

--- am-kernels/tests/my-tests/src/mem.c
+++ am-kernels/tests/my-tests/src/mem.c
@@ -6,7 +6,7 @@ int main() {
 	uint8_t *start =(uint8_t *)0x0f000000;
 	// uint8_t *end   =(uint8_t *)0x0f000004;
 	uint8_t *end   =(uint8_t *)0x0f001fff;
-	for(uint8_t *p=start;p<end;p++)*p=(uint8_t)((uintptr_t)p & 0xFF);
-	for(uint8_t *p=start;p<end;p++)if(*p != (uint8_t)((uintptr_t)p & 0xFF))panic("mem error\n");
+	for(uint8_t *p=start;p<=end;p++)*p=(uint8_t)((uintptr_t)p & 0xFF);
+	for(uint8_t *p=start;p<=end;p++)if(*p != (uint8_t)((uintptr_t)p & 0xFF))panic("mem error\n");
 	return 0;
 }
