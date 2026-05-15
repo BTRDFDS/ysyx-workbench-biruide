29e33652214416e87d97d7751ac0b12a6a83eee4
Bi RuiDe <1776427100@qq.com>
2026-05-15 14:12:03 +0800

    先只留下data

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -6,15 +6,8 @@ static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); /
 void putch(char ch) {asm volatile("sb %0, 0(%1)" : : "r"(ch), "r"(0x10000000));}
 void halt(int code) {asm volatile("mv a0, %0; ebreak" : :"r"(code));while (1);}
 int main(const char *args);
-extern char* _data_start_;
-extern char _data_size_;
-extern char* _data_begin_;
-extern char* _bss_start_;
-extern char _bss_size_;
+extern char _data_start_,_data_size_,_data_begin_;
 void _trm_init() {
-    if(_data_size_!=0&&_data_start_!=0)
-        for(size_t i=0;i<_data_size_;i++){_data_begin_[i]=_data_start_[i];}
-    if(_bss_size_ !=0&&_bss_start_!=0 )
-        for(size_t i=0;i<_bss_size_;i++){_bss_start_[i]=0;}
+    for(size_t i=0;i<(size_t)&_data_size_;i++){((char *)&_data_start_)[i]=((char *)&_data_begin_)[i];}
     halt(main(mainargs));
 }
