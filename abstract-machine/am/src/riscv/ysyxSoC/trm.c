e269a800711604bbec6b00ede79e55c0a3b55ea7
tracer-ysyx <tracer@ysyx.org>
2026-05-15 16:10:31 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:10:31 up  7:34,  1 user,  load average: 0.21, 0.35, 0.26

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -7,7 +7,19 @@ void putch(char ch) {asm volatile("sb %0, 0(%1)" : : "r"(ch), "r"(0x10000000));}
 void halt(int code) {asm volatile("mv a0, %0; ebreak" : :"r"(code));while (1);}
 int main(const char *args);
 extern char _data_start_,_data_size_,_data_begin_;
+extern char _bss_start_,_bss_size_;
 void _trm_init() {
-    for(size_t i=0;i<(size_t)&_data_size_;i++){((char *)&_data_start_)[i]=((char *)&_data_begin_)[i];}
+    uint32_t* data=(uint32_t*)&_data_start_;
+    while((data-(uint32_t*)&_data_start_)<(size_t)&_data_size_){
+        *data=*(uint32_t*)(&_data_begin_+(data-(uint32_t*)&_data_start_));
+        data++;
+    }
+    uint32_t* bss=(uint32_t*)&_bss_start_;
+    while((bss-(uint32_t*)&_bss_start_)<(size_t)&_bss_size_){
+        *bss=0;
+        bss++;
+    }
+    // for(size_t i=0;i<(size_t)&_data_size_;i++){((char *)&_data_start_)[i]=((char *)&_data_begin_)[i];}
+    // for(size_t i=0;i<(size_t)&_bss_size_;i++){((char *)&_bss_start_)[i]=0;}
     halt(main(mainargs));
 }
