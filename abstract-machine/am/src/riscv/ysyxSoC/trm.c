3442fa6c870c4c0d94184ede425dfc8ea1802556
tracer-ysyx <tracer@ysyx.org>
2026-05-20 15:37:49 +0800

    > compile NEMU ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:37:49 up  2:25,  1 user,  load average: 0.28, 0.22, 0.24

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -9,19 +9,20 @@ int main(const char *args);
 extern char _data_start_,_data_size_,_data_begin_;
 extern char _bss_start_,_bss_size_;
 void _trm_init() {
+    // *(volatile char *)(0x10000000L) = 'A';
     uint8_t* data=(uint8_t*)&_data_start_;
     while((data-(uint8_t*)&_data_start_)<(size_t)&_data_size_){
         *data = *(&_data_begin_ + (data - (uint8_t*)&_data_start_));
         data++;
     }
-    // *(volatile char *)(0x10000000L) = 'A';
+    // *(volatile char *)(0x10000000L) = 'B';
     // *(volatile char *)(0x10000000L) = '\n';
     uint8_t* bss=(uint8_t*)&_bss_start_;
     while((bss-(uint8_t*)&_bss_start_)<(size_t)&_bss_size_){
         *bss=0;
         bss++;
     }
-    // *(volatile char *)(0x10000000L) = 'B';
+    // *(volatile char *)(0x10000000L) = 'C';
     // *(volatile char *)(0x10000000L) = '\n';
     halt(main(mainargs));
 }
