7006ada0781824445788c594ec5ff444f6d392b6
Bi RuiDe <1776427100@qq.com>
2026-07-01 23:08:42 +0800

    学习test

---
+++ npc/chisel/src/add.scala
@@ -0,0 +1,14 @@
+import chisel3._
+import chisel3.util._
+class add extends Module{
+	val io = IO(new Bundle{
+		val a = Input(UInt(32.W))
+		val b = Input(UInt(32.W))
+		val x = Output(UInt(32.W))
+		val y = Output(UInt(32.W))
+	})
+	io.y := io.a + io.b
+	val reg = RegInit(0.U(32.W))
+	reg := io.a + io.b
+	io.x := reg
+}
