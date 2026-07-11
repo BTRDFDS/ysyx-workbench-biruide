2a4321a71044ab9efe0a2a0963ae045b53a49a9f
Bi RuiDe <1776427100@qq.com>
2026-07-11 21:55:13 +0800

    探针用不了，或者说我学不会，我还是选择手动控制

--- npc/chisel/src/add.scala
+++ npc/chisel/src/add.scala
@@ -1,14 +1,18 @@
 import chisel3._
 import chisel3.util._
+import chisel3.probe._
 class add extends Module{
 	val io = FlatIO(new Bundle{
 		val a = Input(UInt(32.W))
 		val b = Input(UInt(32.W))
 		val x = Output(UInt(32.W))
 		val y = Output(UInt(32.W))
+		val p = Output(Probe(UInt(32.W)))
 	})
 	io.y := io.a + io.b
 	val reg = RegInit(0.U(32.W))
 	reg := reg + 1.U
 	io.x := reg
+	define(io.p,ProbeValue(reg))
+
 }
