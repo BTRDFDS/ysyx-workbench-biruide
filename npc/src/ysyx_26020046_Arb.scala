6d4ee063fa9eb7c58262bc7191ae93e2cca58552
tracer-ysyx <tracer@ysyx.org>
2026-07-12 15:42:33 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:42:33 up  7:55,  1 user,  load average: 0.63, 0.55, 0.55

--- npc/src/ysyx_26020046_Arb.scala
+++ npc/src/ysyx_26020046_Arb.scala
@@ -59,9 +59,9 @@ class ysyx_26020046_Arb extends Module{
 	val addr 		= RegInit(0.U(8.W))
 	switch(status){
 		is(ArbStatus.Idle){
-			when(lsu.arvalid)	{status := ArbStatus.LsuR}
-			.elsewhen(lsu.awvalid){status := ArbStatus.LsuW}
-			.elsewhen(ifu.arvalid){status := ArbStatus.IfuR}
+			when(lsu.arvalid)		{status := ArbStatus.LsuR}
+			.elsewhen(lsu.awvalid)	{status := ArbStatus.LsuW}
+			.elsewhen(ifu.arvalid)	{status := ArbStatus.IfuR}
 		}
 		is(ArbStatus.LsuR){when(lsu.rready & backValid){status := ArbStatus.Idle}}
 		is(ArbStatus.LsuW){when(lsu.bready & backValid){status := ArbStatus.Idle}}
@@ -90,5 +90,11 @@ class ysyx_26020046_Arb extends Module{
 			is(ArbStatus.LsuW){lsu.bresp := 1.U}
 			is(ArbStatus.IfuR){ifu.rresp := 1.U}
 		}
+	}
+	// when(addrEnum === ArbAddr.Clint){backValid := clt.}
+	switch(status){
+		is(ArbStatus.LsuR){backValid := Mux(addrEnum === ArbAddr.Clint,clt.rvalid,out.rvalid)}
+		is(ArbStatus.IfuR){backValid := Mux(addrEnum === ArbAddr.Clint,clt.rvalid,out.rvalid)}
+		is(ArbStatus.LsuW){backValid := Mux(addrEnum === ArbAddr.Clint,clt.bvalid,out.bvalid)}
 	}
 }
