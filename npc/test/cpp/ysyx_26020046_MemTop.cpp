40ca1776eb3497f26c915d297b9dc9fc8c0a9834
tracer-ysyx <tracer@ysyx.org>
2026-07-12 17:53:16 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:53:16 up 10:05,  1 user,  load average: 0.91, 0.84, 0.61

--- npc/test/cpp/ysyx_26020046_MemTop.cpp
+++ npc/test/cpp/ysyx_26020046_MemTop.cpp
@@ -86,7 +86,6 @@ extern "C" int getRegPc(int addr);
 extern "C" void stop(){
 	NpcWave();
 	tfp->close();
-	printf("runStep=%d ebreak = %d\n",runStep,success);
 	for(int i=0;i<32;i++){
 		printf("reg[%2d]=%8x ",i,getRegPc(i));
 		if(i%8==7)printf("\n");
@@ -94,6 +93,7 @@ extern "C" void stop(){
 	bool success = getRegPc(10)==0
 	delete top;
 	delete contextp;
+	printf("runStep=%d ebreak = %d\n",runStep,success);
 	if(success)exit(0);
 	else exit(1);
 }
