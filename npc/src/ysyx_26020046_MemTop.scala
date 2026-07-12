6931bb586255de7c479b660f4ac744fd0b83867d
tracer-ysyx <tracer@ysyx.org>
2026-07-12 09:17:53 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:17:53 up  1:31,  1 user,  load average: 0.66, 0.62, 0.43

--- npc/src/ysyx_26020046_MemTop.scala
+++ npc/src/ysyx_26020046_MemTop.scala
@@ -1,4 +1,63 @@
-// import chisel3._
-// import chisel3.util._
-// import WidthConsts._
+import chisel3._
+import chisel3.util._
+import WidthConsts._
+import chisel3.experimental.ExtModule
 //TODO:需要BlackBox
+//根据官网，blackbox已经被废弃了，使用import chisel3.experimental.ExtModule
+object MemStatus extends ChiselEnum{val Idle,Read,Write = Value}
+class ysyx_26020046_MemTop extends Module{
+	val cpu = new ysyx_26020046()
+	val mem = new ysyx_26020046_Mem()
+	val status = RegInit(MemStatus.Idle)
+	val rAddr = RegInit(0.U(32.W))
+	val wAddr = RegInit(0.U(32.W))
+	val wData = RegInit(0.U(32.W))
+	val wStrb = RegInit(0.U(4.W))
+	val wAddrValid = RegInit(false.B)
+	val wDataValid = RegInit(false.B)
+	val writeValid = (wAddrValid|cpu.io.awvalid)&(wDataValid|cpu.io.wvalid)
+	switch(status){
+		is(MemStatus.Idle){
+			when(cpu.io.axi4.arvalid)	{status := MemStatus.Read}
+			elsewhen(writeValid)		{status := MemStatus.Write}
+		}
+		is(MemStatus.Read)	{when(cpu.io.axi4.rready){status := MemStatus.Idle}}
+		is(MemStatus.Write)	{when(cpu.io.axi4.bready){status := MemStatus.Idle}}
+	}
+	cpu.io.axi4.arready := status === MemStatus.Idle
+	cpu.io.axi4.rdata	:= Mux(status === MemStatus.Read,mem.read.data,0.U)
+	cpu.io.axi4.rresp	:= 0.U//OKAY
+	cpu.io.axi4.rvalid	:= status === MemStatus.Read
+	when(status === MemStatus.Idle & cpu.io.axi4.arvalid){rAddr := cpu.io.axi4.araddr}
+
+	cpu.io.awready	:= status === MemStatus.Idle
+	cpu.io.wready	:= status === MemStatus.Idle
+	cpu.io.bresp	:= 0.U//OKAY
+	cpu.io.bvalid	:= status === MemStatus.Write
+	when(status === MemStatus.Idle & cpu.io.awvalid){
+		wAddr := cpu.io.axi4.wAddr
+		wAddrValid := true.B
+	}
+	when(status === MemStatus.Write & cpu.io.axi4.bready){
+		wAddrValid := false.B
+		wDataValid := false.B
+	}
+	when(status === MemStatus.Idle & cpu.io.wvalid){
+		wData := cpu.io.axi4.wData
+		wStrb := cpu.io.axi4.wStrb
+		wDataValid := true.B
+	}
+
+}
+class ysyx_26020046_Mem extends ExtModule{
+    val read = IO(new Boundle{
+		val valid	= Input(Bool())
+		val addr	= Input(UInt(32.W))
+		val data	= Output(UInt(32.W))
+	})
+	val write  = IO(new Boundle{
+		val valid	= Input(Bool())
+		val addr	= Input(UInt(32.W))
+		val data	= Input(UInt(32.W))
+	})
+}
