3cb24813b37c43a9657e274a61aa8f20c91ba985
tracer-ysyx <tracer@ysyx.org>
2026-07-12 13:59:17 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:59:17 up  6:11,  1 user,  load average: 0.53, 0.66, 0.68

--- npc/src/ysyx_26020046_Wbu.scala
+++ npc/src/ysyx_26020046_Wbu.scala
@@ -72,8 +72,8 @@ class ysyx_26020046_Wbu() extends Module {
 		mepc 	:= in.pipe.pc
 		out.imme.back := Back.Error
 		out.imme.addr	:= mtvec
-		printf("error,stop!!!\n")
 		assert(false.B)
+		printf("error,stop!!!\n")
 		stop()
 	}otherwise{
 		out.imme.back := Back.Ready
