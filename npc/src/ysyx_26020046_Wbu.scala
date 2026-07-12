f03c0cea17a530ef32f82b85c94ea4673c52ea3b
tracer-ysyx <tracer@ysyx.org>
2026-07-12 16:57:02 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:57:02 up  9:09,  1 user,  load average: 1.07, 0.55, 0.42

--- npc/src/ysyx_26020046_Wbu.scala
+++ npc/src/ysyx_26020046_Wbu.scala
@@ -108,7 +108,7 @@ class ysyx_26020046_Wbu() extends Module {
 class ysyx_26020046_Chk extends ExtModule{
 	val io = IO(new Bundle{
 		val reg = Input(Vec(RegNum, UInt(BitWidth.W)))
-		val ebreak = Input(Bool)
+		val ebreak = Input(Bool())
 		val pc = Input(UInt(BitWidth.W))
 	})
 	setInline("ysyx_26020046_Chk.sv",
