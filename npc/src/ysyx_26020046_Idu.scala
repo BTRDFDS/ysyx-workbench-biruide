0ae4144acab89eb7c2a711658aa664f9c90a962a
tracer-ysyx <tracer@ysyx.org>
2026-07-12 13:21:54 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:21:54 up  5:34,  1 user,  load average: 0.57, 0.79, 0.54

--- npc/src/ysyx_26020046_Idu.scala
+++ npc/src/ysyx_26020046_Idu.scala
@@ -50,10 +50,6 @@ class ysyx_26020046_Idu extends Module{
 		val aluValid = WireInit(true.B)
 		val csrValid = WireInit(true.B)
 
-
-	// dontTouch(aluValid)
-	// dontTouch(csrValid)
-
 		val csrOp	= WireInit(CsrOp.Null)
 		val csrMesg = WireInit(0.U(BitWidth.W))
 
