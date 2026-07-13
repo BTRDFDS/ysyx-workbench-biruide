18cd9a7f878685c51748055e2455dc5d43421460
tracer-ysyx <tracer@ysyx.org>
2026-07-13 09:29:44 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:29:44 up  2:14,  1 user,  load average: 0.47, 0.57, 0.50

--- npc/src/ysyx_26020046_Wbu.scala
+++ npc/src/ysyx_26020046_Wbu.scala
@@ -133,18 +133,52 @@ class ysyx_26020046_Chk extends ExtModule{
 	);
 	import "DPI-C" function void ebreak();
 	always_comb if(io_ebreak)ebreak();
-	wire [31:0] io_reg [31:0];
-    assign io_reg = '{
-    	 io_reg_0, io_reg_1, io_reg_2, io_reg_3, io_reg_4, io_reg_5, io_reg_6, io_reg_7,
-         io_reg_8, io_reg_9,io_reg_10,io_reg_11,io_reg_12,io_reg_13,io_reg_14,io_reg_15,
-        io_reg_16,io_reg_17,io_reg_18,io_reg_19,io_reg_20,io_reg_21,io_reg_22,io_reg_23,
-        io_reg_24,io_reg_25,io_reg_26,io_reg_27,io_reg_28,io_reg_29,io_reg_30,io_reg_31
-    };
+	// wire [31:0] io_reg [31:0];
+    // assign io_reg = '{
+    // 	 io_reg_0, io_reg_1, io_reg_2, io_reg_3, io_reg_4, io_reg_5, io_reg_6, io_reg_7,
+    //      io_reg_8, io_reg_9,io_reg_10,io_reg_11,io_reg_12,io_reg_13,io_reg_14,io_reg_15,
+    //     io_reg_16,io_reg_17,io_reg_18,io_reg_19,io_reg_20,io_reg_21,io_reg_22,io_reg_23,
+    //     io_reg_24,io_reg_25,io_reg_26,io_reg_27,io_reg_28,io_reg_29,io_reg_30,io_reg_31
+    // };
 
 	export "DPI-C" function getRegPc;
 	function int getRegPc(input int addr);
-		logic [4:0] regAddr = addr[4:0];
-		return (regAddr == 0) ? io_pc : io_reg[regAddr];
+		// return (addr == 0) ? io_pc : io_reg[addr];
+		case(addr)
+			32'd00:return io_pc;
+			32'd01:return io_reg_1;
+			32'd02:return io_reg_2;
+			32'd03:return io_reg_3;
+			32'd04:return io_reg_4;
+			32'd05:return io_reg_5;
+			32'd06:return io_reg_6;
+			32'd07:return io_reg_7;
+			32'd08:return io_reg_8;
+			32'd09:return io_reg_9;
+			32'd10:return io_reg_10;
+			32'd11:return io_reg_11;
+			32'd12:return io_reg_12;
+			32'd13:return io_reg_13;
+			32'd14:return io_reg_14;
+			32'd15:return io_reg_15;
+			32'd16:return io_reg_16;
+			32'd17:return io_reg_17;
+			32'd18:return io_reg_18;
+			32'd19:return io_reg_19;
+			32'd20:return io_reg_20;
+			32'd21:return io_reg_21;
+			32'd22:return io_reg_22;
+			32'd23:return io_reg_23;
+			32'd24:return io_reg_24;
+			32'd25:return io_reg_25;
+			32'd26:return io_reg_26;
+			32'd27:return io_reg_27;
+			32'd28:return io_reg_28;
+			32'd29:return io_reg_29;
+			32'd30:return io_reg_30;
+			32'd31:return io_reg_31;
+			default: return 0;
+		endcase
 	endfunction
 	import "DPI-C" function void check();
 	always_ff@(posedge clock) if(io_check)check();
