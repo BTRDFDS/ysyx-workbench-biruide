670d5f49ead45d36c808746ab0714eb11441c590
Bi RuiDe <1776427100@qq.com>
2026-07-13 16:55:20 +0800

    jalr实现不完全，没有完全实现对于低位的清零

--- npc/src/ysyx_26020046_Exu.scala
+++ npc/src/ysyx_26020046_Exu.scala
@@ -49,7 +49,7 @@ class ysyx_26020046_Exu extends Module {
 			is(ExuAlu.Sra)	{result := (input1.asSInt >> input2(4,0)).asUInt}
 			is(ExuAlu.Csr)	{result := in.pipe.csrMesg}
 			is(ExuAlu.Imm)	{result := in.pipe.result}
-			is(ExuAlu.Jalr)	{result := (in.pipe.r1 + in.pipe.result)&~1.U}
+			is(ExuAlu.Jalr)	{result := Cat((in.pipe.r1 + in.pipe.result)(31,0), 0.U(1.W))}
 		}
 		val enBfun = WireInit(false.B)
 		switch(in.pipe.bfu){
