abe506acd8b06230b3786014a929640708263c81
tracer-ysyx <tracer@ysyx.org>
2026-07-13 11:47:21 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:47:21 up  4:23,  1 user,  load average: 0.37, 0.46, 0.48

--- npc/src/ysyx_26020046_Lsu.scala
+++ npc/src/ysyx_26020046_Lsu.scala
@@ -135,11 +135,16 @@ class ysyx_26020046_Lsu extends Module{
 		.elsewhen(ready & in.imme.back === Back.Ready){out.imme.back	:= Back.Ready}
 		.otherwise{out.imme.back	:= Back.Wait}
 	}
-	when(addrError){switch(in.pipe.lsuOp){
+	when(addrError){
+		switch(in.pipe.lsuOp){
 		is(LsuOp.Load)	{out.pipe.csrMesg := 4.U}//读取地址不对齐
 		is(LsuOp.Store)	{out.pipe.csrMesg := 6.U}//写入地址不对齐
-	}}elsewhen(backError){switch(in.pipe.lsuOp){
+		}
+	}
+	elsewhen(backError){
+		switch(in.pipe.lsuOp){
 		is(LsuOp.Load)	{out.pipe.csrMesg := 5.U}//读取故障
 		is(LsuOp.Store)	{out.pipe.csrMesg := 7.U}//写入故障
-	}}
+		}
+	}
 }
