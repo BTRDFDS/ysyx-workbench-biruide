7ed8d48f67fd16742d08ab0d2ad87e1538cf601d
tracer-ysyx <tracer@ysyx.org>
2026-07-13 08:57:19 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:57:19 up  1:45,  1 user,  load average: 0.32, 0.18, 0.12

--- npc/test/cpp/ysyx_26020046_MemTop.cpp
+++ npc/test/cpp/ysyx_26020046_MemTop.cpp
@@ -158,10 +158,6 @@ int main(int argc, char** argv) {
 		for(int i=0;i<10;i++){
 			top->clock=0;top->reset=1;top->eval();
 			top->clock=1;top->reset=1;top->eval();
-	#ifdef NPC_WAVE
-		contextp->timeInc(1);
-		tfp->dump(contextp->time());
-	#endif
 		}
 		top->clock=0;top->reset=0;top->eval();
 		runStep=0;
