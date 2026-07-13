9047292181482cab066e060e6cd8621fdc8040b2
Bi RuiDe <1776427100@qq.com>
2026-07-13 09:31:55 +0800

    找到difftest的问题了，是getRegPc有问题

--- npc/src/ysyx_26020046_Wbu.scala
+++ npc/src/ysyx_26020046_Wbu.scala
@@ -133,17 +133,8 @@ class ysyx_26020046_Chk extends ExtModule{
 	);
 	import "DPI-C" function void ebreak();
 	always_comb if(io_ebreak)ebreak();
-	// wire [31:0] io_reg [31:0];
-    // assign io_reg = '{
-    // 	 io_reg_0, io_reg_1, io_reg_2, io_reg_3, io_reg_4, io_reg_5, io_reg_6, io_reg_7,
-    //      io_reg_8, io_reg_9,io_reg_10,io_reg_11,io_reg_12,io_reg_13,io_reg_14,io_reg_15,
-    //     io_reg_16,io_reg_17,io_reg_18,io_reg_19,io_reg_20,io_reg_21,io_reg_22,io_reg_23,
-    //     io_reg_24,io_reg_25,io_reg_26,io_reg_27,io_reg_28,io_reg_29,io_reg_30,io_reg_31
-    // };
-
 	export "DPI-C" function getRegPc;
 	function int getRegPc(input int addr);
-		// return (addr == 0) ? io_pc : io_reg[addr];
 		case(addr)
 			32'd00:return io_pc;
 			32'd01:return io_reg_1;
