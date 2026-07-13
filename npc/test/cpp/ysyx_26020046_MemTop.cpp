ccae4dd1514e3e198d8a826fd126017609cc2d75
tracer-ysyx <tracer@ysyx.org>
2026-07-13 09:54:39 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:54:39 up  2:38,  1 user,  load average: 0.43, 0.54, 0.56

--- npc/test/cpp/ysyx_26020046_MemTop.cpp
+++ npc/test/cpp/ysyx_26020046_MemTop.cpp
@@ -65,6 +65,7 @@ extern "C" int pmem_read(int raddr) {
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
 	uint32_t waddrX=(uint32_t)waddr;
 	// printf("write addr= %x at Times=%d %x => %x\n",waddrX,runStep,wdata,wmask);
+	logFile<<"write addr= "<<std::hex<<waddrX<<" at T="<<std::dec<<runStep<<" "<<std::hex<<wdata<<" => "<<std::hex<<wmask<<std::endl;
 	if(waddrX==0x10000000){
 		printf("%c",wdata);
 		return;
