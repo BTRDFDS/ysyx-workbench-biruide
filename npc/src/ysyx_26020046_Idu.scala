5a6753a7b33750da10307d840d157678a0941aeb
tracer-ysyx <tracer@ysyx.org>
2026-07-14 10:47:14 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:47:14 up  1:40,  1 user,  load average: 0.71, 0.41, 0.23

--- npc/src/ysyx_26020046_Idu.scala
+++ npc/src/ysyx_26020046_Idu.scala
@@ -166,7 +166,11 @@ class ysyx_26020046_Idu extends Module{
 			}
 			when(opEnum === Op.Icsr){
 				switch(funct3){
-					is(0b000.U){out.pipe.csr := ExuCsr.Null;	in.imme.csrAddr := CsrAddr.Mtvec.asUInt}
+					is(0b000.U){
+						out.pipe.csr := ExuCsr.Null;
+						when(Cat(funct7,r2Addr,r1Addr,rdAddr) === 0b0011000_00010_00000_00000.U){in.imme.csrAddr := CsrAddr.Mepc.asUInt}
+						.otherwise{in.imme.csrAddr := CsrAddr.Mtvec.asUInt}
+						}
 					is(0b001.U){out.pipe.csr := ExuCsr.Write;	in.imme.csrAddr := Cat(funct7,r2Addr)}
 					is(0b010.U){out.pipe.csr := ExuCsr.Read;	in.imme.csrAddr := Cat(funct7,r2Addr)}
 					//TODO:0b010这个地方有待验证，原本是(oIfId.code.r1=='0)?NACSR:RACSR;
