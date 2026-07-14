b6ebf9546af14ef19092f5e346ed0b6b842b1ad2
tracer-ysyx <tracer@ysyx.org>
2026-07-14 11:33:49 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:33:49 up  2:23,  1 user,  load average: 0.39, 0.58, 0.65

--- am-kernels/kernels/yield-number/yield-number.c
+++ am-kernels/kernels/yield-number/yield-number.c
@@ -8,10 +8,10 @@ typedef union {
 } PCB;
 static PCB pcb[2], pcb_boot, *current = &pcb_boot;
 static uint8_t cnt = 0;
-static uint8_t nextstatus = 1;
+static uint8_t nextstatus = 0;
 static void f(void *arg) {
 	if((uintptr_t)arg == nextstatus){
-		nextstatus = ((uintptr_t)arg == 1 ? 2 : 1);
+		nextstatus = ((uintptr_t)arg == 0 ? 1 : 0);
 		cnt++;
 		yield();
 	}
@@ -26,10 +26,9 @@ static Context *schedule(Event ev, Context *prev) {
 }
 
 int main() {
-	halt(2);
 	cte_init(schedule);
-	pcb[0].cp = kcontext((Area) { pcb[0].stack, &pcb[0] + 1 }, f, (void *)1L);
-	pcb[1].cp = kcontext((Area) { pcb[1].stack, &pcb[1] + 1 }, f, (void *)2L);
+	pcb[0].cp = kcontext((Area) { pcb[0].stack, &pcb[0] + 1 }, f, (void *)0L);
+	pcb[1].cp = kcontext((Area) { pcb[1].stack, &pcb[1] + 1 }, f, (void *)1L);
 	yield();
 	halt(-1);
 }
