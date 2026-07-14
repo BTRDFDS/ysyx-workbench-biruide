6aca3e07c40962d7dec18068fcfd7b79210041e7
tracer-ysyx <tracer@ysyx.org>
2026-07-14 15:36:32 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:36:32 up  6:10,  1 user,  load average: 0.53, 0.61, 0.54

--- am-kernels/tests/my-tests/src/main.c
+++ am-kernels/tests/my-tests/src/main.c
@@ -296,8 +296,8 @@ void test_csrr(){
 	asm volatile("csrr %0, mcycle"  : "=r"(time[8]));
 	asm volatile("csrr %0, mcycle"  : "=r"(time[9]));
 	printf("time: %x %x %x %x %x %x %x %x %x %x\n", time[0], time[1], time[2], time[3], time[4], time[5], time[6], time[7], time[8], time[9]);
-	for(int i=0;i<9;i++){
-		if(time[i+1]-time[i]!=2){//会有压栈，需要2
+	for(int i=0;i<10-2;i++){
+		if(time[i+1]-time[i]!=time[i+2]-time[i+1]){//会有压栈，需要2
 			printf("csrr test maybe \033[1;31mFAIL\033[0m\n");
 			assert(0);
 		}
