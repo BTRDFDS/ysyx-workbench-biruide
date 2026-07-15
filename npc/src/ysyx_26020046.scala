a4e945dc8163e1aad46cc8f19c8173674c4c1ce5
tracer-ysyx <tracer@ysyx.org>
2026-07-15 10:28:59 +0800

    > generate verilog ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:28:59 up  2:45,  1 user,  load average: 0.28, 0.61, 0.35

--- npc/src/ysyx_26020046.scala
+++ npc/src/ysyx_26020046.scala
@@ -4,7 +4,7 @@ import WidthConsts._
 
 class ysyx_26020046 extends Module {
 	val io = IO(new Bundle {
-		val interrupt = Input(Bool())
+		val interrupt = Output(Bool())
 		val master = new Axi4MasterOut()
 		val slave = Flipped(new Axi4MasterOut())
 	})
@@ -78,6 +78,8 @@ class ysyx_26020046 extends Module {
 	io.slave.bvalid	:= false.B
 	io.slave.bresp	:= 0.U
 	io.slave.bid	:= 0.U
+
+	io.interrupt := false.B
 
 	dontTouch(io.master)
 	dontTouch(io.slave)
