41af46c5580182eb21b19985c64a3c5f3a2110ae
tracer-ysyx <tracer@ysyx.org>
2026-07-17 12:13:46 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:13:46 up  3:21,  1 user,  load average: 0.23, 0.41, 0.41

--- npc/src/ysyx_26020046_Lsu.scala
+++ npc/src/ysyx_26020046_Lsu.scala
@@ -114,8 +114,8 @@ class ysyx_26020046_Lsu extends Module{
 		}
 		when(in.pipe.lsuOp === LsuOp.Load){
 			val rdata = RegInit(0.U(BitWidth.W))
-			when(status === LsuStatus.Back & axi4.rvalid){rdata := axi4.rdata >> (8.U * in.pipe.result(1,0))}
-			// when(status === LsuStatus.Back & axi4.rvalid){rdata := axi4.rdata}
+			// when(status === LsuStatus.Back & axi4.rvalid){rdata := axi4.rdata >> (8.U * in.pipe.result(1,0))}
+			when(status === LsuStatus.Back & axi4.rvalid){rdata := axi4.rdata}
 			switch(in.pipe.lsuAddr){
 				is(LsuAddr.B ){out.pipe.result := Cat(Fill(BitWidth- 8,rdata( 7)),rdata( 7,0))}
 				is(LsuAddr.H ){out.pipe.result := Cat(Fill(BitWidth-16,rdata(15)),rdata(15,0))}
