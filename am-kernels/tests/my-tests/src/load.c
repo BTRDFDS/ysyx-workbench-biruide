884cb1ca93954bd4059049946185433b96b550fe
Bi RuiDe <1776427100@qq.com>
2026-07-17 22:10:24 +0800

    确认再次明确，错位为bootloader错误，写入bss的时候全都有一个+1的偏移

--- am-kernels/tests/my-tests/src/load.c
+++ am-kernels/tests/my-tests/src/load.c
@@ -5,7 +5,6 @@ uint32_t a[2] = {0,1};
 int main() {
 	// return a[3];
 	for(uint32_t i = 0; i < 2; i ++) {
-        // *(volatile char *)(0x10000000L) = '0'+i;
 		// check(a[i]==i);
 		uint32_t p=a[i];
 		if(p!=i)return p;
