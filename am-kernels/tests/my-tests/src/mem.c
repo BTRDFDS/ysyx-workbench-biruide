e51e3c747e7a61552a36c9c7eadcd464aa7518ee
tracer-ysyx <tracer@ysyx.org>
2026-07-17 10:36:20 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:36:20 up  1:53,  1 user,  load average: 0.59, 0.27, 0.24

--- am-kernels/tests/my-tests/src/mem.c
+++ am-kernels/tests/my-tests/src/mem.c
@@ -5,10 +5,10 @@
 int main() {
 	uintptr_t start =0x0f000000;
 	uintptr_t end   =0x0f001fff;
-	for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)*p=(uint8_t)((uintptr_t)p&0xFF);
-	for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)if(*p != (uint8_t)((uintptr_t)p&0xFF))halt((uint32_t)((uintptr_t)p)|0x000A0000);
-	// for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)*p=(uint16_t)((uintptr_t)p&0xFFFF);
-	// for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)if(*p != (uint16_t)((uintptr_t)p&0xFFFF))halt((uint32_t)((uintptr_t)p)|0x000B0000);
+	// for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)*p=(uint8_t)((uintptr_t)p&0xFF);
+	// for(uint8_t *p=(uint8_t*)start;p<=(uint8_t*)end;p++)if(*p != (uint8_t)((uintptr_t)p&0xFF))halt((uint32_t)((uintptr_t)p)|0x000A0000);
+	for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)*p=(uint16_t)((uintptr_t)p&0xFFFF);
+	for(uint16_t *p=(uint16_t*)start;p<=(uint16_t*)end;p++)if(*p != (uint16_t)((uintptr_t)p&0xFFFF))halt((uint32_t)((uintptr_t)p)|0x000B0000);
 	// for(uint32_t *p=(uint32_t*)start;p<=(uint32_t*)end;p++)*p=(uint32_t)((uintptr_t)p);
 	// for(uint32_t *p=(uint32_t*)start;p<=(uint32_t*)end;p++)if(*p != (uint32_t)((uintptr_t)p))halt((uint32_t)((uintptr_t)p)|0x000C0000);
 	return 0;
