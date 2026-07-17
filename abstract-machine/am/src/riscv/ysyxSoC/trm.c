e17b5b0ceb6173077921a7ae6410d6123c3b95e4
tracer-ysyx <tracer@ysyx.org>
2026-07-17 16:06:44 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:06:44 up  6:52,  1 user,  load average: 0.04, 0.16, 0.19

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -12,6 +12,7 @@ int main(const char *args);
 extern char _data_start_,_data_size_,_data_begin_;
 extern char _bss_start_,_bss_size_;
 void _trm_init() {
+	asm volatile("nop");
 	uint8_t* data=(uint8_t*)&_data_start_;
 	while((data-(uint8_t*)&_data_start_)<(size_t)&_data_size_){
 		*data = *(&_data_begin_ + (data - (uint8_t*)&_data_start_));
