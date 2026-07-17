b46d691c160664d050d9958d30d7e54e311b199c
tracer-ysyx <tracer@ysyx.org>
2026-07-17 15:28:36 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:28:36 up  6:17,  1 user,  load average: 0.07, 0.22, 0.30

--- am-kernels/tests/my-tests/src/load.c
+++ am-kernels/tests/my-tests/src/load.c
@@ -3,7 +3,7 @@
 int a[10] = {0,1,2,3,4,5,6,7,8,9};
 int main() {
 	for(int i = 0; i < 10; i ++) {
-        *(volatile char *)(0x10000000L) = '0'+i;
+        // *(volatile char *)(0x10000000L) = '0'+i;
 		check(a[i]==i);
 	}
 	return 0;
