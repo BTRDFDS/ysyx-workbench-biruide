5f69a18a3abc876cfb1b0972001a97de62ed446e
tracer-ysyx <tracer@ysyx.org>
2026-07-18 15:33:25 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:33:25 up  9:08,  1 user,  load average: 0.39, 0.71, 0.57

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -92,7 +92,9 @@ extern "C" void check(){
 	if(NpcDifftestCheck(getRegPc(0)))NpcReturn("difftest",-1);
 }
 extern "C" void flash_read(int32_t addr, int32_t *data) {
-	// assert(0);
+	#ifdef NPC_WAVE
+	logFile<<"flash	R "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep;
+	#endif
 	uint32_t addrX=((uint32_t)addr)&0xfffffffc;
 	if(addrX-flashAddr>=flashSize|addrX<flashAddr){NpcReturn("flash read error",addrX);}
 	uint32_t temp=
@@ -102,7 +104,7 @@ extern "C" void flash_read(int32_t addr, int32_t *data) {
 		((uint32_t)flash[addrX-flashAddr+3]<< 8);
 	*data=temp;
 	#ifdef NPC_WAVE
-	logFile<<"flash	R "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep<<" => "<<std::hex<<temp<<std::endl;
+	logFile<<" => "<<std::hex<<temp<<std::endl;
 	#endif
 }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
