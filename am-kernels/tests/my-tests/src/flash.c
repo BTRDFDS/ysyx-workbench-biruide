eeb1ab4375a2d4cd856dbf122ecb6cc76764652b
tracer-ysyx <tracer@ysyx.org>
2026-07-18 15:41:40 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:41:40 up  9:17,  1 user,  load average: 0.49, 0.72, 0.67

--- am-kernels/tests/my-tests/src/flash.c
+++ am-kernels/tests/my-tests/src/flash.c
@@ -1,7 +1,7 @@
 #include "trap.h"
 int main(){
 	const uint32_t flashAddr	=0x30000000;
-	for(uint32_t i=0;i<0x100;i++){
+	for(uint32_t i=0;i<0x10;i++){
 		uint8_t p=*((uint8_t*)(flashAddr+i));
 		if(p!=(i&0xff))return (p)|((i&0xff)<<16);
 	}
