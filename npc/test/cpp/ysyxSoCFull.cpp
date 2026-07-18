afd1320938f15b2c1a2505b6ca16047f0d5dbd74
tracer-ysyx <tracer@ysyx.org>
2026-07-18 15:36:06 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:36:06 up  9:11,  1 user,  load average: 0.54, 0.67, 0.58

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -97,7 +97,7 @@ extern "C" void flash_read(int32_t addr, int32_t *data) {
 	logFile<<"flash	R "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep;
 	#endif
 	// if(addrX-flashAddr>=flashSize|addrX<flashAddr){NpcReturn("flash read error",addrX);}
-	if(addrX>=flashSize)NpcRetuen("flash read error",addrX);
+	if(addrX>=flashSize)NpcReturn("flash read error",addrX);
 	uint32_t temp=
 		((uint32_t)flash[addrX-flashAddr+0]<< 0)|
 		((uint32_t)flash[addrX-flashAddr+1]<< 8)|
