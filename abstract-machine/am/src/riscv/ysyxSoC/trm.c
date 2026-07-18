fab1dbd939c3a19cff46c92931acf5933d026cd1
tracer-ysyx <tracer@ysyx.org>
2026-07-18 11:55:01 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:55:01 up  5:21,  1 user,  load average: 0.33, 0.20, 0.11

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -26,8 +26,8 @@ void _trm_init() {
 	}
 	// //波特率
 	*(volatile char *)(0x10000003L)=0b10000011;
-	*(volatile char *)(0x10000000L)=0x00;
-	*(volatile char *)(0x10000001L)=0x90;
+	*(volatile char *)(0x10000000L)=0x01;
+	*(volatile char *)(0x10000001L)=0x00;
 	//115200*16==50MHz/36864(0x9000)
 	*(volatile char *)(0x10000003L)=0b00000011;
 	//复位FIFO
