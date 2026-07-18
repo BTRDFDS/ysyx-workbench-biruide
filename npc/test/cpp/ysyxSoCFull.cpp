d12bda15804fc42103202fc1fb6c7fb10399c71c
tracer-ysyx <tracer@ysyx.org>
2026-07-18 11:55:21 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:55:21 up  5:21,  1 user,  load average: 0.43, 0.23, 0.12

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -196,8 +196,8 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 	printf("\033[1;32m Welcome to NPC[\033[1;36m%s %s\033[1;32m] \033[0m\n",__DATE__,__TIME__);
-	// for(int i=0;(!contextp->gotFinish());i++){
-	for(int i=0;i<500L&(!contextp->gotFinish());i++){
+	for(int i=0;(!contextp->gotFinish());i++){
+	// for(int i=0;i<500L&(!contextp->gotFinish());i++){
 		NpcWave();
 		top->clock=1;top->eval();
 		NpcWave();
