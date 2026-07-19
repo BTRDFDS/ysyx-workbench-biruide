c6f91943b011ddc8d8b0912b69f0905eac794959
tracer-ysyx <tracer@ysyx.org>
2026-07-19 20:07:23 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:07:23 up 11:39,  1 user,  load average: 1.09, 1.06, 1.06

--- am-kernels/tests/my-tests/src/spi.c
+++ am-kernels/tests/my-tests/src/spi.c
@@ -19,10 +19,10 @@
 // }
 uint32_t bitrev(uint32_t x){
     uint32_t r=0;
-    for(int i=0;i<32;i++){
-        r<<=1;
-        r|=(x&1);
-        x>>=1;
+    for(int i=0;i<4;i++){
+        r<<=16;
+        r|=(x&0xff);
+        x>>=16;
     }
     return r;
 }
@@ -39,5 +39,5 @@ int main(){
     *(volatile uint32_t*)(RTx0) = bitrev(0x03000000);//需要反写
 	*(volatile uint32_t*)(Ctrl) = 0b10101101000000;
 	while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
-	return bitrev(*(volatile uint32_t*)(RTx1)>>1);
+	return bitrev(*(volatile uint32_t*)(RTx1));
 }
