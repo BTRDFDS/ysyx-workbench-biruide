238c323bec50979c7b82a7060ad631ebf86b678e
tracer-ysyx <tracer@ysyx.org>
2026-07-19 17:21:20 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:21:20 up  8:47,  1 user,  load average: 1.16, 1.08, 1.03

--- am-kernels/tests/my-tests/src/spi.c
+++ am-kernels/tests/my-tests/src/spi.c
@@ -1,31 +1,34 @@
 #include "trap.h"
 	const uint32_t Addr = 0x10001000;
 	const uint32_t RTx0	= Addr;
+	const uint32_t RTx1	= Addr+0x04;
 	const uint32_t Ctrl = Addr+0x10;
 	const uint32_t Div	= Addr+0x14;
 	const uint32_t SS   = Addr+0x18;
-void checkBitrev(uint8_t x){
-	*(volatile uint32_t*)(Addr) = x;
-	*(volatile uint32_t*)(Ctrl) = 0b10100100010000;
-	while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
-	// return *(volatile uint32_t*)(Addr);
-	uint8_t y = (*(volatile uint32_t*)(Addr)>>8)&0xff;
-	for(int i=0;i<8;i++){
-		if(((y>>i)&0b1)!=((x>>(7-i))&0b1)){
-			halt(0xf0000|(y<<8)|x);
-		}
-	}
-}
+// void checkBitrev(uint8_t x){
+// 	*(volatile uint32_t*)(Addr) = x;
+// 	*(volatile uint32_t*)(Ctrl) = 0b10100100010000;
+// 	while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
+// 	// return *(volatile uint32_t*)(Addr);
+// 	uint8_t y = (*(volatile uint32_t*)(Addr)>>8)&0xff;
+// 	for(int i=0;i<8;i++){
+// 		if(((y>>i)&0b1)!=((x>>(7-i))&0b1)){
+// 			halt(0xf0000|(y<<8)|x);
+// 		}
+// 	}
+// }
 int main(){
-	// return 0;
-	*(volatile uint32_t*)(Ctrl) = 0b10100000010000;//0b10100100010000
-	//CHAR_LEN=16
-	//Rx_NEG=0 上升沿
-	//Tx_NEG=0 上升沿
+	*(volatile uint32_t*)(Ctrl) = 0b10101001000000;
+	//CHAR_LEN=64 01000000
+	//Rx_NEG=1
+	//Tx_NEG=0
 	//LSB=1
 	//ASS=1
 	*(volatile uint32_t*)(Div) = 0;//除数为0，计算结果为主频的1/2
-	*(volatile uint32_t*)(SS) = 0b1<<7;//SS=7
-	for(uint8_t i=0;i<0xff;i++) checkBitrev(i);
-	return 0;
+	*(volatile uint32_t*)(SS) = 0b1;//SS=0 flash
+	// for(uint8_t i=0;i<0xff;i++) checkBitrev(i);
+    *(volatile uint32_t*)(Addr) = 0x30000000;
+	*(volatile uint32_t*)(Ctrl) = 0b10101101000000;
+	while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
+	return *(volatile uint32_t*)(RTx1);
 }
