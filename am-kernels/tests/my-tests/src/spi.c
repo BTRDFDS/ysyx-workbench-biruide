3bb6d39fce2b7f79897712aeee2a4c83d3543b7c
Bi RuiDe <1776427100@qq.com>
2026-07-19 21:48:46 +0800

    手动测试通过了，之前的问题是我看flash手册有问题，从在下降沿更新，主在上升沿读取刚刚好。现在等待自动测试结果

--- am-kernels/tests/my-tests/src/spi.c
+++ am-kernels/tests/my-tests/src/spi.c
@@ -5,37 +5,25 @@
 	const uint32_t Ctrl = Addr+0x10;
 	const uint32_t Div	= Addr+0x14;
 	const uint32_t SS   = Addr+0x18;
-// void checkBitrev(uint8_t x){
-// 	*(volatile uint32_t*)(Addr) = x;
-// 	*(volatile uint32_t*)(Ctrl) = 0b10100100010000;
-// 	while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
-// 	// return *(volatile uint32_t*)(Addr);
-// 	uint8_t y = (*(volatile uint32_t*)(Addr)>>8)&0xff;
-// 	for(int i=0;i<8;i++){
-// 		if(((y>>i)&0b1)!=((x>>(7-i))&0b1)){
-// 			halt(0xf0000|(y<<8)|x);
-// 		}
-// 	}
-// }
 uint32_t bitrev(uint32_t x){
-    uint32_t r=0;
-    for(int i=0;i<32;i++){
-        r<<=1;
-        r|=(x&1);
-        x>>=1;
-    }
-    return r;
+	uint32_t r=0;
+	for(int i=0;i<32;i++){
+		r<<=1;
+		r|=(x&1);
+		x>>=1;
+	}
+	return r;
 }
 uint32_t wordrev(uint32_t x){
-    uint32_t r=0;
-    for(int i=0;i<4;i++){
-        r<<=8;
-        r|=(x&0xff);
-        x>>=8;
-    }
-    return r;
+	uint32_t r=0;
+	for(int i=0;i<4;i++){
+		r<<=8;
+		r|=(x&0xff);
+		x>>=8;
+	}
+	return r;
 }
-int main(){
+int main(){//仅仅适用于cpp未修改能加载程序的情况
 	*(volatile uint32_t*)(Ctrl) = 0b10100001000000;
 	//CHAR_LEN=64 01000000
 	//Rx_NEG=1
@@ -44,10 +32,33 @@ int main(){
 	//ASS=1
 	*(volatile uint32_t*)(Div) = 0;//除数为0，计算结果为主频的1/2
 	*(volatile uint32_t*)(SS) = 0b1;//SS=0 flash
-	// for(uint8_t i=0;i<0xff;i++) checkBitrev(i);
-    *(volatile uint32_t*)(RTx0) = bitrev(0x03000000);//需要反写
-	*(volatile uint32_t*)(Ctrl) = 0b10100101000000;
-	while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
-	// return wordrev(*(volatile uint32_t*)(RTx1)>>1);
-	return wordrev(bitrev(*(volatile uint32_t*)(RTx0+4)));
+
+	// *(volatile uint32_t*)(RTx0) = bitrev(0x03000000);//需要反写
+	// *(volatile uint32_t*)(Ctrl) = 0b10100101000000;
+	// while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
+	// return wordrev(bitrev(*(volatile uint32_t*)(RTx1)));//需要字节内和字节分别取反
+
+	for(uint32_t i=0;i<0x100;i++){
+		*(volatile uint32_t*)(RTx0) = 0x03000000+i;
+		*(volatile uint32_t*)(Ctrl) = 0b10100101000000;
+		while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
+		uint8_t p = wordrev(bitrev(*(volatile uint32_t*)(RTx1))) & 0xff;
+		if(p!=(i&0xff))return (p)|((i&0xff)<<16);
+	}
+	for(uint32_t i=0;i+1<0x100;i+=2){
+		*(volatile uint32_t*)(RTx0) = 0x03000000+i;
+		*(volatile uint32_t*)(Ctrl) = 0b10100101000000;
+		while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
+		uint16_t p = wordrev(bitrev(*(volatile uint32_t*)(RTx1))) & 0xffff;
+		if(p!=(((i+1)<<8)|i))return p;
+	}
+	for(uint32_t i=0;i+3<0x100;i+=4){
+		*(volatile uint32_t*)(RTx0) = 0x03000000+i;
+		*(volatile uint32_t*)(Ctrl) = 0b10100101000000;
+		while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
+		uint32_t p = wordrev(bitrev(*(volatile uint32_t*)(RTx1)));
+		if(p!=(((i+3)<<24)|((i+2)<<16)|((i+1)<<8)|i))return p;
+	}
+
+	return 0;
 }
