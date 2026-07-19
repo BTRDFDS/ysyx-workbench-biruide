70ce737765b90386f9b91d39ac105fd41be0969a
Bi RuiDe <1776427100@qq.com>
2026-07-19 21:49:40 +0800

    忘了取反了

--- am-kernels/tests/my-tests/src/spi.c
+++ am-kernels/tests/my-tests/src/spi.c
@@ -39,21 +39,21 @@ int main(){//仅仅适用于cpp未修改能加载程序的情况
 	// return wordrev(bitrev(*(volatile uint32_t*)(RTx1)));//需要字节内和字节分别取反
 
 	for(uint32_t i=0;i<0x100;i++){
-		*(volatile uint32_t*)(RTx0) = 0x03000000+i;
+		*(volatile uint32_t*)(RTx0) = bitrev(0x03000000+i);
 		*(volatile uint32_t*)(Ctrl) = 0b10100101000000;
 		while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
 		uint8_t p = wordrev(bitrev(*(volatile uint32_t*)(RTx1))) & 0xff;
 		if(p!=(i&0xff))return (p)|((i&0xff)<<16);
 	}
 	for(uint32_t i=0;i+1<0x100;i+=2){
-		*(volatile uint32_t*)(RTx0) = 0x03000000+i;
+		*(volatile uint32_t*)(RTx0) = bitrev(0x03000000+i);
 		*(volatile uint32_t*)(Ctrl) = 0b10100101000000;
 		while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
 		uint16_t p = wordrev(bitrev(*(volatile uint32_t*)(RTx1))) & 0xffff;
 		if(p!=(((i+1)<<8)|i))return p;
 	}
 	for(uint32_t i=0;i+3<0x100;i+=4){
-		*(volatile uint32_t*)(RTx0) = 0x03000000+i;
+		*(volatile uint32_t*)(RTx0) = bitrev(0x03000000+i);
 		*(volatile uint32_t*)(Ctrl) = 0b10100101000000;
 		while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
 		uint32_t p = wordrev(bitrev(*(volatile uint32_t*)(RTx1)));
