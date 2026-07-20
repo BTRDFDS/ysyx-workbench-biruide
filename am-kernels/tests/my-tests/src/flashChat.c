8a06231dd4a5b474bc4eb3b5a089807513b402f0
Bi RuiDe <1776427100@qq.com>
2026-07-21 07:31:53 +0800

    测试允许程序

--- am-kernels/tests/my-tests/src/flashChat.c
+++ am-kernels/tests/my-tests/src/flashChat.c
@@ -1,49 +1,10 @@
 #include "trap.h"
-
-uint32_t flash_read(uint32_t addr){
-	if(addr>>24!=0x00)halt(-1);//error,虽然寻址空间是到3f，但是实际上用不了因为就24位
-	const uint32_t Addr = 0x10001000;
-	const uint32_t RTx0	= Addr;
-	const uint32_t RTx1	= Addr+0x04;
-	const uint32_t Ctrl = Addr+0x10;
-	const uint32_t Div	= Addr+0x14;
-	const uint32_t SS   = Addr+0x18;
-	//CHAR_LEN=64 01000000
-	//Rx_NEG=1
-	//Tx_NEG=0
-	//LSB=1
-	//ASS=1
-	*(volatile uint32_t*)(Ctrl)	= 0b10100001000000;
-	*(volatile uint32_t*)(Div)	= 0;
-	*(volatile uint32_t*)(SS)	= 0b1;//SS=0 flash
-	uint32_t code = 0x03000000|(addr&0xffffff);
-	uint32_t rev=0;
-	for(int i=0;i<32;i++){
-		rev<<=1;
-		rev|=(code&1);
-		code>>=1;
-	}
-	*(volatile uint32_t*)(RTx0) = rev;
-	*(volatile uint32_t*)(Ctrl) = 0b10100101000000;
-	while(((*(volatile uint32_t*)(Ctrl)>>8)&0b1)==1);
-	code=*(volatile uint32_t*)(RTx1);
-	for(int i=0;i<32;i++){
-		rev<<=1;
-		rev|=(code&1);
-		code>>=1;
-	}
-	for(int i=0;i<4;i++){
-		code<<=8;
-		code|=(rev&0xff);
-		rev>>=8;
-	}
-	return code;
-}
 int main(){
-	const uint32_t start	=0x0f000000;
+	const uint32_t flashAddr	=0x30000000;
+	const uint32_t sramAddr		=0x0f000000;
 	for(uint32_t i=0;i<0x40;i+=4){
-		*(uint32_t*)(start+i)=flash_read(i);
+		*(uint32_t*)(sramAddr+i)=*((uint32_t*)(flashAddr+i));
 	}
-	asm volatile("mv t0, %0; jalr t0" : : "r"(start));
+	asm volatile("mv t0, %0; jalr t0" : : "r"(sramAddr));
 	return 0;
 }
--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -163,25 +163,25 @@ void NpcInitMem(int argc, char** argv){
 	// 	if(i%16==15)printf("\n");
 	// }
 	NpcDifftestInit8(mromSize,mrom,mromAddr,"/home/biruide/ysyx-workbench/npc/test/cpp/lib/riscv32-nemu-interpreter-so-mrom");
-	for(uint32_t i=0;i<0x100;i++){
-		flash[i]=i&0xff;
-	}
-	// file =fopen("/home/biruide/ysyx-workbench/am-kernels/tests/my-tests/build/myTest_chat-riscv32i-ysyxsoc.bin","rb");
-	// if(file==NULL){printf("can't open myTest_chat\n");exit(-1);}
-	// fseek(file, 0, SEEK_END);
-    // fileSize = ftell(file);
-    // fseek(file, 0, SEEK_SET);
-    // wordsRead = fread(flash, sizeof(uint8_t), fileSize/sizeof(uint8_t), file);
-	// if(wordsRead!=fileSize/sizeof(uint8_t)){printf("can't read file\n");}
-	// fclose(file);
-	// for(uint32_t i=0;i<0x100;i+=4){
-	// 	uint32_t temp=
-	// 	((uint32_t)flash[i+0]<< 0)|
-	// 	((uint32_t)flash[i+1]<< 8)|
-	// 	((uint32_t)flash[i+2]<<16)|
-	// 	((uint32_t)flash[i+3]<<24);
-	// 	logFile<<std::hex<<temp<<std::endl;
+	// for(uint32_t i=0;i<0x100;i++){
+	// 	flash[i]=i&0xff;
 	// }
+	file =fopen("/home/biruide/ysyx-workbench/am-kernels/tests/my-tests/build/myTest_chat-riscv32i-ysyxsoc.bin","rb");
+	if(file==NULL){printf("can't open myTest_chat\n");exit(-1);}
+	fseek(file, 0, SEEK_END);
+    fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    wordsRead = fread(flash, sizeof(uint8_t), fileSize/sizeof(uint8_t), file);
+	if(wordsRead!=fileSize/sizeof(uint8_t)){printf("can't read file\n");}
+	fclose(file);
+	for(uint32_t i=0;i<0x100;i+=4){
+		uint32_t temp=
+		((uint32_t)flash[i+0]<< 0)|
+		((uint32_t)flash[i+1]<< 8)|
+		((uint32_t)flash[i+2]<<16)|
+		((uint32_t)flash[i+3]<<24);
+		logFile<<std::hex<<temp<<std::endl;
+	}
 }
 void NpcDifftestGetGpr(uint32_t *gpr){
 	if(gpr==NULL){NpcReturn("difftest unable",-1);}
