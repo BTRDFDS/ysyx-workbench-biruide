374446856ca3eda6798f188fd188ccacdb8b288d
Bi RuiDe <1776427100@qq.com>
2026-07-21 07:15:10 +0800

    过不了好像单纯是因为我的500的时限没有删

--- am-kernels/tests/my-tests/src/flash.c
+++ am-kernels/tests/my-tests/src/flash.c
@@ -1,18 +1,18 @@
 #include "trap.h"
 int main(){
 	const uint32_t flashAddr	=0x30000000;
-	// for(uint32_t i=0;i<0x100;i++){
-	// 	uint8_t p=*((uint8_t*)(flashAddr+i));
-	// 	if(p!=(i&0xff))return (p)|((i&0xff)<<16);
-	// }
-	// for(uint32_t i=0;i+1<0x100;i+=2){
-	// 	uint16_t p=*((uint16_t*)(flashAddr+i));
-	// 	if(p!=(((i+1)<<8)|i))return p;
-	// }
+	for(uint32_t i=0;i<0x100;i++){
+		uint8_t p=*((uint8_t*)(flashAddr+i));
+		if(p!=(i&0xff))return (p)|((i&0xff)<<16);
+	}
+	for(uint32_t i=0;i+1<0x100;i+=2){
+		uint16_t p=*((uint16_t*)(flashAddr+i));
+		if(p!=(((i+1)<<8)|i))return p;
+	}
 	for(uint32_t i=0;i+3<0x100;i+=4){
 		uint32_t p=*((uint32_t*)(flashAddr+i));
 		if(p!=(((i+3)<<24)|((i+2)<<16)|((i+1)<<8)|i))return p;
 	}
-	// return *((uint32_t*)(flashAddr+4));//目前只有手动测试能过。其它有待检查
+	// return *((uint32_t*)(flashAddr+4));
 	return 0;
 }
