b0b9728e91501a566494c5ac9705ced050842501
tracer-ysyx <tracer@ysyx.org>
2026-07-20 19:23:42 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:23:42 up 10:36,  1 user,  load average: 0.14, 0.15, 0.11

--- am-kernels/tests/my-tests/src/flash.c
+++ am-kernels/tests/my-tests/src/flash.c
@@ -1,17 +1,18 @@
 #include "trap.h"
 int main(){
 	const uint32_t flashAddr	=0x30000000;
-	for(uint32_t i=0;i<0x100;i++){
-		uint8_t p=*((uint8_t*)(flashAddr+i));
-		if(p!=(i&0xff))return (p)|((i&0xff)<<16);
-	}
-	for(uint32_t i=0;i+1<0x100;i+=2){
-		uint16_t p=*((uint16_t*)(flashAddr+i));
-		if(p!=(((i+1)<<8)|i))return p;
-	}
-	for(uint32_t i=0;i+3<0x100;i+=4){
-		uint32_t p=*((uint32_t*)(flashAddr+i));
-		if(p!=(((i+3)<<24)|((i+2)<<16)|((i+1)<<8)|i))return p;
-	}
-	return 0;
+	// for(uint32_t i=0;i<0x100;i++){
+	// 	uint8_t p=*((uint8_t*)(flashAddr+i));
+	// 	if(p!=(i&0xff))return (p)|((i&0xff)<<16);
+	// }
+	// for(uint32_t i=0;i+1<0x100;i+=2){
+	// 	uint16_t p=*((uint16_t*)(flashAddr+i));
+	// 	if(p!=(((i+1)<<8)|i))return p;
+	// }
+	// for(uint32_t i=0;i+3<0x100;i+=4){
+	// 	uint32_t p=*((uint32_t*)(flashAddr+i));
+	// 	if(p!=(((i+3)<<24)|((i+2)<<16)|((i+1)<<8)|i))return p;
+	// }
+	return *((uint32_t*)(flashAddr+0));
+	// return 0;
 }
