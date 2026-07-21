488b86682de48033264061b49287c34302d09fb5
tracer-ysyx <tracer@ysyx.org>
2026-07-21 18:17:02 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:17:02 up 11:10,  1 user,  load average: 0.56, 0.82, 0.68

--- am-kernels/tests/my-tests/src/psram.c
+++ am-kernels/tests/my-tests/src/psram.c
@@ -1,7 +1,13 @@
 #include "trap.h"
 int main(){
-    // *(volatile uint32_t*)0x80000000 = 0x76543210;
+	// *(volatile uint32_t*)0x80000000 = 0x76543210;
 	// return *(volatile uint32_t*)0x80000000;
-    *(volatile uint8_t*)0x80ffffff = 0x12;
-    return *(volatile uint8_t*)0x80ffffff;
+	// *(volatile uint8_t*)0x80ffffff = 0x12;
+	// return *(volatile uint8_t*)0x80ffffff;
+	const uint32_t size = 0x100;
+	for(uint32_t i = 0; i < size; i++)*(volatile uint8_t*)(0x80000000+i) = i&0xff;
+	for(uint32_t i = 0; i < size; i+=1)if(*(volatile  uint8_t*)(0x80000000+i) != (i&0xff))return i;
+	for(uint32_t i = 0; i < size; i+=2)if(*(volatile uint16_t*)(0x80000000+i) != (((i+1)&0xff)<<8|(i&0xff)))return i;
+	for(uint32_t i = 0; i < size; i+=4)if(*(volatile uint32_t*)(0x80000000+i) != (((i+3)&0xff)<<24|((i+2)&0xff)<<16|((i+1)&0xff)<<8|(i&0xff)))return i;
+	return 0;
 }
