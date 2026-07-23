cafd6fc1618158162055aabb3bb3c72fbee880e5
tracer-ysyx <tracer@ysyx.org>
2026-07-23 09:57:26 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:57:26 up  2:07,  1 user,  load average: 0.13, 0.21, 0.25

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -55,7 +55,7 @@ void _bootloader() {//SSLB
 void _trm_init() {//FSLB
 	uint32_t* load=(uint32_t*)&_load_start_;
 	while((load-(uint32_t*)&_load_start_)<=(size_t)&_load_size_){
-		*(uint32_t*)load = (uint32_t)*(&_load_begin_ + (load - (uint32_t*)&_load_start_));
+		*load = *(&_load_begin_ + (load - (uint32_t*)&_load_start_));
 		load+=4;
 	}
 	_bootloader();
