b2df6662a9ce83d4c74540a76b9e96fe94aee2f3
tracer-ysyx <tracer@ysyx.org>
2026-07-24 17:47:44 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:47:44 up 10:02,  1 user,  load average: 0.25, 0.24, 0.27

--- am-kernels/tests/my-tests/src/sdram.c
+++ am-kernels/tests/my-tests/src/sdram.c
@@ -1,27 +1,27 @@
 #include "trap.h"
 int main(){
-	const uint32_t size = 0x1000;
+	const uint32_t size = 0x40;
 	const uint32_t addr = 0xa0000000;
-	*(volatile uint32_t*)(0xa0000000+0x40) = 0x76543210;
-	return *(volatile uint32_t*)(0xa0000000+0x40);
+	// *(volatile uint32_t*)(addr+size) = 0x76543210;
+	// return *(volatile uint32_t*)(addr+size);
 
 	// for(uint32_t i = 0; i < size; i++)		*(volatile uint8_t*)	(addr+i)  = i&0xff;
 	// for(uint32_t i = 0; i < size; i+=1)if(	*(volatile uint8_t*)	(addr+i) != (i&0xff))return i;
 	// for(uint32_t i = 0; i < size; i+=2)if(	*(volatile uint16_t*)	(addr+i) != (((i+1)&0xff)<<8|(i&0xff)))return i;
 	// for(uint32_t i = 0; i < size; i+=4)if(	*(volatile uint32_t*)	(addr+i) != (((i+3)&0xff)<<24|((i+2)&0xff)<<16|((i+1)&0xff)<<8|(i&0xff)))return i;
 	
-	// for(uint32_t i = 0; i < size; i+=4){
-	// 	*(volatile uint8_t*)(addr+i+0) = i&0xff;
-	// 	*(volatile uint8_t*)(addr+i+1) = (i+1)&0xff;
-	// 	*(volatile uint8_t*)(addr+i+2) = (i+2)&0xff;
-	// 	*(volatile uint8_t*)(addr+i+3) = (i+3)&0xff;
-	// 	if(*(volatile uint32_t*)(addr+i) != (((i+3)&0xff)<<24|((i+2)&0xff)<<16|((i+1)&0xff)<<8|(i&0xff)))return 1;
-	// 	if(*(volatile uint16_t*)(addr+i+0) != (((i+1)&0xff)<<8|((i+0)&0xff)))return 2;
-	// 	if(*(volatile uint16_t*)(addr+i+2) != (((i+3)&0xff)<<8|((i+2)&0xff)))return 3;
-	// 	if(*(volatile uint8_t*)(addr+i+0) != ((i+0)&0xff))return 4;
-	// 	if(*(volatile uint8_t*)(addr+i+1) != ((i+1)&0xff))return 5;
-	// 	if(*(volatile uint8_t*)(addr+i+2) != ((i+2)&0xff))return 6;
-	// 	if(*(volatile uint8_t*)(addr+i+3) != ((i+3)&0xff))return 7;
-	// }
+	for(uint32_t i = 0; i < size; i+=4){
+		*(volatile uint8_t*)(addr+i+0) = i&0xff;
+		*(volatile uint8_t*)(addr+i+1) = (i+1)&0xff;
+		*(volatile uint8_t*)(addr+i+2) = (i+2)&0xff;
+		*(volatile uint8_t*)(addr+i+3) = (i+3)&0xff;
+		if(*(volatile uint32_t*)(addr+i) != (((i+3)&0xff)<<24|((i+2)&0xff)<<16|((i+1)&0xff)<<8|(i&0xff)))return 1;
+		if(*(volatile uint16_t*)(addr+i+0) != (((i+1)&0xff)<<8|((i+0)&0xff)))return 2;
+		if(*(volatile uint16_t*)(addr+i+2) != (((i+3)&0xff)<<8|((i+2)&0xff)))return 3;
+		if(*(volatile uint8_t*)(addr+i+0) != ((i+0)&0xff))return 4;
+		if(*(volatile uint8_t*)(addr+i+1) != ((i+1)&0xff))return 5;
+		if(*(volatile uint8_t*)(addr+i+2) != ((i+2)&0xff))return 6;
+		if(*(volatile uint8_t*)(addr+i+3) != ((i+3)&0xff))return 7;
+	}
 	return 0;
 }
