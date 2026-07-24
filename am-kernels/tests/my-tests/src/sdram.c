518cedee37295d5af384b9d200b9caefd791f88c
tracer-ysyx <tracer@ysyx.org>
2026-07-24 14:44:04 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:44:04 up  7:04,  1 user,  load average: 0.71, 0.57, 0.39

--- am-kernels/tests/my-tests/src/sdram.c
+++ am-kernels/tests/my-tests/src/sdram.c
@@ -1,6 +1,12 @@
 #include "trap.h"
 int main(){
-	*(volatile uint32_t*)0xa0000000 = 0x76543210;
-	return *(volatile uint32_t*)0xa0000000;
+	// *(volatile uint32_t*)0xa0000000 = 0x76543210;
+	// return *(volatile uint32_t*)0xa0000000;
+	const uint32_t size = 0x1000;
+	const uint32_t addr = 0xa0000000;
+	for(uint32_t i = 0; i < size; i++)		*(volatile uint8_t*)	(addr+i)  = i&0xff;
+	for(uint32_t i = 0; i < size; i+=1)if(	*(volatile uint8_t*)	(addr+i) != (i&0xff))return i;
+	for(uint32_t i = 0; i < size; i+=2)if(	*(volatile uint16_t*)	(addr+i) != (((i+1)&0xff)<<8|(i&0xff)))return i;
+	for(uint32_t i = 0; i < size; i+=4)if(	*(volatile uint32_t*)	(addr+i) != (((i+3)&0xff)<<24|((i+2)&0xff)<<16|((i+1)&0xff)<<8|(i&0xff)))return i;
 	return 0;
 }
