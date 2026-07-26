b37033d37da3865564ea61e2acbddc8b12ab386f
tracer-ysyx <tracer@ysyx.org>
2026-07-27 07:25:08 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:25:08 up 33 min,  1 user,  load average: 0.86, 0.70, 0.49

--- am-kernels/tests/my-tests/src/gpio.c
+++ am-kernels/tests/my-tests/src/gpio.c
@@ -5,7 +5,7 @@ int main(){
 	*(volatile uint32_t*)(addr+0x8) = 0x76543210;
 	uint32_t water = 0b1010101010101010;
 	while(1){
-		for(int i=0;i<2000;i++);
+		for(int i=0;i<20000;i++);
 		*(volatile uint32_t*)(addr+0x0) = water;
 		uint32_t temp = water>>31;
 		water = water << 1;
