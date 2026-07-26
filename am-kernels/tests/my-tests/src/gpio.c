350b8f12698656e5717cf947b36b90c9e5f41b30
Bi RuiDe <1776427100@qq.com>
2026-07-27 07:21:14 +0800

    初步成功，准备试试流水灯

--- am-kernels/tests/my-tests/src/gpio.c
+++ am-kernels/tests/my-tests/src/gpio.c
@@ -3,6 +3,11 @@ int main(){
 	const uint32_t addr = 0x10002000;
 	*(volatile uint32_t*)(addr+0x0) = 0b1010101010101010;
 	*(volatile uint32_t*)(addr+0x8) = 0x76543210;
-	while(1);
+	uint32_t water = 0b1010101010101010;
+	while(1){
+		for(int i=0;i<50;i++);
+		*(volatile uint32_t*)(addr+0x0) = water;
+		water = water << 1;
+	}
 	return 0;
 }
