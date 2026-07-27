911f6c90eadcd76ca329738da2a00571c4d624df
Bi RuiDe <1776427100@qq.com>
2026-07-27 21:11:01 +0800

    尝试解决指令错误问题

--- npc/test/cpp/include/npcConfig.h
+++ npc/test/cpp/include/npcConfig.h
@@ -7,10 +7,10 @@
 // #define NPC_E_TRACE
 // #define NPC_D_TRACE
 #define NPC_NVBroad
-// #define NPC_MIN_TRACE
+#define NPC_MIN_TRACE
 // #define NPC_DIFFTEST
 // #define NPC_M_TRACE
 // #define NPC_WAVE
-const uint64_t NpcMinTraceBegin = 1352578000L;
+const uint64_t NpcMinTraceBegin = 464529400L;
 
 #endif
