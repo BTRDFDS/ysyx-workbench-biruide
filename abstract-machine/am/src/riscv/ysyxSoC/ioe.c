2682c84ff5878b6ce79e733f5c3e2495aaa6db25
Bi RuiDe <1776427100@qq.com>
2026-07-27 17:09:21 +0800

    禁用键盘

--- abstract-machine/am/src/riscv/ysyxSoC/ioe.c
+++ abstract-machine/am/src/riscv/ysyxSoC/ioe.c
@@ -14,6 +14,8 @@ void __am_uart_config(AM_UART_CONFIG_T *);
 void __am_uart_tx(AM_UART_TX_T *);
 void __am_uart_rx(AM_UART_RX_T *);
 
+static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
+
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
   [AM_TIMER_CONFIG] = __am_timer_config,
@@ -22,6 +24,7 @@ static void *lut[128] = {
   [AM_UART_CONFIG ] = __am_uart_config,
   [AM_UART_TX     ] = __am_uart_tx,
   [AM_UART_RX     ] = __am_uart_rx,
+  [AM_INPUT_CONFIG] = __am_input_config,
 };
 
 bool ioe_init() {
