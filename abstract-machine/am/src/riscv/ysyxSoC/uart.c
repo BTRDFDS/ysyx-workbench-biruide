4bf4cdf7ba1ecc6111d454347d6f08451f2bf511
tracer-ysyx <tracer@ysyx.org>
2026-07-27 16:45:01 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:45:01 up  9:31,  1 user,  load average: 0.66, 0.35, 0.15

--- abstract-machine/am/src/riscv/ysyxSoC/uart.c
+++ abstract-machine/am/src/riscv/ysyxSoC/uart.c
@@ -1,5 +1,5 @@
 #include <am.h>
-#include <nemu.h>
+#include <klib-macros.h>
 
 void __am_uart_init() {
 		*(volatile char *)(0x10000003L)=0b10000011;
@@ -16,5 +16,6 @@ void __am_uart_tx(AM_UART_TX_T *uart) {
 }
 
 void __am_uart_rx(AM_UART_RX_T *uart) {
-  uart->data = getch();
+	if(((*(volatile char *)(0x10000005L))&0b00000001)==1) uart->data =  (*(volatile char *)(0x10000000L))&0xff;
+	uart->data =  0xff;
 }
