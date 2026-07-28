b27cba271497f80745671593153241701c0d0824
tracer-ysyx <tracer@ysyx.org>
2026-07-28 21:02:20 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:02:20 up  8:24,  1 user,  load average: 1.06, 0.76, 0.46

--- ysyxSoC/perip/ps2/ps2_top_apb.v
+++ ysyxSoC/perip/ps2/ps2_top_apb.v
@@ -49,17 +49,18 @@ always_ff@(posedge clock) begin
 					ready <= 1'b1;
 
 					if(over==1)begin//上i一个是不是F0
+						// $strobe("key:%h over:%b",buffer[8:1],over);
 						over<=0;
 						fifo[w_ptr] <= 8'b0;
 					end else begin
 						fifo[w_ptr] <= buffer[8:1];  // kbd键盘 scan code
 						if(buffer[8:1]==8'hf0)begin
+							// $strobe("key:%h over:%b",fifo[w_ptr-1],over);
 							over<=1;
 						end
 					end
 					// $strobe("key: %b %h over:%b",fifo[w_ptr-1],fifo[w_ptr-1],over);
 					// $strobe("fifo",fifo[w_ptr-1]," ",fifo[r_ptr]," w_ptr:",w_ptr," r_ptr:",r_ptr," overflow:",overflow);
-
 
 				end
 				count <= 0;
