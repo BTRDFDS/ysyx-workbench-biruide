bef725d60982904b27e7ff3e9b8251e1fcf595fd
Bi RuiDe <1776427100@qq.com>
2026-07-28 14:02:35 +0800

    用nvborad试一下

--- npc/test/cpp/include/npcConfig.h
+++ npc/test/cpp/include/npcConfig.h
@@ -6,11 +6,11 @@
 // #define NPC_F_TRACE
 // #define NPC_E_TRACE
 // #define NPC_D_TRACE
-// #define NPC_NVBroad
-#define NPC_MIN_TRACE
+#define NPC_NVBroad
+// #define NPC_MIN_TRACE
 // #define NPC_DIFFTEST
-#define NPC_M_TRACE
-#define NPC_WAVE
+// #define NPC_M_TRACE
+// #define NPC_WAVE
 const uint64_t NpcMinTraceBegin = 464529400L;
 
 #endif
