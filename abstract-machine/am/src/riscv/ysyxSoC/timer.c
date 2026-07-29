ae0c9574599d551e1554fa338af8e7d8e3a284b8
tracer-ysyx <tracer@ysyx.org>
2026-07-29 13:11:18 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:11:18 up  5:05,  1 user,  load average: 1.58, 1.21, 1.04

---
+++ abstract-machine/am/src/riscv/ysyxSoC/timer.c
@@ -0,0 +1,27 @@
+#include <am.h>
+
+static uint64_t beginTime;
+void __am_timer_init() {
+  uint32_t timeU,timeD;
+  asm volatile("lw %0, 0(%1)" : "=r"(timeD) : "r"(0x0200BFF8));
+  asm volatile("lw %0, 0(%1)" : "=r"(timeU) : "r"(0x0200BFF8 + 4));
+  beginTime=((uint64_t)timeU << 32) | (uint64_t)timeD;
+}
+
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
+  uint64_t nowTime;
+  uint32_t timeU,timeD;
+  asm volatile("lw %0, 0(%1)" : "=r"(timeD) : "r"(0x0200BFF8));
+  asm volatile("lw %0, 0(%1)" : "=r"(timeU) : "r"(0x0200BFF8 + 4));
+  nowTime=((uint64_t)timeU << 32) | (uint64_t)timeD;
+  uptime->us=(nowTime-beginTime);//我需要尽可能快速
+}
+
+void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
+  rtc->second = 0;
+  rtc->minute = 0;
+  rtc->hour   = 0;
+  rtc->day    = 0;
+  rtc->month  = 0;
+  rtc->year   = 1900;
+}
