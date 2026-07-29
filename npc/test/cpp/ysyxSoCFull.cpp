fdaca1b5a1060802af43b953622b3633011ad6fb
Bi RuiDe <1776427100@qq.com>
2026-07-29 19:57:54 +0800

    不对，可能是我的CPU有问题

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -336,7 +336,7 @@ int main(int argc, char** argv) {
 	NpcInitDevice(argc, argv);
 	NpcInitMem(argc, argv);
 	{//初始化
-		for(int i=0;i<10;i++){
+		for(int i=0;i<12;i++){
 			top->clock=0;top->reset=1;top->eval();
 			top->clock=1;top->reset=1;top->eval();
 		}
@@ -348,8 +348,8 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 	printf("\033[1;32m Welcome to NPC[\033[1;36m%s %s\033[1;32m] \033[0m\n",__DATE__,__TIME__);
-	for(int i=0;(!contextp->gotFinish());i++){
-	// for(int i=0;i<100000000L&(!contextp->gotFinish());i++){
+	// for(int i=0;(!contextp->gotFinish());i++){
+	for(int i=0;i<4500L&(!contextp->gotFinish());i++){
 		#ifdef NPC_NVBroad
 			nvboard_update();
 		#endif
