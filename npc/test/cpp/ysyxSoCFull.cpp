ee06456991634eb014a206061c78e9716af9ef4c
Bi RuiDe <1776427100@qq.com>
2026-07-29 20:32:01 +0800

    第一次会卡住

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -337,8 +337,8 @@ int main(int argc, char** argv) {
 	NpcInitMem(argc, argv);
 	{//初始化
 		for(int i=0;i<12;i++){
-			top->clock=0;top->reset=1;top->eval();
-			top->clock=1;top->reset=1;top->eval();
+			NpcWave();top->clock=0;top->reset=1;top->eval();
+			NpcWave();top->clock=1;top->reset=1;top->eval();
 		}
 		top->clock=0;top->reset=0;top->eval();
 		runStep=0;
@@ -348,8 +348,8 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 	printf("\033[1;32m Welcome to NPC[\033[1;36m%s %s\033[1;32m] \033[0m\n",__DATE__,__TIME__);
-	for(int i=0;(!contextp->gotFinish());i++){
-	// for(int i=0;i<45000L&(!contextp->gotFinish());i++){
+	// for(int i=0;(!contextp->gotFinish());i++){
+	for(int i=0;i<500000L&(!contextp->gotFinish());i++){
 		#ifdef NPC_NVBroad
 			nvboard_update();
 		#endif
