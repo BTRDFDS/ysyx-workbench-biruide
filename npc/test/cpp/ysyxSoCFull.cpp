bed1fd569d68d9b1a64f2c76153026b0bc10c47f
Bi RuiDe <1776427100@qq.com>
2026-07-30 10:50:24 +0800

    添加IPC计算

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -45,65 +45,24 @@ const uint32_t flashAddr	=0x30000000;
 const uint32_t flashSize	=0x00ffffff;//flash极限地址是bfff_ffff
 uint8_t flash[flashSize];
 
-uint64_t runStep;
+uint64_t numCycle;
+uint64_t numInst;
 
 void NpcEbreak(int returnCode);
 void NpcRun(uint32_t times);
 void NpcReturn(const char* msg,int returnCode);
 void NpcWave();
 ////////////////////////////////////////////////////////////////////////////////////////
-/*
-extern "C" int pmem_read(int raddr) {
-	uint32_t raddrX=(uint32_t)raddr;
-	if(raddrX-psramAddr>=psramSize|raddrX<psramAddr|raddrX==0){return 0;}
-	uint32_t temp=
-		((uint32_t)psram[raddrX-psramAddr+0]<< 0)|
-		((uint32_t)psram[raddrX-psramAddr+1]<< 8)|
-		((uint32_t)psram[raddrX-psramAddr+2]<<16)|
-		((uint32_t)psram[raddrX-psramAddr+3]<<24);
-	// printf("Read addr= %x at T=%d => %x\n",raddrX,runStep,temp);
-	#ifdef NPC_WAVE
-	logFile<<"Read addr= "<<std::hex<<raddrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep<<" => "<<std::hex<<temp<<std::endl;
-	#endif
-	return temp;
-}
-extern "C" void pmem_write(int waddr, int wdata, char wmask) {
-	uint32_t addrX=(uint32_t)waddr;
-	#ifdef NPC_M_TRACE
-		logFile<<"write addr= "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep<<" "<<std::hex<<wdata<<" ="<<std::bitset<4>(wmask)<<"> ";
-	#endif
-	if(addrX==0x10000000){
-		printf("%c",wdata);
-		fflush(stdout);
-		return;
-	}
-	if((((addrX-psramAddr)>>2)>psramSize|addrX<=psramAddr)|(addrX==0)){
-		return;
-	}
-	uint32_t index = (addrX-psramAddr) & 0xfffffffc;
-	if(wmask&0b1000)psram[index+3]=(uint8_t)(wdata>>24);
-	if(wmask&0b0100)psram[index+2]=(uint8_t)(wdata>>16);
-	if(wmask&0b0010)psram[index+1]=(uint8_t)(wdata>> 8);
-	if(wmask&0b0001)psram[index+0]=(uint8_t)(wdata    );
-	uint32_t temp=
-		((uint32_t)psram[index+0]<< 0)|
-		((uint32_t)psram[index+1]<< 8)|
-		((uint32_t)psram[index+2]<<16)|
-		((uint32_t)psram[index+3]<<24);
-	#ifdef NPC_M_TRACE
-		logFile<<std::hex<<temp<<std::endl;
-	#endif
-}
-*/
 extern "C" int getRegPc(int addr);
 extern "C" void ebreak(){NpcReturn("\nebreak",getRegPc(10)!=0);}
 extern "C" void check(){
+	numInst++;
 	if(NpcDifftestCheck(getRegPc(0)))NpcReturn("difftest",-1);
 }
 extern "C" void flash_read(int32_t addr, int32_t *data) {
 	uint32_t addrX=((uint32_t)addr)&0xfffffffc;
 	#ifdef NPC_M_TRACE
-	logFile<<"flash	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep;
+	logFile<<"flash	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle;
 	#endif
 	// if(addrX-flashAddr>=flashSize|addrX<flashAddr){NpcReturn("flash read error",addrX);}
 	if(addrX>=flashSize)NpcReturn("flash read error",addrX);
@@ -127,16 +86,16 @@ extern "C" void mrom_read(int32_t addr, int32_t *data) {
 		((uint32_t)mrom[addrX-mromAddr+3]<<24);
 	*data=temp;
 	#ifdef NPC_M_TRACE
-	logFile<<"mrom	R "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep<<" => "<<std::hex<<temp<<std::endl;
+	logFile<<"mrom	R "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle<<" => "<<std::hex<<temp<<std::endl;
 	#endif
 }
 extern "C" int psram_read(int32_t addr){
 	uint32_t addrX=((uint32_t)addr)&0xfffffffc;
 	#if defined(NPC_M_TRACE)
-		logFile<<"psram	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep;
+		logFile<<"psram	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle;
 	#elif defined(NPC_MIN_TRACE)
-		// if(runStep >= NpcMinTraceBegin)logFile<<std::hex<<getRegPc(0)<<"\n";
-		// if((addr&0xfffffff0) == (0xa00164b4&0xfffffff0))logFile<<"psram	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep;
+		// if(numCycle >= NpcMinTraceBegin)logFile<<std::hex<<getRegPc(0)<<"\n";
+		// if((addr&0xfffffff0) == (0xa00164b4&0xfffffff0))logFile<<"psram	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle;
 	#endif
 	if(addrX>=psramSize)NpcReturn("psram read error",addrX);
 	uint32_t temp=
@@ -153,9 +112,9 @@ extern "C" int psram_read(int32_t addr){
 extern "C" void psram_write(int addr,int data){
 	uint32_t addrX=(uint32_t)addr;
 	#if defined(NPC_M_TRACE)
-		logFile<<"Psram	W "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep<<" "<<std::hex<<data<<" => ";
+		logFile<<"Psram	W "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle<<" "<<std::hex<<data<<" => ";
 	#elif defined(NPC_MIN_TRACE)
-		// if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0))logFile<<"Psram	W "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep<<" "<<std::hex<<data<<" => ";
+		// if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0))logFile<<"Psram	W "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle<<" "<<std::hex<<data<<" => ";
 	#endif
 	psram[addrX+0]=(uint8_t)(data&0xff);
 	#if defined(NPC_M_TRACE)
@@ -167,10 +126,10 @@ extern "C" void psram_write(int addr,int data){
 extern "C" int sdram_read(int32_t addr){
 	uint32_t addrX=((uint32_t)addr);
 	#if defined(NPC_M_TRACE)
-		logFile<<"sdram	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep;
+		logFile<<"sdram	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle;
 	#elif defined(NPC_MIN_TRACE)
-		if(runStep >= NpcMinTraceBegin)logFile<<"sdram	R "<<std::hex<<getRegPc(0)<<"\n";
-		if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0) || runStep >= NpcMinTraceBegin)logFile<<"sdram	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep;
+		if(numCycle >= NpcMinTraceBegin)logFile<<"sdram	R "<<std::hex<<getRegPc(0)<<"\n";
+		if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0) || numCycle >= NpcMinTraceBegin)logFile<<"sdram	R "<<std::hex<<addr<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle;
 	#endif
 	if(addrX>=sdramSize)NpcReturn("sdram read error",addrX);
 	uint32_t temp=
@@ -181,22 +140,22 @@ extern "C" int sdram_read(int32_t addr){
 	#ifdef NPC_M_TRACE
 		logFile<<" => "<<std::hex<<temp<<std::endl;
 	#elif defined(NPC_MIN_TRACE)
-		if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0) || runStep >= NpcMinTraceBegin)logFile<<" => "<<std::hex<<temp<<std::endl;
+		if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0) || numCycle >= NpcMinTraceBegin)logFile<<" => "<<std::hex<<temp<<std::endl;
 	#endif
 	return temp;
 }
 extern "C" void sdram_write(int addr,int data){
 	uint32_t addrX=(uint32_t)addr;
 	#if defined(NPC_M_TRACE)
-		logFile<<"sdram	W "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep<<" "<<std::hex<<data<<" => ";
+		logFile<<"sdram	W "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle<<" "<<std::hex<<data<<" => ";
 	#elif defined(NPC_MIN_TRACE)
-		if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0) || runStep >= NpcMinTraceBegin)logFile<<"sdram	W "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<runStep<<" "<<std::hex<<data<<" => ";
+		if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0) || numCycle >= NpcMinTraceBegin)logFile<<"sdram	W "<<std::hex<<addrX<<" at 0x "<<std::hex<<getRegPc(0)<<" T="<<std::dec<<numCycle<<" "<<std::hex<<data<<" => ";
 	#endif
 	sdram[addrX+0]=(uint8_t)(data&0xff);
 	#if defined(NPC_M_TRACE)
 		logFile<<std::hex<<(uint32_t)sdram[addrX+0]<<std::endl;
 	#elif defined(NPC_MIN_TRACE)
-		if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0) || runStep >= NpcMinTraceBegin)logFile<<std::hex<<(uint32_t)sdram[addrX+0]<<std::endl;
+		if((addr&0x00fffff0) == (0xa00164b4&0x00fffff0) || numCycle >= NpcMinTraceBegin)logFile<<std::hex<<(uint32_t)sdram[addrX+0]<<std::endl;
 	#endif
 }
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -299,7 +258,7 @@ void NpcWave(){
 		contextp->timeInc(1);
 		tfp->dump(contextp->time());
 	#elif defined(NPC_MIN_TRACE)
-	if(runStep >= NpcMinTraceBegin){
+	if(numCycle >= NpcMinTraceBegin){
 		contextp->timeInc(1);
 		tfp->dump(contextp->time());
 	}
@@ -313,7 +272,7 @@ void NpcReturn(const char* msg,int returnCode){
 	#ifdef NPC_NVBroad
 		nvboard_quit();
 	#endif
-	printf("%s runStep=%ld pc=0x %x\n",msg,runStep,getRegPc(0)-4);//实质上是已经是next pc了
+	printf("%s cycle=%ld inst=%ld IPC=%f pc=0x %x\n",msg,numCycle,numInst,(float)(numInst/numCycle),getRegPc(0)-4);//实质上是已经是next pc了
 	const char *regsName[] = {
 	"pc", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
 	"s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -341,7 +300,7 @@ int main(int argc, char** argv) {
 			top->clock=1;top->reset=1;top->eval();
 		}
 		top->clock=0;top->reset=0;top->eval();
-		runStep=0;
+		numCycle=0;
 	}
 	printf("\033[1;32m Welcome to NPC[\033[1;36m%s %s\033[1;32m] \033[0m\n",__DATE__,__TIME__);
 	for(int i=0;(!contextp->gotFinish());i++){
@@ -353,7 +312,7 @@ int main(int argc, char** argv) {
 		top->clock=1;top->eval();
 		NpcWave();
 		top->clock=0;top->eval();
-		runStep++;
+		numCycle++;
 	}
 	NpcReturn("ending",-1);
 }
