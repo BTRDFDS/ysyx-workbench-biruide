3651590cfd6993914e60b76c20a5c6eb53dcb607
tracer-ysyx <tracer@ysyx.org>
2026-07-30 10:58:52 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:58:52 up  4:06,  1 user,  load average: 0.79, 0.65, 0.37

--- am-kernels/tests/my-tests/src/putch.c
+++ am-kernels/tests/my-tests/src/putch.c
@@ -1,8 +1,8 @@
 #include <trap.h>
 int main(){
     putch('A');
-    putch('B');
-    putch('C');
-    putch('D');
-    putch('\n');
+    // putch('B');
+    // putch('C');
+    // putch('D');
+    // putch('\n');
 }
