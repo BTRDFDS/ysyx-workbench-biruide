7286ec9d0cb4dd3d1ccbfd733c6b5b4264c791b6
tracer-ysyx <tracer@ysyx.org>
2026-07-30 22:48:42 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:48:42 up  8:28,  1 user,  load average: 0.92, 0.62, 0.55

--- ysyxSoC/perip/amba/apb_delayer.v
+++ ysyxSoC/perip/amba/apb_delayer.v
@@ -25,7 +25,7 @@ module apb_delayer(
 );
 logic [31:0] cnt,data;
 logic has,done,err;
-localparam rs = 327;//5.1118*64
+localparam rs = 263;//(5.1118-1)*64
 // localparam s = 6;//2^6
 always_ff @(posedge clock) begin
 	if(reset | ~in_psel | ~in_penable)has <= 0;
