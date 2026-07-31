a49fc995244e6793cb9cde9785adee858371bf6b
tracer-ysyx <tracer@ysyx.org>
2026-07-31 14:43:29 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:43:29 up  7:16,  1 user,  load average: 1.71, 1.83, 1.61

--- npc/test/cpp/include/npcConfig.h
+++ npc/test/cpp/include/npcConfig.h
@@ -9,7 +9,7 @@
 // #define NPC_NVBroad
 // #define NPC_MIN_TRACE
 // #define NPC_DIFFTEST
-#define NPC_M_TRACE
+// #define NPC_M_TRACE
 // #define NPC_WAVE
 const uint64_t NpcMinTraceBegin = 464529400L;
 
