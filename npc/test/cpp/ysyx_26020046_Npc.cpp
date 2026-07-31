b06fa37e729544549a39259bfb8ee000c80b54ec
tracer-ysyx <tracer@ysyx.org>
2026-07-31 14:42:02 +0800

    > generate verilog for Npc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:42:02 up  7:15,  1 user,  load average: 1.67, 1.84, 1.59

--- npc/test/cpp/ysyx_26020046_Npc.cpp
+++ npc/test/cpp/ysyx_26020046_Npc.cpp
@@ -248,35 +248,11 @@ void NpcInitMem(int argc, char** argv){
 	fseek(file, 0, SEEK_END);
 	long fileSize = ftell(file);
 	fseek(file, 0, SEEK_SET);
-	size_t wordsRead = fread(flash, sizeof(uint8_t), fileSize/sizeof(uint8_t), file);
+	size_t wordsRead = fread(sdram, sizeof(uint8_t), fileSize/sizeof(uint8_t), file);
 	if(wordsRead!=fileSize/sizeof(uint8_t)){printf("can't read file\n");}
 	fclose(file);
-	// printf("fileSize=%lx\n",fileSize);
-	// printf("has open.fileSize=%lx\n",fileSize);
-	// for(int i=0;i<0x100;i+=4){//小段检查
-	// 	printf("%02x%02x%02x%02x ",flash[i+3],flash[i+2],flash[i+1],flash[i]);
-	// 	if(i%16==15)printf("\n");
-	// }
-	NpcDifftestInit8(flashSize,flash,flashAddr,"/home/biruide/ysyx-workbench/npc/test/cpp/lib/riscv32-nemu-interpreter-so-flash-sdram");
-	// for(uint32_t i=0;i<0x100;i++){
-	// 	flash[i]=i&0xff;
-	// }
-	// file =fopen("/home/biruide/ysyx-workbench/am-kernels/tests/my-tests/build/myTest_chat-riscv32i-ysyxsoc.bin","rb");
-	// if(file==NULL){printf("can't open myTest_chat\n");exit(-1);}
-	// fseek(file, 0, SEEK_END);
-	// fileSize = ftell(file);
-	// fseek(file, 0, SEEK_SET);
-	// wordsRead = fread(flash, sizeof(uint8_t), fileSize/sizeof(uint8_t), file);
-	// if(wordsRead!=fileSize/sizeof(uint8_t)){printf("can't read file\n");}
-	// fclose(file);
-	// for(uint32_t i=0;i<0x100;i+=4){
-	// 	uint32_t temp=
-	// 	((uint32_t)flash[i+0]<< 0)|
-	// 	((uint32_t)flash[i+1]<< 8)|
-	// 	((uint32_t)flash[i+2]<<16)|
-	// 	((uint32_t)flash[i+3]<<24);
-	// 	logFile<<std::hex<<temp<<std::endl;
-	// }
+	NpcDifftestInit8(sdramSize,sdram,sdramAddr,"/home/biruide/ysyx-workbench/npc/test/cpp/lib/riscv32-nemu-interpreter-so-flash-sdram");
+
 }
 void NpcDifftestGetGpr(uint32_t *gpr){
 	if(gpr==NULL){NpcReturn("difftest unable",-1);}
