a6df4444823692a52ec7c8930b2e0f34040ac526
tracer-ysyx <tracer@ysyx.org>
2026-08-01 08:46:28 +0800

    > generate verilog for Npc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:46:28 up  1:29,  1 user,  load average: 1.03, 0.58, 0.48

--- npc/src/ysyx_26020046_Npc.scala
+++ npc/src/ysyx_26020046_Npc.scala
@@ -3,12 +3,12 @@ import chisel3.util._
 import WidthConsts._
 //TODO:需要BlackBox
 //根据官网，blackbox已经被废弃了，使用ExtModule
-object MemStatus extends ChiselEnum{val Idle,Read,Write = Value}
+object NpcStatus extends ChiselEnum{val Idle,Read,Write = Value}
 class ysyx_26020046_Npc extends Module{
 	val PcInit:UInt=0x80000000L.U
 	val cpu = Module(new ysyx_26020046(PcInit))
 	val mem = Module(new ysyx_26020046_Mem())
-	val status = RegInit(MemStatus.Idle)
+	val status = RegInit(NpcStatus.Idle)
 	val rAddr = RegInit(0.U(32.W))
 	val wAddr = RegInit(0.U(32.W))
 	val wData = RegInit(0.U(32.W))
@@ -17,12 +17,12 @@ class ysyx_26020046_Npc extends Module{
 	val wDataValid = RegInit(false.B)
 	val writeValid = (wAddrValid||cpu.io.master.awvalid)&(wDataValid||cpu.io.master.wvalid)
 	switch(status){
-		is(MemStatus.Idle){
-			when(cpu.io.master.arvalid)	{status := MemStatus.Read}
-			.elsewhen(writeValid)		{status := MemStatus.Write}
+		is(NpcStatus.Idle){
+			when(cpu.io.master.arvalid)	{status := NpcStatus.Read}
+			.elsewhen(writeValid)		{status := NpcStatus.Write}
 		}
-		is(MemStatus.Read)	{when(cpu.io.master.rready){status := MemStatus.Idle}}
-		is(MemStatus.Write)	{when(cpu.io.master.bready){status := MemStatus.Idle}}
+		is(NpcStatus.Read)	{when(cpu.io.master.rready){status := NpcStatus.Idle}}
+		is(NpcStatus.Write)	{when(cpu.io.master.bready){status := NpcStatus.Idle}}
 	}
 	when(cpu.io.master.arvalid && cpu.io.master.araddr(31,28) =/= 0x8.U(4.W)){
 		printf("npc mem error rAddr:%x\n",cpu.io.master.araddr)
@@ -35,38 +35,38 @@ class ysyx_26020046_Npc extends Module{
 		printf("npc mem error wAddr:%x\n",cpu.io.master.awaddr)
 		stop()
 	}
-	cpu.io.master.arready := status === MemStatus.Idle
-	cpu.io.master.rdata	:= Mux(status === MemStatus.Read,mem.read.data,0.U)
+	cpu.io.master.arready := status === NpcStatus.Idle
+	cpu.io.master.rdata	:= Mux(status === NpcStatus.Read,mem.read.data,0.U)
 	cpu.io.master.rresp	:= 0.U//OKAY
-	cpu.io.master.rvalid	:= status === MemStatus.Read
-	when(status === MemStatus.Idle & cpu.io.master.arvalid){rAddr := cpu.io.master.araddr}
+	cpu.io.master.rvalid	:= status === NpcStatus.Read
+	when(status === NpcStatus.Idle & cpu.io.master.arvalid){rAddr := cpu.io.master.araddr}
 
-	cpu.io.master.awready	:= status === MemStatus.Idle
-	cpu.io.master.wready	:= status === MemStatus.Idle
+	cpu.io.master.awready	:= status === NpcStatus.Idle
+	cpu.io.master.wready	:= status === NpcStatus.Idle
 	cpu.io.master.bresp	:= 0.U//OKAY
-	cpu.io.master.bvalid	:= status === MemStatus.Write
-	when(status === MemStatus.Idle & cpu.io.master.awvalid){
+	cpu.io.master.bvalid	:= status === NpcStatus.Write
+	when(status === NpcStatus.Idle & cpu.io.master.awvalid){
 		wAddr := cpu.io.master.awaddr
 		wAddrValid := true.B
 	}
-	when(status === MemStatus.Write & cpu.io.master.bready){
+	when(status === NpcStatus.Write & cpu.io.master.bready){
 		wAddrValid := false.B
 		wDataValid := false.B
 	}
-	when(status === MemStatus.Idle & cpu.io.master.wvalid){
+	when(status === NpcStatus.Idle & cpu.io.master.wvalid){
 		wData := cpu.io.master.wdata
 		wStrb := cpu.io.master.wstrb
 		wDataValid := true.B
 	}
 
-	mem.read.valid 	:= status === MemStatus.Read
+	mem.read.valid 	:= status === NpcStatus.Read
 	mem.read.addr  	:= rAddr
-	mem.write.valid := status === MemStatus.Write && wAddr(31,28) === 0x8.U(4.W)
+	mem.write.valid := status === NpcStatus.Write && wAddr(31,28) === 0x8.U(4.W)
 	mem.write.addr  := wAddr
 	mem.write.strb  := wStrb
 	mem.write.data  := wData
 
-	when(status === MemStatus.Write && wAddr === 0x10000000.U(32.W)){printf("%c",wData(7,0))}
+	when(status === NpcStatus.Write && wAddr === 0x10000000.U(32.W)){printf("%c",wData(7,0))}
 
 	cpu.io.master.rid	:= 0.U
 	cpu.io.master.rlast	:= false.B
