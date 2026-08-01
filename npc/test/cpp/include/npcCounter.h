f28e4247797592bd1fde62ee0da8c4aa2c5248f0
tracer-ysyx <tracer@ysyx.org>
2026-08-01 12:14:42 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:14:42 up  4:57,  1 user,  load average: 0.38, 0.66, 0.47

--- npc/test/cpp/include/npcCounter.h
+++ npc/test/cpp/include/npcCounter.h
@@ -47,9 +47,10 @@ extern "C" void lsuStoreWait()	{numLsuStoreWait++;	}
 
 void printCounter(){
 	printf("cycle=%ld inst=%ld IPC=%f\n",numCycle,numInst,(float)(((float)numInst)/((float)numCycle)));//实质上是已经是next pc了
-	printf("ich hit= %ld miss= %ld HpA= %f MpA= %f AMAT =%f\n",numIchHit,numIchMiss,
+	printf("ich hit= %ld miss= %ld Hp= %f Mp= %f Access= %ld Penalty= %ldAMAT =%f\n",numIchHit,numIchMiss,
 	(float)((float)numIchHit)/((float)numIfuInst),
 	(float)((float)numIchMiss)/((float)numIfuInst),
+	numIchAccess,numIchPenalty,
 	((float)numIchAccess/(float)numIchHit)+((float)numIchPenalty/(float)numIchMiss)
 	);
 	printf("ifu inst = %ld wait= %ld WpI= %f jump= %ld for= %ld back= %ld fpj= %f bpj= %f\n",
