e075bf0a879c1ca4cd55d83b137ce2b16260d2be
tracer-ysyx <tracer@ysyx.org>
2026-08-01 12:22:39 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:22:39 up  5:05,  1 user,  load average: 0.28, 0.37, 0.42

--- npc/test/cpp/include/npcCounter.h
+++ npc/test/cpp/include/npcCounter.h
@@ -46,12 +46,12 @@ extern "C" void lsuStore()		{numLsuStore++;		}
 extern "C" void lsuStoreWait()	{numLsuStoreWait++;	}
 
 void printCounter(){
-	printf("cycle=%ld inst=%ld IPC=%f\n",numCycle,numInst,(float)(((float)numInst)/((float)numCycle)));//实质上是已经是next pc了
-	printf("ich hit= %ld miss= %ld Hp= %f Mp= %f Access= %ld Penalty= %ldAMAT =%f\n",numIchHit,numIchMiss,
+	printf("cycle=%ld inst=%ld IPC=%f CPI= %f\n",numCycle,numInst,((float)numInst)/((float)numCycle),((float)numCycle)/((float)numInst));//实质上是已经是next pc了
+	printf("ich hit= %ld miss= %ld Hp= %f Mp= %f Access= %ld Penalty= %ld AMAT =%f\n",numIchHit,numIchMiss,
 	(float)((float)numIchHit)/((float)numIfuInst),
 	(float)((float)numIchMiss)/((float)numIfuInst),
 	numIchAccess,numIchPenalty,
-	((float)numIchAccess/(float)numIchHit)+((float)numIchPenalty/(float)numIchMiss)
+	(numIchAccess+numIchPenalty)/((float)numIfuInst)
 	);
 	printf("ifu inst = %ld wait= %ld WpI= %f jump= %ld for= %ld back= %ld fpj= %f bpj= %f\n",
 		numIfuInst,numIfuStall,(float)((float)numIfuStall)/((float)numIfuInst),
