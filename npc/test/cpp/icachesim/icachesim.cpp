d55146aa2633be2080dd1ec979c1cdda13b610e7
Bi RuiDe <1776427100@qq.com>
2026-08-02 22:45:39 +0800

    批量测试

--- npc/test/cpp/icachesim/icachesim.cpp
+++ npc/test/cpp/icachesim/icachesim.cpp
@@ -9,17 +9,19 @@
 // const uint32_t num=1<<bit;//2^bit
 uint32_t bolckTag[0x100]{};
 uint32_t bolckValid[0x100]{};
-uint32_t data[0x20000000];
-uint64_t end=0;
+std::fstream file;
 int main() {
-	std::fstream file("./bin/microbench-train.bin", std::ios::in | std::ios::binary);
-	if (!file.is_open()) {printf("Failed to open file\n");return -1;}
-	for(;file.read((char*)&data[end], sizeof(uint32_t));end++);
-	for(uint32_t size=2;size<4;size++){
-		for(uint32_t bit=0;bit<4;bit++){
+	for(uint32_t size=2;size<=4;size++){
+		for(uint32_t bit=2;bit<=4;bit++){
+			file.open("./bin/microbench-train.bin", std::ios::in | std::ios::binary);
+			if (!file.is_open()) {printf("Failed to open file\n");return -1;}
+			uint32_t pc;
 			uint64_t hit=0;
-			for(uint64_t i=0;i<end;i++){
-				uint32_t pc = data[i];
+			for(uint64_t cnt=0;;cnt++){
+				if(!file.read((char*)&pc, sizeof(pc))){
+					printf("%dB\t%dW\t:cnt= %ld hit= %ld Hp= %f\n",1<<bit,1<<size,cnt,hit,(float)hit/cnt);
+					break;
+				}
 				uint32_t index = (pc>>(size))&((1<<bit)-1);
 				uint32_t tag = pc>>(size+bit)&((1<<(32-bit-size))-1);
 				if(bolckValid[index] && bolckTag[index]==tag)hit++;
@@ -28,7 +30,7 @@ int main() {
 					bolckValid[index]=true;
 				}
 			}
-			printf("%dBlock%d :cnt= %ld hit= %ld Hp= %f\n",1<<bit,1<<size,end,hit,(float)hit/end);
+			file.close();
 		}
 	}
 }
