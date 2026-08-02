26b4b074ec3fbe3b9a5e5db55fbe41734589de33
tracer-ysyx <tracer@ysyx.org>
2026-08-03 07:28:07 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:28:07 up 43 min,  1 user,  load average: 0.89, 0.62, 0.44

--- ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi.v
+++ ysyxSoC/perip/sdram/core_sdram_axi4/sdram_axi.v
@@ -73,9 +73,9 @@ module sdram_axi
     ,output          sdram_ras_o
     ,output          sdram_cas_o
     ,output          sdram_we_o
-    ,output [  2:0]  sdram_dqm_o
+    ,output [  3:0]  sdram_dqm_o
     ,output [ 12:0]  sdram_addr_o
-    ,output [  3:0]  sdram_ba_o
+    ,output [  2:0]  sdram_ba_o
     ,output [ 31:0]  sdram_data_output_o
     ,output          sdram_data_out_en_o
 );
