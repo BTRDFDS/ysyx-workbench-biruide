372fa09f1e25816ab37756f669088f6fe162d4b1
tracer-ysyx <tracer@ysyx.org>
2026-08-02 10:54:00 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:54:00 up  2:50,  1 user,  load average: 1.32, 1.23, 1.19

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -22,7 +22,7 @@ void NpcDifftestGetGpr(uint32_t *gpr){
 	}
 ////////////////////////////////////////////////////////////////////////////////////////
 void NpcInitDeviceMem(int argc, char** argv){
-	deviceInit(argc, argv,"TOP.ysyxSoCFull.asic.cpu.cpu.wbu.wbuChk")
+	deviceInit(argc, argv,"TOP.ysyxSoCFull.asic.cpu.cpu.wbu.wbuChk");
 	top = new VysyxSoCFull{contextp};
 	#if defined(NPC_WAVE)  || defined(NPC_MIN_TRACE)
 		Verilated::traceEverOn(true);
