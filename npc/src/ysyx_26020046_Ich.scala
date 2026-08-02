c45ac9325536319ff42fc2dc81601ff14d7432e4
tracer-ysyx <tracer@ysyx.org>
2026-08-02 21:01:28 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:01:28 up 12:32,  1 user,  load average: 3.21, 2.57, 2.12

--- npc/src/ysyx_26020046_Ich.scala
+++ npc/src/ysyx_26020046_Ich.scala
@@ -10,8 +10,9 @@ class ysyx_26020046_Ich(val Yosys:Boolean=false) extends Module {
 	val ifu = IO(Flipped(new LoaderBus(BitWidth-2)))
 	val bar = IO(new LoaderBus())
 
-	val CacheBit	= 5
+	val CacheBit	= 4
 	val CacheNum    = 1 << CacheBit
+	val CacheSiz	
 
 	val data	= Reg(Vec(CacheNum, UInt(BitWidth.W)))
 	val tag		= Reg(Vec(CacheNum, UInt((BitWidth-2-CacheBit).W)))
