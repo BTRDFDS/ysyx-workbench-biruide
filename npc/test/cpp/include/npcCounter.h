b7f84e7419b0d7443dc7fdd7efa5875ec8ed1fd7
tracer-ysyx <tracer@ysyx.org>
2026-08-02 11:00:21 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:00:21 up  2:56,  1 user,  load average: 1.30, 1.43, 1.33

--- npc/test/cpp/include/npcCounter.h
+++ npc/test/cpp/include/npcCounter.h
@@ -102,12 +102,6 @@ VerilatedContext* contextp;//verilator上下文
 	#include "verilated_fst_c.h"
 	VerilatedFstC* tfp;//波形文件
 	#endif
-void deviceInit(int argc, char** argv,const char* scopeAddr){
-	contextp = new VerilatedContext;
-	contextp->commandArgs(argc, argv);
-	scope=svGetScopeFromName(scopeAddr);
-	svSetScope(scope);
-}
 void NpcWave(){
 	#ifdef NPC_WAVE
 		contextp->timeInc(1);
--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -22,8 +22,10 @@ void NpcDifftestGetGpr(uint32_t *gpr){
 	}
 ////////////////////////////////////////////////////////////////////////////////////////
 void NpcInitDeviceMem(int argc, char** argv){
-	// printf("ASD\n");
-	deviceInit(argc, argv,"TOP.ysyxSoCFull.asic.cpu.cpu.wbu.wbuChk");
+	contextp = new VerilatedContext;
+	contextp->commandArgs(argc, argv);
+	scope=svGetScopeFromName(scopeAddr);
+	svSetScope(scope);
 	top = new VysyxSoCFull{contextp};
 	#if defined(NPC_WAVE)  || defined(NPC_MIN_TRACE)
 		Verilated::traceEverOn(true);
