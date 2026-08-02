83af439dd89d3d90f115fc54147a3e8946372a6e
tracer-ysyx <tracer@ysyx.org>
2026-08-02 12:00:39 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:00:39 up  3:53,  1 user,  load average: 2.09, 2.33, 2.27

--- npc/test/cpp/include/npcCounter.h
+++ npc/test/cpp/include/npcCounter.h
@@ -96,15 +96,17 @@ void logFileClose(){
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 #if defined(NPC_I_PC_TRACE)
-	std::fstream iPcTraceFile;//输出日志文件：
+	std::fstream iPcTraceFile;
+	#include <cerrno> 
 	#endif
 void iPcTraceFileInit(){
 	#if defined(NPC_I_PC_TRACE)
 		iPcTraceFile.open("./iPcTrace.log",std::ios::binary);
 		if(!iPcTraceFile.is_open()) {
+			std::perror("Failed to open iPcTrace file");
 		printf("Failed to open iPcTrace file!\n");
 		exit(-1);
-	}
+		}
 	#endif
 	}
 void iPcTraceFileWrite(uint32_t pc){
