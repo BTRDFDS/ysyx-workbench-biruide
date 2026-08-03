1f77f0318671c9f5eeecfd6aa41b4f5aaa5978bd
tracer-ysyx <tracer@ysyx.org>
2026-08-03 14:17:49 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:17:49 up  7:13,  1 user,  load average: 0.28, 0.24, 0.21

--- ysyxSoC/perip/sdram/sdram.v
+++ ysyxSoC/perip/sdram/sdram.v
@@ -120,7 +120,7 @@ module sdramCore(
 			case(state)
 				Idle	:cnt <= code==Write?3'b0:mode.Latency[2:0];
 				Wait	:cnt <= (cnt<mode.Latency[2:0]) ? cnt+3'b1 : 3'b0;
-				Burst	:cnt <= cnt+3'b1;
+				Burst	:cnt <= (code==BurstStop || cnt==burstLen) ?mode.Latency[2:0]:cnt+3'b1;
 				Done	:cnt <= 3'b1;
 			endcase
 			if(state==Idle)begin
