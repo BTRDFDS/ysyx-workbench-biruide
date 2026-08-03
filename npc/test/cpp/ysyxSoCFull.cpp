ab3bd6320102ec19303f62e001a71694eb1599fb
Bi RuiDe <1776427100@qq.com>
2026-08-03 18:12:27 +0800

    修正假rDone的问题

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -107,7 +107,7 @@ int main(int argc, char** argv) {
 	}
 	printf("\033[1;32m Welcome to ysyxSoCFull[\033[1;36m%s %s\033[1;32m] \033[0m\n",__DATE__,__TIME__);
 	// for(int i=0;(!contextp->gotFinish());i++){
-	for(int i=0;i<500000&(!contextp->gotFinish());i++){
+	for(int i=0;i<300&(!contextp->gotFinish());i++){
 		#ifdef NPC_NVBroad
 			nvboard_update();
 		#endif
