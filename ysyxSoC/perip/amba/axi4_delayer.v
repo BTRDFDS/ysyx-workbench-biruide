9f2c90bb78e3594c79ccb8253bf2880dd45c5113
tracer-ysyx <tracer@ysyx.org>
2026-08-03 21:07:18 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:07:18 up 13:52,  1 user,  load average: 0.14, 0.29, 0.42

--- ysyxSoC/perip/amba/axi4_delayer.v
+++ ysyxSoC/perip/amba/axi4_delayer.v
@@ -95,7 +95,7 @@ module axi4_delayer(
 
 `define Delay
 `ifdef Delay
-localparam rs = 327;//(5.1118-1)*64//TODO
+localparam rs = 654;//(5.1118-1)*64//TODO
 // logic [31:0] wCnt;
 // logic wHas,wDone;
 // assign wDone = wHas & (wCnt[31:6] == '0);
