719b1e7beaf19541c2c6b820683ca01c875397f7
Bi RuiDe <1776427100@qq.com>
2026-08-03 18:31:50 +0800

    一个更新的w计数器

--- ysyxSoC/perip/amba/axi4_delayer.v
+++ ysyxSoC/perip/amba/axi4_delayer.v
@@ -94,22 +94,59 @@ module axi4_delayer(
 	// assign in_bresp = out_bresp;
 
 
-logic [31:0] wCnt;
-logic wHas,wDone;
+// logic [31:0] wCnt;
+// logic wHas,wDone;
 localparam rs = 30;//(5.1118-1)*64//TODO
-assign wDone = wHas & (wCnt[31:6] == '0);
-always_ff @(posedge clock) begin
-	if(reset | (out_bvalid & in_bready & wDone))wHas <= 0;
-	else if(out_bvalid & wHas==0)wHas <= 1;
+// assign wDone = wHas & (wCnt[31:6] == '0);
+// always_ff @(posedge clock) begin
+// 	if(reset | (out_bvalid & in_bready & wDone))wHas <= 0;
+// 	else if(out_bvalid & wHas==0)wHas <= 1;
 
-	if(reset | (out_bvalid & in_bready & wDone))wCnt[31:6] <= 'd0;
-	else if(wHas)wCnt[31:6]<=(wCnt[31:6]=='d0)?'d0:(wCnt[31:6]-'d1);
-	else wCnt <= wCnt + rs;
+// 	if(reset | (out_bvalid & in_bready & wDone))wCnt[31:6] <= 'd0;
+// 	else if(wHas)wCnt[31:6]<=(wCnt[31:6]=='d0)?'d0:(wCnt[31:6]-'d1);
+// 	else wCnt <= wCnt + rs;
+// end
+// assign out_bready	= wDone?in_bready	:'b0;
+// assign in_bvalid	= wDone?out_bvalid	:'b0;
+// assign in_bid		= wDone?out_bid		:'b0;
+// assign in_bresp		= wDone?out_bresp	:'b0;
+logic [31:0]	wRcnt;
+logic [25:0]	wAcnt,wFcnt;
+logic wHasAddr,wHasData,wDone;
+always_ff@(posedge clock)begin
+	if(reset)begin
+		wHasAddr<=0;
+		wHasData<=0;
+	end else if(wDone)begin
+		wHasAddr<=0;
+		wHasData<=0;
+	end else if(wFcnt==wAcnt)begin
+		wHasAddr<=0;
+		wHasData<=0;
+	end else begin
+		if(out_awready & in_awvalid)wHasAddr<=1;
+		if(out_wready  & in_wvalid )wHasData<=1;
+	end
+	if(reset | ~wHasAddr | ~wHasData)begin
+		wRcnt	<= 'd0;
+		wAcnt	<= 'd0;
+		wFcnt	<= 'd0;
+		wDone	<= 'd0;
+	end else begin
+		if(out_bvalid)wFcnt <= wRcnt[31:6];
+		if(wFcnt==wAcnt)begin
+			wDone <= 1;
+			wRcnt <='0;
+			wAcnt <='0;
+		end else begin
+			wDone <=0;
+			wRcnt <= wRcnt + rs;
+			wAcnt <= wAcnt + 'd1;
+		end
+	end
 end
-assign out_bready	= wDone?in_bready	:'b0;
-assign in_bvalid	= wDone?out_bvalid	:'b0;
-assign in_bid		= wDone?out_bid		:'b0;
-assign in_bresp		= wDone?out_bresp	:'b0;
+
+
 
 logic [31:0]	rRcnt;
 logic [25:0]	rAcnt;
