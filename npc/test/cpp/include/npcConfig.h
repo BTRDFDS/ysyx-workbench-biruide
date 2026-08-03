d77a94ed9541e6dd3b9f1547a34039d01aa6c39c
Bi RuiDe <1776427100@qq.com>
2026-08-03 21:54:08 +0800

    悬案:写状态机卡死了

--- npc/test/cpp/include/npcConfig.h
+++ npc/test/cpp/include/npcConfig.h
@@ -10,8 +10,8 @@
 // #define NPC_I_PC_TRACE
 // #define NPC_MIN_TRACE
 // #define NPC_DIFFTEST
-// #define NPC_M_TRACE
-// #define NPC_WAVE
+#define NPC_M_TRACE
+#define NPC_WAVE
 const uint64_t NpcMinTraceBegin = 464529400L;
 
 #endif
