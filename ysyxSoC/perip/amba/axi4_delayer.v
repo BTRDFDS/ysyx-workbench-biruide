7624bf76de8671bdffb115bcf2445e3a85f6a139
tracer-ysyx <tracer@ysyx.org>
2026-08-03 16:36:18 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:36:18 up  9:27,  1 user,  load average: 1.04, 0.58, 0.46

--- ysyxSoC/perip/amba/axi4_delayer.v
+++ ysyxSoC/perip/amba/axi4_delayer.v
@@ -70,12 +70,12 @@ module axi4_delayer(
 	assign out_arlen = in_arlen;
 	assign out_arsize = in_arsize;
 	assign out_arburst = in_arburst;
-	// assign out_rready = in_rready;
-	// assign in_rvalid = out_rvalid;
-	// assign in_rid = out_rid;
-	// assign in_rdata = out_rdata;
-	// assign in_rresp = out_rresp;
-	// assign in_rlast = out_rlast;
+	assign out_rready = in_rready;
+	assign in_rvalid = out_rvalid;
+	assign in_rid = out_rid;
+	assign in_rdata = out_rdata;
+	assign in_rresp = out_rresp;
+	assign in_rlast = out_rlast;
 	assign in_awready = out_awready;
 	assign out_awvalid = in_awvalid;
 	assign out_awid = in_awid;
@@ -88,75 +88,75 @@ module axi4_delayer(
 	assign out_wdata = in_wdata;
 	assign out_wstrb = in_wstrb;
 	assign out_wlast = in_wlast;
-	// assign out_bready = in_bready;
-	// assign in_bvalid = out_bvalid;
-	// assign in_bid = out_bid;
-	// assign in_bresp = out_bresp;
+	assign out_bready = in_bready;
+	assign in_bvalid = out_bvalid;
+	assign in_bid = out_bid;
+	assign in_bresp = out_bresp;
 
 
-logic [31:0] wCnt;
-logic wHas,wDone;
-localparam rs = 263;
-assign wDone = wHas & (wCnt[31:6] == '0);
-always_ff @(posedge clock) begin
-	if(reset | (out_bvalid & in_bready & wDone))wHas <= 0;
-	else if(out_bvalid & wHas==0)wHas <= 1;
+// logic [31:0] wCnt;
+// logic wHas,wDone;
+// localparam rs = 263;
+// assign wDone = wHas & (wCnt[31:6] == '0);
+// always_ff @(posedge clock) begin
+// 	if(reset | (out_bvalid & in_bready & wDone))wHas <= 0;
+// 	else if(out_bvalid & wHas==0)wHas <= 1;
 
-	if(reset | (out_bvalid & in_bready & wDone))wCnt[31:6] <= 'd0;
-	else if(wHas)wCnt[31:6]<=(wCnt[31:6]=='d0)?'d0:(wCnt[31:6]-'d1);
-	else wCnt <= wCnt + rs;
-end
-assign out_bready	= wDone?in_bready	:'b0;
-assign in_bvalid	= wDone?out_bvalid	:'b0;
-assign in_bid		= wDone?out_bid		:'b0;
-assign in_bresp		= wDone?out_bresp	:'b0;
+// 	if(reset | (out_bvalid & in_bready & wDone))wCnt[31:6] <= 'd0;
+// 	else if(wHas)wCnt[31:6]<=(wCnt[31:6]=='d0)?'d0:(wCnt[31:6]-'d1);
+// 	else wCnt <= wCnt + rs;
+// end
+// assign out_bready	= wDone?in_bready	:'b0;
+// assign in_bvalid	= wDone?out_bvalid	:'b0;
+// assign in_bid		= wDone?out_bid		:'b0;
+// assign in_bresp		= wDone?out_bresp	:'b0;
 
-logic [31:0]	rRcnt;
-logic [25:0]	rAcnt;
-logic [2:0]		rIcnt,rOcnt;
-logic [25:0]	rFcnt		[7:0];
-logic [31:0]	rFifoData	[7:0];
-logic [1:0]		rFifoResp	[7:0];
-logic 			rFifoLast	[7:0];
-logic [3:0]		rFifoId		[7:0];
-logic rDone;
-always_ff@(posedge clock)begin
-	if(reset)begin
-		rRcnt <= 'd0;
-		rAcnt <= 'd0;
-		rIcnt <= 'd0;
-		rOcnt <= 'd0;
-	end else begin
-		if(out_rvalid)begin
-			rFcnt[rIcnt] <= rRcnt[31:6];
-			rFifoData[rIcnt] <= out_rdata;
-			rFifoResp[rIcnt] <= out_rresp;
-			rFifoLast[rIcnt] <= out_rlast;
-			rFifoId	 [rIcnt] <= out_rid;
-			rIcnt <= rIcnt + 'd1;
-		end
-		if(rFcnt[rOcnt]==rAcnt)begin
-			rDone <= 1;
-			if(rFifoLast[rOcnt])rOcnt <= rIcnt;
-			else				rOcnt <= rOcnt +'d1;
-		end else begin
-			rDone <= 0;
-		end
-		if(rFcnt[rOcnt]==rAcnt & rFifoLast[rOcnt])begin
-			rRcnt <= 'd0;
-			rAcnt <= 'd0;
-		end else begin
-			rRcnt <= rRcnt+rs;
-			rAcnt <= rAcnt+'d1;
-		end
-	end
-end
-	assign out_rready	= in_rready;
+// logic [31:0]	rRcnt;
+// logic [25:0]	rAcnt;
+// logic [2:0]		rIcnt,rOcnt;
+// logic [25:0]	rFcnt		[7:0];
+// logic [31:0]	rFifoData	[7:0];
+// logic [1:0]		rFifoResp	[7:0];
+// logic 			rFifoLast	[7:0];
+// logic [3:0]		rFifoId		[7:0];
+// logic rDone;
+// always_ff@(posedge clock)begin
+// 	if(reset)begin
+// 		rRcnt <= 'd0;
+// 		rAcnt <= 'd0;
+// 		rIcnt <= 'd0;
+// 		rOcnt <= 'd0;
+// 	end else begin
+// 		if(out_rvalid)begin
+// 			rFcnt[rIcnt] <= rRcnt[31:6];
+// 			rFifoData[rIcnt] <= out_rdata;
+// 			rFifoResp[rIcnt] <= out_rresp;
+// 			rFifoLast[rIcnt] <= out_rlast;
+// 			rFifoId	 [rIcnt] <= out_rid;
+// 			rIcnt <= rIcnt + 'd1;
+// 		end
+// 		if(rFcnt[rOcnt]==rAcnt)begin
+// 			rDone <= 1;
+// 			if(rFifoLast[rOcnt])rOcnt <= rIcnt;
+// 			else				rOcnt <= rOcnt +'d1;
+// 		end else begin
+// 			rDone <= 0;
+// 		end
+// 		if(rFcnt[rOcnt]==rAcnt & rFifoLast[rOcnt])begin
+// 			rRcnt <= 'd0;
+// 			rAcnt <= 'd0;
+// 		end else begin
+// 			rRcnt <= rRcnt+rs;
+// 			rAcnt <= rAcnt+'d1;
+// 		end
+// 	end
+// end
+// 	assign out_rready	= in_rready;
 
-	assign in_rvalid	= rDone;
-	assign in_rid		= rDone?rFifoId		[rOcnt-1'd1]:'d0;
-	assign in_rdata		= rDone?rFifoData	[rOcnt-1'd1]:'d0;
-	assign in_rresp		= rDone?rFifoResp	[rOcnt-1'd1]:'d0;
-	assign in_rlast		= rDone?rFifoLast	[rOcnt-1'd1]:'b0;
+// 	assign in_rvalid	= rDone;
+// 	assign in_rid		= rDone?rFifoId		[rOcnt-1'd1]:'d0;
+// 	assign in_rdata		= rDone?rFifoData	[rOcnt-1'd1]:'d0;
+// 	assign in_rresp		= rDone?rFifoResp	[rOcnt-1'd1]:'d0;
+// 	assign in_rlast		= rDone?rFifoLast	[rOcnt-1'd1]:'b0;
 
 endmodule
