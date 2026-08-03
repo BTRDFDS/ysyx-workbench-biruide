71ba8560a517b4092dc50a181dcc5e5f8618812a
tracer-ysyx <tracer@ysyx.org>
2026-08-03 20:19:25 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:19:25 up 13:05,  1 user,  load average: 0.34, 0.65, 0.62

--- npc/src/ysyx_26020046_Ich.scala
+++ npc/src/ysyx_26020046_Ich.scala
@@ -28,8 +28,9 @@ class ysyx_26020046_Ich(val Yosys:Boolean=false) extends Module {
 	addrTag 	:= ifu.addr(BitWidth-2-1,CacheBit+CacheWidth)
 	addrIdx 	:= ifu.addr(CacheBit+CacheWidth-1,CacheWidth)
 	addrOffset	:= ifu.addr(CacheWidth-1,0)
+	dontTouch(addrIdx)
 	dontTouch(addrOffset)
-	
+
 	val state	= RegInit(IchState.Imm)
 	val cnt		= RegInit(0.U(CacheWidth.W))
 	switch(state){
