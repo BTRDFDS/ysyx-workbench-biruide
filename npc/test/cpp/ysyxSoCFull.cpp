7b91470d3591818ca67296c6f27fae58aea78be6
tracer-ysyx <tracer@ysyx.org>
2026-08-03 13:27:59 +0800

    > generate verilog for YsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:27:59 up  6:25,  1 user,  load average: 0.53, 0.50, 0.40

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -106,8 +106,8 @@ int main(int argc, char** argv) {
 		numCycle=0;
 	}
 	printf("\033[1;32m Welcome to ysyxSoCFull[\033[1;36m%s %s\033[1;32m] \033[0m\n",__DATE__,__TIME__);
-	// for(int i=0;(!contextp->gotFinish());i++){
-	for(int i=0;i<5000&(!contextp->gotFinish());i++){
+	for(int i=0;(!contextp->gotFinish());i++){
+	// for(int i=0;i<5000&(!contextp->gotFinish());i++){
 		#ifdef NPC_NVBroad
 			nvboard_update();
 		#endif
