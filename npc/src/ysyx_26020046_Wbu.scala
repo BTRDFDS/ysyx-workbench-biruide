fbc398f78ed984aa2faf6c4655aa573857c699a4
Bi RuiDe <1776427100@qq.com>
2026-08-06 12:44:17 +0800

    修正difftest触发延后问题

--- npc/src/ysyx_26020046_Wbu.scala
+++ npc/src/ysyx_26020046_Wbu.scala
@@ -131,7 +131,7 @@ class ysyx_26020046_Wbu(val Yosys:Boolean=false) extends Module {
 		val check = Reg(Bool())
 		when(~check && pipeValid){check := true.B}
 		when( check && in.pipe.valid){check := false.B}
-		wbuChk.io.check := check && in.pipe.valid
+		wbuChk.io.check := check && pipeValid
 		wbuChk.clock := clock
 	}
 }
