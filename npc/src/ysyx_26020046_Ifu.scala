60c6deeb24c8ce4191ac26c11b78befb7fd1a502
tracer-ysyx <tracer@ysyx.org>
2026-08-06 10:08:01 +0800

    > generate verilog for BuilderYsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:08:01 up  3:10,  1 user,  load average: 0.87, 0.47, 0.48

--- npc/src/ysyx_26020046_Ifu.scala
+++ npc/src/ysyx_26020046_Ifu.scala
@@ -18,13 +18,12 @@ class ysyx_26020046_Ifu(val PcInit:UInt,val Yosys:Boolean=false) extends Module{
 			}
 			when(in.imme.back === Back.Error || in.imme.back === Back.Jump){error := in.imme.addr(1,0) =/= 0.U}
 		}.otherwise{
-			when(ich.ready){
 			switch(in.imme.back){
 				is(Back.Jump)	{pc := in.imme.addr(31,2)	}
 				is(Back.Error)	{pc := in.imme.addr(31,2)	}
 			}
 			when(in.imme.back === Back.Error || in.imme.back === Back.Jump){error := in.imme.addr(1,0) =/= 0.U}
-		}}
+		}
 		out.pipe.pc	:= Cat(pc,0.U(2.W))
 	//状态机
 		switch(state){
