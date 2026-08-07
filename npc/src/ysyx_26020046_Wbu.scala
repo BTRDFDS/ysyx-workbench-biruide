cefa47defb960cdfd8cec1b8469fe2a0de34481b
tracer-ysyx <tracer@ysyx.org>
2026-08-07 09:03:55 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:03:55 up  2:14,  1 user,  load average: 0.89, 0.27, 0.19

--- npc/src/ysyx_26020046_Wbu.scala
+++ npc/src/ysyx_26020046_Wbu.scala
@@ -130,7 +130,7 @@ class ysyx_26020046_Wbu(val Yosys:Boolean=false) extends Module {
 		wbuChk.io.pc := pipePc
 		val check = Reg(Bool())
 		when(~check && pipeValid){check := true.B}
-		when( check && in.pipe.valid){check := false.B}
+		when( check && (pipeValid || in.pipe.valid)){check := false.B}
 		wbuChk.io.check := check && (pipeValid || in.pipe.valid)
 		wbuChk.clock := clock
 	}
