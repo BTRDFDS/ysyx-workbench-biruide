32a9a7a685d9de67aa38b1e5e19cd99a2064f5ec
tracer-ysyx <tracer@ysyx.org>
2026-08-07 14:51:24 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:51:24 up  8:03,  1 user,  load average: 0.88, 0.60, 0.38

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -107,8 +107,8 @@ int main(int argc, char** argv) {
 		numCycle=0;
 	}
 	printf("\033[1;32m Welcome to ysyxSoCFull[\033[1;36m%s %s\033[1;32m] \033[0m\n",__DATE__,__TIME__);
-	// for(int i=0;(!contextp->gotFinish());i++){
-	for(int i=0;i<500000&(!contextp->gotFinish());i++){
+	for(int i=0;(!contextp->gotFinish());i++){
+	// for(int i=0;i<500000&(!contextp->gotFinish());i++){
 		#ifdef NPC_NVBroad
 			nvboard_update();
 		#endif
