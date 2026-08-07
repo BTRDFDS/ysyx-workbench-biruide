916622bcc81fe7f7d91dc5287e5cf63fd7d66979
tracer-ysyx <tracer@ysyx.org>
2026-08-07 10:53:25 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:53:25 up  4:03,  1 user,  load average: 1.77, 1.25, 0.93

--- npc/src/ysyx_26020046_Wbu.scala
+++ npc/src/ysyx_26020046_Wbu.scala
@@ -140,8 +140,6 @@ class ysyx_26020046_Wbu(val Yosys:Boolean=false) extends Module {
 		pc		:= in.pipe.pc
 		wbuChk.io.check	:= check
 		wbuChk.io.pc	:= pc
-		wbuChk.io.pipeRd	:= pipeRdAddr
-		wbuChk.io.pipeRes	:= pipeResult
 		wbuChk.clock	:= clock
 	}
 }
@@ -150,8 +148,6 @@ class ysyx_26020046_WbuChk extends ExtModule{
 		val reg		= Input(Vec(RegNum, UInt(BitWidth.W)))
 		val ebreak	= Input(Bool())
 		val pc		= Input(UInt(BitWidth.W))
-		val pipeRd	= Input(UInt(RegWidth.W))
-		val pipeRes	= Input(UInt(BitWidth.W))
 		val check	= Input(Bool())
 	})
 	val clock = IO(Input(Clock()))
@@ -164,8 +160,6 @@ class ysyx_26020046_WbuChk extends ExtModule{
 		input logic [31:0] io_reg_16,io_reg_17,io_reg_18,io_reg_19,io_reg_20,io_reg_21,io_reg_22,io_reg_23,
 		input logic [31:0] io_reg_24,io_reg_25,io_reg_26,io_reg_27,io_reg_28,io_reg_29,io_reg_30,io_reg_31,
 		input logic [31:0] io_pc,
-		input logic [4:0] io_pipeRd,
-		input logic [31:0] io_pipeRes,
 		input logic io_check,
 		input logic clock
 	);
@@ -173,44 +167,41 @@ class ysyx_26020046_WbuChk extends ExtModule{
 	always_ff@(posedge clock) if(io_ebreak)ebreak();
 	export "DPI-C" function getRegPc;
 	function int getRegPc(input int addr);
-		if(addr=={27'b0,io_pipeRd} && io_pipeRd!=0)return io_pipeRes;
-		else begin
-			case(addr)
-				32'd00:return io_pc;
-				32'd01:return io_reg_1;
-				32'd02:return io_reg_2;
-				32'd03:return io_reg_3;
-				32'd04:return io_reg_4;
-				32'd05:return io_reg_5;
-				32'd06:return io_reg_6;
-				32'd07:return io_reg_7;
-				32'd08:return io_reg_8;
-				32'd09:return io_reg_9;
-				32'd10:return io_reg_10;
-				32'd11:return io_reg_11;
-				32'd12:return io_reg_12;
-				32'd13:return io_reg_13;
-				32'd14:return io_reg_14;
-				32'd15:return io_reg_15;
-				32'd16:return io_reg_16;
-				32'd17:return io_reg_17;
-				32'd18:return io_reg_18;
-				32'd19:return io_reg_19;
-				32'd20:return io_reg_20;
-				32'd21:return io_reg_21;
-				32'd22:return io_reg_22;
-				32'd23:return io_reg_23;
-				32'd24:return io_reg_24;
-				32'd25:return io_reg_25;
-				32'd26:return io_reg_26;
-				32'd27:return io_reg_27;
-				32'd28:return io_reg_28;
-				32'd29:return io_reg_29;
-				32'd30:return io_reg_30;
-				32'd31:return io_reg_31;
-				default: return 0;
-			endcase
-		end
+		case(addr)
+			32'd00:return io_pc;
+			32'd01:return io_reg_1;
+			32'd02:return io_reg_2;
+			32'd03:return io_reg_3;
+			32'd04:return io_reg_4;
+			32'd05:return io_reg_5;
+			32'd06:return io_reg_6;
+			32'd07:return io_reg_7;
+			32'd08:return io_reg_8;
+			32'd09:return io_reg_9;
+			32'd10:return io_reg_10;
+			32'd11:return io_reg_11;
+			32'd12:return io_reg_12;
+			32'd13:return io_reg_13;
+			32'd14:return io_reg_14;
+			32'd15:return io_reg_15;
+			32'd16:return io_reg_16;
+			32'd17:return io_reg_17;
+			32'd18:return io_reg_18;
+			32'd19:return io_reg_19;
+			32'd20:return io_reg_20;
+			32'd21:return io_reg_21;
+			32'd22:return io_reg_22;
+			32'd23:return io_reg_23;
+			32'd24:return io_reg_24;
+			32'd25:return io_reg_25;
+			32'd26:return io_reg_26;
+			32'd27:return io_reg_27;
+			32'd28:return io_reg_28;
+			32'd29:return io_reg_29;
+			32'd30:return io_reg_30;
+			32'd31:return io_reg_31;
+			default: return 0;
+		endcase
 	endfunction
 	import "DPI-C" function void wbuCheck();
 	always_ff@(posedge clock) if(io_check)wbuCheck();
