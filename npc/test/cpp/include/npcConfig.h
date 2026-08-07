8a9a9779347860b1f8d1c6d02043e78e0df840da
tracer-ysyx <tracer@ysyx.org>
2026-08-07 12:02:04 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:02:04 up  5:12,  1 user,  load average: 1.52, 0.73, 0.43

--- npc/test/cpp/include/npcConfig.h
+++ npc/test/cpp/include/npcConfig.h
@@ -10,9 +10,9 @@
 // #define NPC_I_CACHE_TRACE
 // #define NPC_D_CACHE_TRACE
 // #define NPC_MIN_TRACE
-#define NPC_DIFFTEST
-#define NPC_M_TRACE
-#define NPC_WAVE
+// #define NPC_DIFFTEST
+// #define NPC_M_TRACE
+// #define NPC_WAVE
 const uint64_t NpcMinTraceBegin = 464529400L;
 
 #endif
