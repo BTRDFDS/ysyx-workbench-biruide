c7bd56c46394598f9a564c42f62dae6a55cda257
tracer-ysyx <tracer@ysyx.org>
2026-08-07 10:11:36 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:11:36 up  3:21,  1 user,  load average: 1.31, 0.77, 0.44

--- npc/src/ysyx_26020046_Wbu.scala
+++ npc/src/ysyx_26020046_Wbu.scala
@@ -197,7 +197,7 @@ class ysyx_26020046_WbuChk extends ExtModule{
 	endfunction
 	import "DPI-C" function void wbuCheck();
 	// always_ff@(posedge clock) if(io_check)wbuCheck();
-	always_ff@(posedge clock) wbuCheck();
+	always_ff@(posedge io_check) wbuCheck();
 
 	// initial $display("%m");
 	endmodule
