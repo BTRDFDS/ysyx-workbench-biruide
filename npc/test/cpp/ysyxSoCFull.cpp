b2e0d91bf79b0f1f1faf7ff6a46c83334f682ccd
Bi RuiDe <1776427100@qq.com>
2026-08-08 13:56:27 +0800

    改用内置时钟

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -91,6 +91,7 @@ void NpcWave(){
 void NpcReturn(const char* msg,int returnCode){
 	NpcWave();
 	printOver(msg,returnCode);
+	contextp->statsPrintSummary();
 	delete top;
 	delete contextp;
 	exit(returnCode);
