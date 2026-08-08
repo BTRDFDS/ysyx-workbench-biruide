161c60c546d7f26fb0195f30eafc20276d6de21b
tracer-ysyx <tracer@ysyx.org>
2026-08-08 10:05:22 +0800

    > generate verilog for BuilderYsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:05:22 up  2:52,  1 user,  load average: 0.63, 0.44, 0.37

--- npc/src/ysyx_26020046_bus.scala
+++ npc/src/ysyx_26020046_bus.scala
@@ -14,7 +14,7 @@ object  WidthConsts{//const
 	val BurstWidth	= 2
 
 	val CacheBit	= 3
-	val CacheWidth	= 1
+	val CacheWidth	= 2
 	val CacheNum    = 1 << CacheBit
 	val CacheSize	= 1 << CacheWidth
 
