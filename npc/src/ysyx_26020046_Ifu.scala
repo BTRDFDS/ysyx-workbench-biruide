0090028410d0070a04da18b9f24dec9c460fdd16
tracer-ysyx <tracer@ysyx.org>
2026-08-08 18:17:23 +0800

    > generate verilog for BuilderYsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:17:23 up 11:05,  1 user,  load average: 2.10, 0.65, 0.31

--- npc/src/ysyx_26020046_Ifu.scala
+++ npc/src/ysyx_26020046_Ifu.scala
@@ -52,7 +52,7 @@ class ysyx_26020046_Ifu(val PcInit:UInt,val Yosys:Boolean=false) extends Module{
 		val ifuInst = Mux(res === IfuRes.Valid,instr,0.U(BitWidth.W));dontTouch(ifuInst)
 		val ifuChk = Module(new ysyx_26020046_IfuChk)
 		ifuChk.clock	:= clock
-		ifuChk.inst		:= state === MemStatus.Call && ich.ready
+		ifuChk.inst		:= state === MemStatus.Call && ich.ready && ~change
 		ifuChk.stall	:= state === MemStatus.Call
 		ifuChk.forward	:= in.imme.back === Back.Jump && in.imme.addr < Cat(pc,0.U(2.W))
 		ifuChk.backward	:= in.imme.back === Back.Jump && in.imme.addr > Cat(pc,0.U(2.W))
