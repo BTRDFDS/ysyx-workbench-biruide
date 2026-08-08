fdef78e380e28b1f02a46dddb34118cd70505bf4
tracer-ysyx <tracer@ysyx.org>
2026-08-08 20:04:36 +0800

    > generate verilog for BuilderYsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:04:36 up 12:55,  1 user,  load average: 1.52, 0.58, 0.33

--- npc/src/ysyx_26020046_Ifu.scala
+++ npc/src/ysyx_26020046_Ifu.scala
@@ -56,7 +56,7 @@ class ysyx_26020046_Ifu(val PcInit:UInt,val Yosys:Boolean=false) extends Module{
 		ifuChk.unable	:= state === MemStatus.Call && ich.ready && (change || in.imme.back === Back.Jump || in.imme.back === Back.Error)
 		ifuChk.stall	:= state === MemStatus.Call
 		ifuChk.jAb		:= in.imme.back === Back.Jump && state === MemStatus.Back
-		ifuChk.jAC		:= in.imme.back === Back.Jump && state === MemStatus.Call
+		ifuChk.jAC		:= state === MemStatus.Call && (ich.ready& ~change & (in.imme.back === Back.Ready || in.imme.back === Back.Wait)) || error
 	}
 }
 class ysyx_26020046_IfuChk extends ExtModule{
