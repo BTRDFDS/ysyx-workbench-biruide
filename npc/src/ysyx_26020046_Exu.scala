6c11f85d7e41a5bbfdce055dc55ff67a7d626170
tracer-ysyx <tracer@ysyx.org>
2026-08-08 21:49:06 +0800

    > generate verilog for BuilderYsyxSoc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:49:06 up 14:43,  1 user,  load average: 1.43, 0.98, 0.74

--- npc/src/ysyx_26020046_Exu.scala
+++ npc/src/ysyx_26020046_Exu.scala
@@ -94,7 +94,7 @@ class ysyx_26020046_Exu(val Yosys:Boolean=false) extends Module {
 			when(pipeEnJcod || enBfun){
 				val hasJump = RegInit(false.B)
 				when(out.imme.back===Back.Jump)	{hasJump := true.B}
-				otherwise						{hasJump := false.B}
+				.otherwise						{hasJump := false.B}
 				out.imme.addr := result
 				out.imme.back := Mux(hasJump,Mux(in.pipe.valid||in.imme.back=/=Back.Wait,in.imme.back,Back.Ready),Back.Jump)
 			}.otherwise{out.imme.back := in.imme.back}
