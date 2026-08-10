6af4c29dfd6c0c881d0bfa3133d3c5b4cfaf52c9
Bi RuiDe <1776427100@qq.com>
2026-08-10 21:53:33 +0800

    把runstoptime单拿出来

--- npc/test/cpp/ysyxSoCFull.cpp
+++ npc/test/cpp/ysyxSoCFull.cpp
@@ -94,8 +94,7 @@ int main(int argc, char** argv) {
 		numCycle=0;
 	}
 	printf("\033[1;32m Welcome to ysyxSoCFull[\033[1;36m%s %s\033[1;32m] \033[0m\n",__DATE__,__TIME__);
-	for(int i=0;(!contextp->gotFinish())&(!stop);i++){
-	// for(int i=0;i<500000&(!contextp->gotFinish()&(!stop));i++){
+	for(int i=0;(i<runstopTime||runstopTime==0)&&(!contextp->gotFinish()&(!stop));i++){
 		#ifdef NPC_NVBroad
 			nvboard_update();
 		#endif
