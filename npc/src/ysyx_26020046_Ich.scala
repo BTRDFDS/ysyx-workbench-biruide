b9becf0e76b4d8a86660826ffb6112d8b860dda8
tracer-ysyx <tracer@ysyx.org>
2026-08-11 17:43:54 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:43:54 up 10:20,  1 user,  load average: 1.35, 0.70, 0.45

--- npc/src/ysyx_26020046_Ich.scala
+++ npc/src/ysyx_26020046_Ich.scala
@@ -79,20 +79,20 @@ class ysyx_26020046_Ich(val Yosys:Boolean=false) extends Module {
 	}
 }
 class ysyx_26020046_IchChk extends ExtModule{
-	// val hit		= IO(Input(Bool()))
+	val hit		= IO(Input(Bool()))
 	val miss	= IO(Input(Bool()))
 	val clock	= IO(Input(Clock()))
 	setInline("ysyx_26020046_IchChk.sv",
 	"""
 	module ysyx_26020046_IchChk(
-		// input logic hit,
+		input logic hit,
 		input logic miss,
 		input logic clock
 	);
-	// import "DPI-C" function void ichHit();
+	import "DPI-C" function void ichHit();
 	import "DPI-C" function void ichMiss();
 
-	// always_ff@(posedge hit)	ichHit();
+	always_ff@(posedge hit)	ichHit();
 	always_ff@(posedge miss)ichMiss();
 	endmodule
 	"""
