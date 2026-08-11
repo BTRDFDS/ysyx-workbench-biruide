d9012c3acf3d3aaf9848c4a1eace07e5932791b7
tracer-ysyx <tracer@ysyx.org>
2026-08-11 18:13:46 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:13:46 up 10:49,  1 user,  load average: 1.63, 0.82, 0.46

--- npc/src/ysyx_26020046_Ich.scala
+++ npc/src/ysyx_26020046_Ich.scala
@@ -92,8 +92,8 @@ class ysyx_26020046_IchChk extends ExtModule{
 	import "DPI-C" function void ichHit();
 	import "DPI-C" function void ichMiss();
 
-	always_ff@(negedge hit)	ichHit();
-	always_ff@(posedge miss)ichMiss();
+	always_ff@(posedge hit)	ichHit();
+	always_ff@(negedge miss)ichMiss();
 	endmodule
 	"""
 	)
