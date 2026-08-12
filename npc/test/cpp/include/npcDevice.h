849d0f3740879bbb4f2a604bda125c3afb6597f1
tracer-ysyx <tracer@ysyx.org>
2026-08-12 22:30:37 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:30:37 up 14:06,  1 user,  load average: 0.45, 0.43, 0.30

--- npc/test/cpp/include/npcDevice.h
+++ npc/test/cpp/include/npcDevice.h
@@ -62,8 +62,8 @@ void printCounter(){
 		numIchMiss,numIchMiss/(float)numIfuInst,
 		numIchHit+numIchMiss);
 	numIfuJbHit=numIduJump+numIduBr-numIfuJbMiss;
-	printf("ifu inst = %ld[+%f] wait= %ld AMAT= %f jbHit= %ld[%f] jbMiss= %ld[%f]\n",
-		numIfuInst,(numIfuInst-numInst)/(float)numInst,
+	printf("ifu inst = %ld[+ %ld %f] wait= %ld AMAT= %f jbHit= %ld[%f] jbMiss= %ld[%f]\n",
+		numIfuInst,numIfuInst-numInst,(numIfuInst-numInst)/(float)numInst,
 		numIfuStall,(float)((float)numIfuStall)/((float)numIfuInst),
 		numIfuJbHit ,numIfuJbHit /(float)(numIfuJbHit+numIfuJbMiss),
 		numIfuJbMiss,numIfuJbMiss/(float)(numIfuJbHit+numIfuJbMiss)
