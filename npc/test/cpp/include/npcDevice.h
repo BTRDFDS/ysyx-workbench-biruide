33ecea6613c1e12bf395f0e0246e7ca2838d101d
tracer-ysyx <tracer@ysyx.org>
2026-08-12 22:28:20 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:28:20 up 14:04,  1 user,  load average: 1.11, 0.32, 0.24

--- npc/test/cpp/include/npcDevice.h
+++ npc/test/cpp/include/npcDevice.h
@@ -61,7 +61,7 @@ void printCounter(){
 		numIchHit ,numIchHit /(float)numIfuInst,
 		numIchMiss,numIchMiss/(float)numIfuInst,
 		numIchHit+numIchMiss);
-	numIfuJbHit-=numIfuJbMiss;
+	numIfuJbHit=numIduJump+numIduBr-numIfuJbMiss;
 	printf("ifu inst = %ld wait= %ld AMAT= %f jbHit= %ld[%f] jbMiss= %ld[%f]\n",
 		numIfuInst,numIfuStall,(float)((float)numIfuStall)/((float)numIfuInst),
 		numIfuJbHit ,numIfuJbHit /(float)(numIfuJbHit+numIfuJbMiss),
