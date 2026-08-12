a251c009129f171e63d91b24fccae38f9a2e5b2e
tracer-ysyx <tracer@ysyx.org>
2026-08-12 19:13:22 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:13:22 up 10:58,  1 user,  load average: 1.08, 0.44, 0.21

--- npc/src/ysyx_26020046_Ifu.scala
+++ npc/src/ysyx_26020046_Ifu.scala
@@ -66,8 +66,9 @@ class ysyx_26020046_Ifu(val PcInit:UInt,val Yosys:Boolean=false) extends Module{
 		val ifuPc	= Mux(out.pipe.res === IfuRes.Valid,Cat(out.pipe.pc,0.U(2.W)),0.U(32.W));dontTouch(ifuPc)
 		val ifuInst	= Mux(out.pipe.res === IfuRes.Valid,out.pipe.instr,0.U(BitWidth.W));dontTouch(ifuInst)
 		val ifuChk = Module(new ysyx_26020046_IfuChk)
+		val noEbreak = RegInit(true.B);when(ich.ready && out.pipe.res === IfuRes.Valid && in.imme.ready && out.pipe.instr === 0x00100073L.U){noEbreak := false.B}
 		ifuChk.clock	:= clock
-		ifuChk.inst		:= ich.ready && out.pipe.res === IfuRes.Valid && in.imme.ready
+		ifuChk.inst		:= ich.ready && out.pipe.res === IfuRes.Valid && in.imme.ready && noEbreak
 		ifuChk.stall	:= out.pipe.res === IfuRes.Null
 		ifuChk.jbMiss	:= in.imme.jump
 		ifuChk.jbHit	:= in.imme.ready && ich.ready && (isBranch || isJal || isJalr)
