03929f8bf9d026684de5ba17cad383ed2d2701e0
tracer-ysyx <tracer@ysyx.org>
2026-08-12 14:18:10 +0800

    > generate verilog for BuilderSta ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:18:10 up  6:18,  1 user,  load average: 1.10, 0.47, 0.68

--- npc/src/ysyx_26020046_Exu.scala
+++ npc/src/ysyx_26020046_Exu.scala
@@ -84,7 +84,7 @@ class ysyx_26020046_Exu(val Yosys:Boolean=false) extends Module {
 		}
 	}
 
-	val shouldBe = Mux(pipeEnJcod || enBfun, result(31,2), pipePc+1.U)
+	val shouldBe = Mux(pipeEnJcod || enBfun, result(31,2)=/=in.pipe.pc, pipePc+1.U=/=in.pipe.pc)
 	val hasSend = RegInit(false.B)
 	when(out.imme.ready){hasSend := false.B}
 	.elsewhen(pipeValid&&(pipeEnJcod||pipeBfu=/=ExuBfu.Null)){hasSend := true.B}
@@ -94,7 +94,7 @@ class ysyx_26020046_Exu(val Yosys:Boolean=false) extends Module {
 	//BJ
 
 	out.imme.jbpu := ~in.imme.error && (pipeValid && (pipeBfu=/=ExuBfu.Null || (pipeEnJcod && pipeAlu=/=ExuAlu.Jalr)))
-	out.imme.jump :=  in.imme.error || (pipeValid && (pipeBfu=/=ExuBfu.Null || pipeEnJcod)&& ~hasSend && (shouldBe=/=in.pipe.pc || pipeAlu===ExuAlu.Jalr))
+	out.imme.jump :=  in.imme.error || (pipeValid && (pipeBfu=/=ExuBfu.Null || pipeEnJcod)&& ~hasSend && (shouldBe || pipeAlu===ExuAlu.Jalr))
 
 	out.imme.ready	:= in.imme.ready || ~pipeValid
 	out.imme.addr	:= Mux(in.imme.error,in.imme.addr,Mux((pipeEnJcod || enBfun),result,Cat(pipePc+1.U,0.U(2.W))))
