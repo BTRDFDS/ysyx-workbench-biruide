d1f8a15cb3a3dd5700ef5c8b1b7298597babdc4f
tracer-ysyx <tracer@ysyx.org>
2026-08-13 16:01:31 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:01:31 up  8:38,  1 user,  load average: 1.05, 0.71, 0.60

--- npc/src/ysyx_26020046.scala
+++ npc/src/ysyx_26020046.scala
@@ -125,7 +125,8 @@ class ysyx_26020046(val PcInit:UInt=0x30000000L.U,val Yosys:Boolean=false) exten
 		chk.rdValue	:= Get(wbu.pipeResult)
 		chk.pc		:= Cat(Get(wbu.pipePc),0.U(2.W))
 		chk.dnpc := 0.U(32.W)
-		when(Get(lsu.pipeValid)){chk.dnpc := Cat(Get(lsu.pipePc),0.U(2.W))}
+		when(chk.ebreak){chk.dnpc := Cat(Get(wbu.pipePc)+1.U,0.U(2.W))}
+		.elsewhen(Get(lsu.pipeValid)){chk.dnpc := Cat(Get(lsu.pipePc),0.U(2.W))}
 		.elsewhen(Get(exu.pipeValid)){chk.dnpc := Cat(Get(exu.pipePc),0.U(2.W))}
 		.elsewhen(Get(idu.pipeRes)===IfuRes.Valid){chk.dnpc := Cat(Get(idu.pipePc),0.U(2.W))}
 		.otherwise{chk.dnpc := Cat(Get(ifu.pipePc),0.U(2.W))}
