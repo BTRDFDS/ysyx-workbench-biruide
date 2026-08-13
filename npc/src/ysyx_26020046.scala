9137e7dc8506ffbca493e8c42eb414fc672085c7
tracer-ysyx <tracer@ysyx.org>
2026-08-13 15:13:47 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:13:47 up  7:50,  1 user,  load average: 1.52, 0.82, 0.53

--- npc/src/ysyx_26020046.scala
+++ npc/src/ysyx_26020046.scala
@@ -212,7 +212,7 @@ class ysyx_26020046_Chk extends ExtModule{
 	import "DPI-C" function void lsuStoreWait();
 	import "DPI-C" function void lsuTrace(int addr);
 
-	import "DPI-C" function void stop();
+	import "DPI-C" function void ebreakStop();
 	import "DPI-C" function void wbuCheck(int pc);
 	export "DPI-C" function getRegPc;
 
@@ -236,7 +236,7 @@ class ysyx_26020046_Chk extends ExtModule{
 		if(store)		lsuStore();
 		if(storeWait)	lsuStoreWait();
 
-		if(ebreak)	stop();
+		if(ebreak)	ebreakStop();
 		if(check)	wbuCheck(dnpc);
 	end
 	always_ff@(posedge load or posedge store)lsuTrace(addr);
--- npc/test/cpp/include/npcDevice.h
+++ npc/test/cpp/include/npcDevice.h
@@ -334,7 +334,7 @@ extern "C" void sdram_write(int addr,int data){
 ////////////////////////////////////////////////////////////////////////////////////////
 extern "C" int getRegPc(int addr);
 extern "C" int getNextPc();
-extern "C" void ebreak(){
+extern "C" void ebreakStop(){
 	numInst++;
 	iCacheTraceFileWrite(getRegPc(0));
 	return NpcFinish("ebreak",getRegPc(10)!=0);
