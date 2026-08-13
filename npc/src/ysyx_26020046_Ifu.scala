8e28dea393f06042ef7e32a6dcab5c110bcd4a77
tracer-ysyx <tracer@ysyx.org>
2026-08-13 19:02:09 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:02:09 up 11:39,  1 user,  load average: 2.73, 1.65, 1.23

--- npc/src/ysyx_26020046_Ifu.scala
+++ npc/src/ysyx_26020046_Ifu.scala
@@ -20,7 +20,7 @@ class ysyx_26020046_Ifu(val PcInit:UInt,val Yosys:Boolean=false) extends Module{
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 	val bp2Cnt	= RegInit(2.U(2.W))
-	val bp2Hit = bp2Cnt >= 2.U
+	val bp2Hit = out.pipe.instr(31)
 
 	val BtbBits	= 3
 	val BtbSize	= 1 << BtbBits
@@ -38,10 +38,9 @@ class ysyx_26020046_Ifu(val PcInit:UInt,val Yosys:Boolean=false) extends Module{
 	when(in.imme.jump){
 		pipePc := in.imme.addr(31,2)
 	}.elsewhen(in.imme.ready&&ich.ready){
-		// when(((isBranch&&bp2Hit) || isJal||isJalr)&&btbHit){//
-		// 			pipePc := btbAddr(btbIndex)
-		// }.otherwise{pipePc := pipePc+1.U}
-		pipePc := pipePc+1.U
+		when(((isBranch&&bp2Hit) || isJal||isJalr)&&btbHit){//
+					pipePc := btbAddr(btbIndex)
+		}.otherwise{pipePc := pipePc+1.U}
 	}
 	val shouldNotJump = in.imme.pc+1.U === in.imme.addr(31,2)
 	when(in.imme.jump && in.imme.jbpu){
