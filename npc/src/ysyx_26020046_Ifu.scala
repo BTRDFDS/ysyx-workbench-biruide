1b5cab320ce73a681bbd850e0cc49783045f3859
tracer-ysyx <tracer@ysyx.org>
2026-08-13 19:11:42 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:11:42 up 11:48,  1 user,  load average: 1.00, 1.13, 1.15

--- npc/src/ysyx_26020046_Ifu.scala
+++ npc/src/ysyx_26020046_Ifu.scala
@@ -20,7 +20,7 @@ class ysyx_26020046_Ifu(val PcInit:UInt,val Yosys:Boolean=false) extends Module{
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 	val bp2Cnt	= RegInit(2.U(2.W))
-	val bp2Hit = out.pipe.instr(31)
+	val bp2Hit = bp2Cnt >= 2.U
 
 	val BtbBits	= 3
 	val BtbSize	= 1 << BtbBits
