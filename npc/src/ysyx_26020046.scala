d7e2055f650c9bf543ccdf307f8e57649df571cf
tracer-ysyx <tracer@ysyx.org>
2026-08-14 12:47:19 +0800

    > generate verilog for BuilderNpc ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:47:19 up  5:59,  1 user,  load average: 1.47, 0.59, 0.51

--- npc/src/ysyx_26020046.scala
+++ npc/src/ysyx_26020046.scala
@@ -99,13 +99,14 @@ class ysyx_26020046(val PcInit:UInt=0x30000000L.U,val Yosys:Boolean=false) exten
 		chk.clock	:= clock
 		val btbIdu = RegInit(false.B);when(Get(idu.out.imme.ready)){btbIdu := (Get(ifu.isBranch)&&Get(ifu.bp2Hit)) || Get(ifu.isJal)||Get(ifu.isJalr)}
 		val btbExu = RegInit(false.B);when(Get(exu.out.imme.ready)){btbExu := btbIdu}
+		val shouldbe = Mux(Get(exu.in.pipe.valid),Get(exu.in.pipe.pc),Get(ifu.pipePc))
 		val noEbreak = RegInit(true.B);when(Get(ifu.ich.ready) && Get(ifu.out.pipe.res) === IfuRes.Valid && Get(ifu.in.imme.ready) && Get(ifu.out.pipe.instr) === 0x00100073L.U && ~Get(ifu.in.imme.jump)){noEbreak := false.B}
 		val chkIfu = Seq(chk.inst,chk.stall,chk.jbMiss,chk.jbHit,chk.miss,chk.ifuMiss);chkIfu.foreach(_ := false.B)
 		when(noEbreak){
 			when(Get(ifu.ich.ready) && Get(ifu.out.pipe.res) === IfuRes.Valid && (Get(ifu.in.imme.ready) || Get(ifu.in.imme.jump))){chk.inst := true.B}
 			when(Get(ifu.out.pipe.res) === IfuRes.Null)							{chk.stall	:= true.B}
 			when(Get(ifu.in.imme.jump))											{chk.jbMiss	:= true.B}
-			when((Get(exu.shouldBe)===Cat(Get(exu.in.pipe.pc),0.U(2.W)))=/=btbExu && (Get(exu.pipeBfu)=/=ExuBfu.Null))	{chk.jbHit	:= true.B}
+			when((Get(exu.shouldBe)===Cat(shouldbe,0.U(2.W)))=/=btbExu && (Get(exu.pipeBfu)=/=ExuBfu.Null))	{chk.jbHit	:= true.B}
 
 
 			when(Get(idu.in.imme.jump) && Get(idu.pipeRes) === IfuRes.Valid)		{chk.miss	:= true.B}
