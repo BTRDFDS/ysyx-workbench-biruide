4e2d5da83717cecd2a3080e6f9061690d62e5a59
tracer-ysyx <tracer@ysyx.org>
2026-08-14 13:47:03 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:47:03 up  7:02,  1 user,  load average: 0.87, 0.71, 0.39

--- npc/test/cpp/bjrsim/ifusim.cpp
+++ npc/test/cpp/bjrsim/ifusim.cpp
@@ -13,9 +13,9 @@ bool BPB2(bool result){
 	return res;
 }
 int main() {
-	// file.open("./bin/BJRmicrobench-test.bin", std::ios::in | std::ios::binary);
+	file.open("./bin/BJRmicrobench-test.bin", std::ios::in | std::ios::binary);
 	// file.open("./bin/BJdiv.bin", std::ios::in | std::ios::binary);
-	file.open("./bin/BJRdummy.bin", std::ios::in | std::ios::binary);
+	// file.open("./bin/BJRdummy.bin", std::ios::in | std::ios::binary);
 	if (!file.is_open()) {printf("Failed to open file\n");return -1;}
 	uint64_t ju{},jr{},bi{},bn{},hitBTFN{},hitBPB2{};
 	for(uint64_t cnt=0;;cnt++){
@@ -48,7 +48,7 @@ int main() {
 		}
 		if(branch){
 			bool res = BPB2(jump);
-			printf("res=%d pc=%8x jump=%d addr=%8x\n",res,pc,jump,addr);
+			// printf("res=%d pc=%8x jump=%d addr=%8x\n",res,pc,jump,addr);
 			if(res==jump){
 				hitBPB2++;
 			}else{
