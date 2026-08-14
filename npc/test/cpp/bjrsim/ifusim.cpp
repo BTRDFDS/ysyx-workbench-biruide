6447e06177d10444c44ad0e00045188df4ec7bad
tracer-ysyx <tracer@ysyx.org>
2026-08-14 11:28:33 +0800

    > sim RTL ysyx_26020046 Bi RuiDe Linux LAPTOP-3IAF75LK 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:28:33 up  4:35,  1 user,  load average: 0.76, 0.67, 0.55

--- npc/test/cpp/bjrsim/ifusim.cpp
+++ npc/test/cpp/bjrsim/ifusim.cpp
@@ -5,88 +5,32 @@
 #include <stdint.h>
 #include <vector>
 std::fstream file;
-
-bool BTFN(bool forward){return forward;}
 bool BPB2(bool result){
 	static uint8_t b2{2};
 	bool res = b2>=2;
-	if(result){
-		if(b2<3){b2++;}
-	}else{
-		if(b2>0){b2--;}
-	}
+	if(result)	{if(b2<3){b2++;}}
+	else 		{if(b2>0){b2--;}}
 	return res;
 }
-class BTB{
-	uint32_t bits,size,mask,cnt;
-	std::vector<uint32_t> tags,addr,uses;
-	public:
-		BTB(uint32_t bit= 4){
-			bits = bit;
-			size = 1<<bits;
-			mask = size-1;
-			cnt = 0;
-			tags.resize(size);
-			addr.resize(size);
-			uses.resize(size);
-		}
-		uint32_t pcl(uint32_t pc,uint32_t tobe=0,bool write=false){
-			uint32_t res{};
-			uint32_t idx = (pc>>2)&mask;
-			if(write){
-				tags[idx] = (pc>>(bits+2));
-				addr[idx] = tobe;
-			}else{
-				if(tags[idx]==(pc>>(bits+2))){
-					res =addr[idx];
-				}
-			}
-			return res;
-		}
-		uint32_t all(uint32_t pc,uint32_t tobe=0,bool write=false){
-			uint32_t res{};
-			if(write){
-				tags[cnt] = pc>>2;
-				addr[cnt] = tobe;
-				cnt = (cnt+1)%size;
-			}else{
-				for(uint32_t i=0;i<size;i++){
-					if(tags[i]==(pc>>2)){
-						res = addr[i];
-						uses[i]++;
-						break;
-					}
-				}
-			}
-			return res;
-		}
-	};
 int main() {
-	BTB btb0(3);
-	BTB btb1(1);
-	file.open("./bin/BJRmicrobench-train.bin", std::ios::in | std::ios::binary);
+	file.open("./bin/BJRmicrobench-test.bin", std::ios::in | std::ios::binary);
 	// file.open("./bin/BJdiv.bin", std::ios::in | std::ios::binary);
 	// file.open("./bin/BJdummy.bin", std::ios::in | std::ios::binary);
 	if (!file.is_open()) {printf("Failed to open file\n");return -1;}
-	uint64_t ju{},jr{},bi{},bn{},btf{},btb{},hitBTFN{},hitBPB2{},hitBTFN_BTB{},hitBPB2_BTB{};
+	uint64_t ju{},jr{},bi{},bn{},hitBTFN{},hitBPB2{};
 	for(uint64_t cnt=0;;cnt++){
 		uint32_t addr{},pc{};
 		uint8_t state{};
 		if(!file.read((char*)&pc, sizeof(pc))){
 			uint64_t br = bi+bn;
-			printf("cnt= %ld ju= %ld[%f] jr= %ld[%f] bi= %ld[%f] bn= %ld[%f] btf= %ld[%f] btb= %ld[%f]\n",
+			printf("cnt= %ld ju= %ld[%f] jr= %ld[%f] bi= %ld[%f] bn= %ld[%f]\n",
 				cnt,
 				ju,ju/float(cnt),
 				jr,jr/(float)br,
 				bi,bi/(float)br,
-				bn,bn/(float)br,
-				btf,btf/(float)br,
-				btb,btb/(float)br
+				bn,bn/(float)br
 			);
-			printf("hitBTFN= %ld[%f]\n",hitBTFN,hitBTFN/(float)br);
-			printf("hitBPB2= %ld[%f]\n",hitBPB2,hitBPB2/(float)br);
-			printf("hitBTFN_BTB= %ld[%f]\n",hitBTFN_BTB,hitBTFN_BTB/(float)cnt);
-			printf("hitBPB2_BTB= %ld[%f]\n",hitBPB2_BTB,hitBPB2_BTB/(float)cnt);
+			printf("bpb2= %ld[%f]\n",hitBPB2,hitBPB2/(float)cnt);
 			break;
 		}
         bool branch{},jump{},sext{};
@@ -98,42 +42,12 @@ int main() {
 		if(branch){
 			if(jump)	bi++;
 			else		bn++;
-			if(sext)	btb++;
-			else		btf++;
 		}else{
 			if(sext)	jr++;
 			else		ju++;
 		}
-
-		bool btfn	= false;
-		bool bpb2	= false;
-		bool btb	= false;
-		if(branch){
-			btfn = BTFN(sext);
-			bpb2 = BPB2(jump);
-		}
-		if(!branch || btfn || bpb2){
-			if(!branch && sext)	btb = btb1.all(pc)==addr && addr!=0;
-			else				btb = btb0.all(pc)==addr && addr!=0;
-		}
-		if(branch){
-			if(jump == btfn)hitBTFN++;
-			if(jump == bpb2)hitBPB2++;
-			if(jump == btfn && (jump?btb:1))hitBTFN_BTB++;
-			if(jump == bpb2 && (jump?btb:1))hitBPB2_BTB++;
-		}
-		if(!branch){
-			hitBTFN++;
-			hitBPB2++;
-		}
-		if(!branch && btb){
-			hitBTFN_BTB++;
-			hitBPB2_BTB++;
-		}
-		if(jump && !btb){
-			if(!branch && sext)	btb1.all(pc,addr,true);
-			else				btb0.all(pc,addr,true);
-		}
+		if(branch)if(BPB2(jump)==jump)	hitBPB2++;
+		else							hitBPB2++;
 	}
 	file.close();
 }
