c03c7e3e89329392af979d5f282459c913b29016
Bi RuiDe <1776427100@qq.com>
2026-08-14 22:00:53 +0800

    尝试修正分支sim

--- npc/test/cpp/bjrsim/ifusim.cpp
+++ npc/test/cpp/bjrsim/ifusim.cpp
@@ -5,11 +5,40 @@
 #include <stdint.h>
 #include <vector>
 std::fstream file;
-bool BPB2(bool result){
+bool BPB2(bool result=false,bool write=false){
 	static uint8_t b2{2};
 	bool res = b2>=2;
-	if(result)	{if(b2<3){b2++;}}
-	else 		{if(b2>0){b2--;}}
+	if(write){
+		if(result)	{if(b2<3){b2++;}}
+		else 		{if(b2>0){b2--;}}
+	}
+	return res;
+}
+
+uint32_t BTB(uint32_t pc,bool &suce,uint32_t addr=0,bool write=false){
+	const  uint32_t BTBbits = 3;
+	const  uint32_t BTBsize = 1<<BTBbits;
+	const  uint32_t BTBmask = BTBsize-1;
+	static uint32_t BTBtags[BTBsize]{};
+	static uint32_t BTBaddr[BTBsize]{};
+	static uint32_t BTBcnt{};
+
+	uint32_t res{};
+	uint32_t BTBidx = (pc>>2)&BTBmask;
+	suce = false;
+	if(write){
+		BTBtags[cBTBcntnt]	= pc>>2;
+		BTBaddr[BTBcnt]		= addr;
+		BTBcnt = (BTBcnt+1)%BTBsize;
+	}else{
+		for(uint32_t i=0;i<BTBsize;i++){
+			if(BTBtags[i]==(pc>>2)){
+				res = BTBaddr[i];
+				suce = true;
+				break;
+			}
+		}
+	}
 	return res;
 }
 int main() {
@@ -17,7 +46,7 @@ int main() {
 	// file.open("./bin/BJdiv.bin", std::ios::in | std::ios::binary);
 	// file.open("./bin/BJRdummy.bin", std::ios::in | std::ios::binary);
 	if (!file.is_open()) {printf("Failed to open file\n");return -1;}
-	uint64_t ju{},jr{},bi{},bn{},hitBTFN{},hitBPB2{};
+	uint64_t ju{},jr{},bi{},bn{},hit{},hitBPB2{},missBPB2{};
 	for(uint64_t cnt=0;;cnt++){
 		uint32_t addr{},pc{};
 		uint8_t state{};
@@ -30,7 +59,9 @@ int main() {
 				bi,bi/(float)br,
 				bn,bn/(float)br
 			);
-			printf("bpb2= %ld[%f]\n",hitBPB2,hitBPB2/(float)cnt);
+			printf("hit = %ld[%f]\n",hit,hit/(float)cnt);
+			printf("hitBPB2= %ld[%f]\n",hitBPB2,hitBPB2/(float)cnt);
+			printf("missBPB2= %ld[%f]\n",missBPB2,missBPB2/(float)cnt);
 			break;
 		}
         bool branch{},jump{},sext{};
@@ -46,16 +77,30 @@ int main() {
 			if(sext)	jr++;
 			else		ju++;
 		}
-		if(branch){
-			bool res = BPB2(jump);
-			// printf("res=%d pc=%8x jump=%d addr=%8x\n",res,pc,jump,addr);
-			if(res==jump){
-				hitBPB2++;
-			}else{
+		bool isJump{},isGet{};
+		uint32_t getAddr{};
+		if(branch)	{isJump = BPB2();}
+		else		{isJump = true;}
+		if(isJump)	{getAddr = BTB(pc,isGet);}
+		if(jump){
+			if(isJump){
+				if(&&isGet&&getAddr==addr)hit++;
+				else {
+					hitBPB2++;
+					if(branch || (!sext))BTB(pc,isGet,addr,true);
+				}
+			}else {
+				missBPB2++;
+				if(branch)BPB2(true,true);
+				if(branch || (!sext))BTB(pc,isGet,addr,true);
 			}
-		}
-		else{
-			hitBPB2++;
+		}else{
+			if(isJump){
+				missBPB2++;
+				if(branch)BPB2(false,true);
+			}else{
+				hitBPB2++;
+			}
 		}
 	}
 	file.close();
