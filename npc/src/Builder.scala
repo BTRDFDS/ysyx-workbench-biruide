8901c3f86f6c9a5661163b306ce7adc768c13414
Bi RuiDe <1776427100@qq.com>
2026-08-20 21:54:36 +0800

    基本接入iverilog成功，目前卡在crc32，有x进入了系统

--- npc/src/Builder.scala
+++ npc/src/Builder.scala
@@ -10,6 +10,18 @@ object FirtoolOptions{
 			"locationInfoStyle=wrapInAtSquareBracket"
 		).reduce(_ + "," + _)
 	)
+	val firtoolOptionsIverilog = Array(
+		"--default-layer-specialization=enable",
+		"--verification-flavor=immediate",
+		"--lowering-options=" + List(
+			// make yosys happy
+			// see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
+			"disallowLocalVariables",
+			"disallowPackedArrays",
+			"noAlwaysComb",//iverilog
+			"locationInfoStyle=wrapInAtSquareBracket"
+		).reduce(_ + "," + _)
+	)
 }
 import FirtoolOptions._
 import circt.stage.ChiselStage.emitSystemVerilogFile
@@ -18,3 +30,4 @@ object BuilderNpc		extends App	{emitSystemVerilogFile(new ysyx_26020046_Npc		,Ar
 object BuilderYsyxSoc	extends App	{emitSystemVerilogFile(new ysyx_26020046			,Array("--target-dir","./build/ysyxsoc"),firtoolOptions)}
 object BuilderSta		extends App	{emitSystemVerilogFile(new ysyx_26020046(0.U,true)	,Array("--target-dir","./build/sta")	,firtoolOptions)}
 object BuilderIcache	extends App	{emitSystemVerilogFile(new ysyx_26020046_Ich(true)	,Array("--target-dir","./build/icache")	,firtoolOptions)}
+object BuilderIverilog	extends App	{emitSystemVerilogFile(new ysyx_26020046_iverilog	,Array("--target-dir","./build/iverilog"),firtoolOptionsIverilog)}
