3cbc4c24ce48f60a406aff6aa659aad70aca66e3
Bi RuiDe <1776427100@qq.com>
2026-08-27 08:58:35 +0800

    900Mhz校准

--- npc/test/cpp/include/npcConfig.h
+++ npc/test/cpp/include/npcConfig.h
@@ -13,10 +13,10 @@
 // #define NPC_BRACHE_TRACE
 // #define NPC_MIN_TRACE
 // #define NPC_M_TRACE
-#define NPC_DIFFTEST
+// #define NPC_DIFFTEST
 // #define NPC_WAVE
 
-const uint64_t runstopTime = 500000;
+const uint64_t runstopTime = 0000;
 const uint64_t NpcMinTraceBegin = 464529400L;
 
 #endif
